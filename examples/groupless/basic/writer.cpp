ecaca05f0f23adc0439f2b73e5a848f421a497a7
Norbert Podhorszki <pnorbert@ornl.gov>
2017-02-16 16:11:19 -0500

    moved example to groupless/basic making room for other ones

--- examples/groupless/basic/writer.cpp
+++ examples/groupless/basic/writer.cpp
@@ -11,6 +11,13 @@
 #include <mpi.h>
 #include "ADIOS_OOP.h"
 
+namespace adios {
+    typedef enum {
+        VARYING_DIMENSION = -1,
+        LOCAL_VALUE = 0,
+        GLOBAL_VALUE = 1
+    };
+}
 
 int main( int argc, char* argv [] )
 {
@@ -26,39 +33,43 @@ int main( int argc, char* argv [] )
     const unsigned int Nx = 10;
     const int Nparts = rand()%6 + 5; // random size per process, 5..10 each
 
-    std::vector<double> NiceVar( Nx );
-    for (int i=0; i < NX; i++)
+    std::vector<double> NiceArray( Nx );
+    for( int i=0; i < Nx; i++ )
     {
-        NiceVar[i] = rank*NX + (double)i;
+        NiceArray[i] = rank*Nx + (double)i;
     }
-    std::vector<float> RaggedVar( Nparts );
-    for (int i=0; i < Nparts; i++)
+
+    std::vector<float> RaggedArray( Nparts );
+    for( int i=0; i < Nparts; i++ )
     {
-        RaggedVar[i] = rank*NX + (float)i;
+        RaggedArray[i] = rank*Nx + (float)i;
     }
 
 
     try
     {
         //Define group and variables with transforms, variables don't have functions, only group can access variables
-        adios::Variable<int>&    varNX      = adios.DefineVariable<unsigned int>( "NX" ); // global scalar value
-        adios::Variable<int>&    varNparts  = adios.DefineVariable<int>( "Nparts" ); // scalar value different on every process
-        adios::Variable<double>& varNice    = adios.DefineVariable<double>( "D", {nproc*Nx} ); // 1D global array
-        adios::Variable<float>&  varRagged  = adios.DefineVariable<float>( "F", {nproc,adios::VARYING_DIMENSION} ); // local array
+        adios::Variable<unsigned int>& varNX = adios.DefineVariable<unsigned int>( "NX" ); // global single-value across processes
+        adios::Variable<int>&    varNproc   = adios.DefineVariable<int>( "nproc", adios::GLOBAL_VALUE ); // same def for global value
+        adios::Variable<int>&    varNparts  = adios.DefineVariable<int>( "Nparts", adios::LOCAL_VALUE ); // a single-value different on every process
+        adios::Variable<double>& varNice    = adios.DefineVariable<double>( "Nice", {nproc*Nx} ); // 1D global array
+        adios::Variable<float>&  varRagged  = adios.DefineVariable<float>( "Ragged", {nproc,adios::VARYING_DIMENSION} ); // ragged array
 
         //add transform to variable in group...not executed (just testing API)
         adios::Transform bzip2 = adios::transform::BZIP2( );
-        NiceVar.AddTransform( bzip2, 1 );
+        varNice->AddTransform( bzip2, 1 );
 
         //Define method for engine creation
         // 1. Get method def from config file or define new one
         adios::Method& bpWriterSettings = adios.GetMethod( "output" );
-        if (bpWriterSettings.undeclared())
+        if( bpWriterSettings.undeclared() )
         {
             // if not defined by user, we can change the default settings
             bpWriterSettings.SetEngine( "BP" ); // BP is the default engine
-            bpWriterSettings.SetAggregation( (nproc+1)/2 ); // number of aggregators
-            bpWriterSettings.AddTransport( "BP", "have_metadata_file=yes" ); // BP is the default transport
+            bpWriterSettings.AddTransport( "File", "lucky=yes" ); // ISO-POSIX file is the default transport
+                                                                  // Passing parameters to the transport
+            bpWriterSettings.SetParameters("have_metadata_file","yes" ); // Passing parameters to the engine
+            bpWriterSettings.SetParameters( "Aggregation", (nproc+1)/2 ); // number of aggregators
         }
 
         //Open returns a smart pointer to Engine containing the Derived class Writer
@@ -68,21 +79,27 @@ int main( int argc, char* argv [] )
         if( bpWriter == nullptr )
             throw std::ios_base::failure( "ERROR: failed to open ADIOS bpWriter\n" );
 
-        if (rank == 0)
+        if( rank == 0 )
         {
+            // Writing a global scalar from only one process
             bpWriter->Write<unsigned int>( varNX, &Nx );
         }
+        // Writing a local scalar on every process. Will be shown at reading as a 1D array
         bpWriter->Write<int>( varNparts, &Nparts );
+
+        // Writing a global scalar on every process is useless. Information will be thrown away
+        // and only rank 0's data will be in the output
+        bpWriter->Write<int>( varNproc, &nproc );
 
         // Make a 1D selection to describe the local dimensions of the variable we write and
         // its offsets in the global spaces
         adios::Selection& sel = adios.SelectionBoundingBox( {Nx}, {rank*Nx} ); // local dims and offsets; both as list
-        NiceVar.SetSelection( sel );
-        bpWriter->Write<double>( varMyDoubles, NiceVar.data() ); // Base class Engine own the Write<T> that will call overloaded Write from Derived
+        NiceArray.SetSelection( sel );
+        bpWriter->Write<double>( varNice, NiceArray.data() ); // Base class Engine own the Write<T> that will call overloaded Write from Derived
 
-        adios::Selection& lsel = adios.SelectionBoundingBox( {Nx} ); // 0 offsets assumed
-        RaggedVar.SetSelection( sel );
-        bpWriter->Write<float>( varMyFloats, RaggedVar.data() ); // Base class Engine own the Write<T> that will call overloaded Write from Derived
+        adios::Selection& lsel = adios.SelectionBoundingBox( {1,Nparts}, {rank,0} );
+        RaggedArray.SetSelection( sel );
+        bpWriter->Write<float>( varRagged, RaggedArray.data() ); // Base class Engine own the Write<T> that will call overloaded Write from Derived
 
         // Indicate we are done for this step
         // N-to-M Aggregation, disk I/O will be performed during this call, unless
