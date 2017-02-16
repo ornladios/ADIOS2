39351d0ce9b8911314d8f195cc3eb23953def58e
Norbert Podhorszki <pnorbert@ornl.gov>
2017-02-16 14:02:24 -0500

    groupless writer updated to no-local-array concept

--- examples/groupless/writer.cpp
+++ examples/groupless/writer.cpp
@@ -23,21 +23,32 @@ int main( int argc, char* argv [] )
     adios::ADIOS adios( MPI_COMM_WORLD, adiosDebug );
 
     //Application variable
-    std::vector<double> myDoubles = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-    std::vector<float> myFloats = { 0, -1, -2, -3, -4, -5, -6, -7, -8, -9 };
-    const unsigned int Nx = static_cast<unsigned int>( myDoubles.size() );
+    const unsigned int Nx = 10;
+    const int Nparts = rand()%6 + 5; // random size per process, 5..10 each
+
+    std::vector<double> NiceVar( Nx );
+    for (int i=0; i < NX; i++)
+    {
+        NiceVar[i] = rank*NX + (double)i;
+    }
+    std::vector<float> RaggedVar( Nparts );
+    for (int i=0; i < Nparts; i++)
+    {
+        RaggedVar[i] = rank*NX + (float)i;
+    }
+
 
     try
     {
         //Define group and variables with transforms, variables don't have functions, only group can access variables
-        adios::Variable<int>&    varNX        = adios.DefineVariable<unsigned int>( "NX" ); // global scalar value
-        adios::Variable<int>&    varRank      = adios.DefineVariable<int>( "rank" ); // scalar value different on every process
-        adios::Variable<double>& varMyDoubles = adios.DefineVariable<double>( "D", {nproc*Nx} ); // 1D global array
-        adios::Variable<float>&  varMyFloats  = adios.DefineVariable<float>( "F" ); // local array
+        adios::Variable<int>&    varNX      = adios.DefineVariable<unsigned int>( "NX" ); // global scalar value
+        adios::Variable<int>&    varNparts  = adios.DefineVariable<int>( "Nparts" ); // scalar value different on every process
+        adios::Variable<double>& varNice    = adios.DefineVariable<double>( "D", {nproc*Nx} ); // 1D global array
+        adios::Variable<float>&  varRagged  = adios.DefineVariable<float>( "F", {nproc,adios::VARYING_DIMENSION} ); // local array
 
         //add transform to variable in group...not executed (just testing API)
         adios::Transform bzip2 = adios::transform::BZIP2( );
-        varMyDoubles.AddTransform( bzip2, 1 );
+        NiceVar.AddTransform( bzip2, 1 );
 
         //Define method for engine creation
         // 1. Get method def from config file or define new one
@@ -61,21 +72,22 @@ int main( int argc, char* argv [] )
         {
             bpWriter->Write<unsigned int>( varNX, &Nx );
         }
-        bpWriter->Write<int>( varRank, &rank );
+        bpWriter->Write<int>( varNparts, &Nparts );
 
         // Make a 1D selection to describe the local dimensions of the variable we write and
         // its offsets in the global spaces
         adios::Selection& sel = adios.SelectionBoundingBox( {Nx}, {rank*Nx} ); // local dims and offsets; both as list
-        varMyDoubles.SetSelection( sel );
-        bpWriter->Write<double>( varMyDoubles, myDoubles.data() ); // Base class Engine own the Write<T> that will call overloaded Write from Derived
+        NiceVar.SetSelection( sel );
+        bpWriter->Write<double>( varMyDoubles, NiceVar.data() ); // Base class Engine own the Write<T> that will call overloaded Write from Derived
 
         adios::Selection& lsel = adios.SelectionBoundingBox( {Nx} ); // 0 offsets assumed
-        bpWriter->Write<float>( varMyFloats, myFloats.data() ); // Base class Engine own the Write<T> that will call overloaded Write from Derived
+        RaggedVar.SetSelection( sel );
+        bpWriter->Write<float>( varMyFloats, RaggedVar.data() ); // Base class Engine own the Write<T> that will call overloaded Write from Derived
 
         // Indicate we are done for this step
         // N-to-M Aggregation, disk I/O will be performed during this call, unless
         // time aggregation postpones all of that to some later step
-        bpWriter->AdvanceStep( );
+        bpWriter->Advance( );
 
         // Called once: indicate that we are done with this output for the run
         bpWriter->Close( );
