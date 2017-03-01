ead16b8f394e553b9d40cc97cf7e5520af3728a1
wfg <wfg@pc0098504.ornl.gov>
2017-03-01 15:50:26 -0500

    Added BP1Aggregator in format

---
+++ include/format/BP1Aggregator.h
@@ -0,0 +1,35 @@
+/*
+ * BP1Aggregator.h
+ *
+ *  Created on: Mar 1, 2017
+ *      Author: wfg
+ */
+
+#ifndef BP1AGGREGATOR_H_
+#define BP1AGGREGATOR_H_
+
+
+
+namespace adios
+{
+namespace format
+{
+
+class BP1Aggregator
+{
+
+
+
+
+
+
+
+
+
+};
+
+
+} //end namespace format
+} //end namespace adios
+
+#endif /* BP1AGGREGATOR_H_ */
