8f73147e21e82eb1518dd8658de5107ac8c02d19
Chuck Atkins <chuck.atkins@kitware.com>
2017-03-30 17:13:25 -0400

    Fix reference assignment
    Since m_ADIOS is a reference type then it can't be called with an explicit
    constructor

--- source/core/Engine.cpp
+++ source/core/Engine.cpp
@@ -20,7 +20,7 @@ Engine::Engine(ADIOS &adios, const std::string engineType,
                MPI_Comm mpiComm, const Method &method, const bool debugMode,
                const unsigned int nthreads, const std::string endMessage)
     : m_MPIComm{mpiComm}, m_EngineType{engineType}, m_Name{name},
-      m_AccessMode{accessMode}, m_Method{method}, m_ADIOS{adios},
+      m_AccessMode{accessMode}, m_Method{method}, m_ADIOS(adios),
       m_DebugMode{debugMode}, m_nThreads{nthreads}, m_EndMessage(endMessage)
 {
   if (m_DebugMode == true)
