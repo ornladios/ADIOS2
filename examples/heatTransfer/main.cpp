e82d6b5d8bc67f9ce9db1dceb338fa502cbd4ed6
Norbert Podhorszki <pnorbert@ornl.gov>
2017-04-03 17:23:17 -0400

    Added read (adios 1.x only) to heat transfer example. Does not print result yet.

--- examples/heatTransfer/main.cpp
+++ examples/heatTransfer/main.cpp
@@ -65,10 +65,10 @@ int main(int argc, char *argv[])
     IO io(settings, mpiHeatTransferComm);
 
     ht.init(true);
-    ht.printT("Initialized T:", mpiHeatTransferComm);
+    //ht.printT("Initialized T:", mpiHeatTransferComm);
     ht.heatEdges();
-    // ht.exchange( mpiHeatTransferComm );
-    ht.printT("Heated T:", mpiHeatTransferComm);
+    ht.exchange( mpiHeatTransferComm );
+    //ht.printT("Heated T:", mpiHeatTransferComm);
     io.write(0, ht, settings, mpiHeatTransferComm);
 
     for (unsigned int t = 1; t <= settings.steps; ++t)
