bca9f3994d0d185aa1be1c68e8a026ad383bd96c
Chuck Atkins <chuck.atkins@kitware.com>
2017-04-25 14:00:59 -0400

    Simplify test source

--- testing/adios2/engine/adios1/TestADIOS1WriteRead.cpp
+++ testing/adios2/engine/adios1/TestADIOS1WriteRead.cpp
@@ -7,45 +7,250 @@
 
 #include <gtest/gtest.h>
 
-#include "../EngineWriteReadTest.h"
+#include "../SmallTestData.h"
 
-class ADIOS1WriteRead1DTest : public EngineWriteRead1DTest
+class ADIOS1WriteReadTest : public ::testing::Test
 {
 public:
-    ADIOS1WriteRead1DTest()
-    : EngineWriteRead1DTest("ADIOS1Writer", "ADIOS1WriteRead1DTest.bp")
-    {
-    }
+    ADIOS1WriteReadTest() = default;
+
+    SmallTestData m_TestData;
 };
 
-TEST_F(ADIOS1WriteRead1DTest, WriteReadTest) { WriteRead(); }
+//******************************************************************************
+// 1D 1x8 test data
+//******************************************************************************
+TEST_F(ADIOS1WriteReadTest, ADIOS2ADIOS2WriteADIOS1Read1D8)
+{
+    {
+        adios::ADIOS adios(adios::Verbose::WARN, true);
+
+        // Declare 1D variables
+        {
+            auto &var_i8 = adios.DefineVariable<char>("i8", adios::Dims{8});
+            auto &var_i16 = adios.DefineVariable<short>("i16", adios::Dims{8});
+            auto &var_i32 = adios.DefineVariable<int>("i32", adios::Dims{8});
+            auto &var_i64 = adios.DefineVariable<long>("i64", adios::Dims{8});
+            auto &var_u8 =
+                adios.DefineVariable<unsigned char>("u8", adios::Dims{8});
+            auto &var_u16 =
+                adios.DefineVariable<unsigned short>("u16", adios::Dims{8});
+            auto &var_u32 =
+                adios.DefineVariable<unsigned int>("u32", adios::Dims{8});
+            auto &var_u64 =
+                adios.DefineVariable<unsigned long>("u64", adios::Dims{8});
+            auto &var_r32 = adios.DefineVariable<float>("r32", adios::Dims{8});
+            auto &var_r64 = adios.DefineVariable<double>("r64", adios::Dims{8});
+        }
+
+        // Create the ADIOS 1 Engine
+        auto method = adios.DeclareMethod("TestMethod");
+        method.SetEngine("ADIOS1Writer");
+        method.AddTransport("File");
+
+        auto engine =
+            adios.Open("ADIOS2ADIOS2WriteADIOS1Read1D8.bp", "w", method);
+        ASSERT_NE(engine, nullptr);
+
+        for (size_t step = 0; step < 3; ++step)
+        {
+            // Retrieve the variables that previously went out of scope
+            auto &var_i8 = adios.GetVariable<char>("i8");
+            auto &var_i16 = adios.GetVariable<short>("i16");
+            auto &var_i32 = adios.GetVariable<int>("i32");
+            auto &var_i64 = adios.GetVariable<long>("i64");
+            auto &var_u8 = adios.GetVariable<unsigned char>("u8");
+            auto &var_u16 = adios.GetVariable<unsigned short>("u16");
+            auto &var_u32 = adios.GetVariable<unsigned int>("u32");
+            auto &var_u64 = adios.GetVariable<unsigned long>("u64");
+            auto &var_r32 = adios.GetVariable<float>("r32");
+            auto &var_r64 = adios.GetVariable<double>("r64");
+
+            // Write each one
+            engine->Write(var_i8, m_TestData.I8.cbegin() + step);
+            engine->Write(var_i16, m_TestData.I16.cbegin() + step);
+            engine->Write(var_i32, m_TestData.I32.cbegin() + step);
+            engine->Write(var_i64, m_TestData.I64.cbegin() + step);
+            engine->Write(var_u8, m_TestData.U8.cbegin() + step);
+            engine->Write(var_u16, m_TestData.U16.cbegin() + step);
+            engine->Write(var_u32, m_TestData.U32.cbegin() + step);
+            engine->Write(var_u64, m_TestData.U64.cbegin() + step);
+            engine->Write(var_r32, m_TestData.R32.cbegin() + step);
+            engine->Write(var_r64, m_TestData.R64.cbegin() + step);
+
+            // Advance to the next time step
+            engine->Advance();
+        }
+
+        // Close the file
+        engine->Close();
+    }
+}
 
 //******************************************************************************
 // 2D 2x4 test data
 //******************************************************************************
-
-class ADIOS1WriteRead2D2x4Test : public EngineWriteRead2D2x4Test
+TEST_F(ADIOS1WriteReadTest, ADIOS2ADIOS2WriteADIOS1Read2D2x4)
 {
-public:
-    ADIOS1WriteRead2D2x4Test()
-    : EngineWriteRead2D2x4Test("ADIOS1Writer", "ADIOS1WriteRead2D2x4Test.bp")
     {
-    }
-};
+        adios::ADIOS adios(adios::Verbose::WARN, true);
 
-TEST_F(ADIOS1WriteRead2D2x4Test, WriteReadTest) { WriteRead(); }
+        // Declare 1D variables
+        {
+            auto &var_i8 = adios.DefineVariable<char>("i8", adios::Dims{2, 4});
+            auto &var_i16 =
+                adios.DefineVariable<short>("i16", adios::Dims{2, 4});
+            auto &var_i32 = adios.DefineVariable<int>("i32", adios::Dims{2, 4});
+            auto &var_i64 =
+                adios.DefineVariable<long>("i64", adios::Dims{2, 4});
+            auto &var_u8 =
+                adios.DefineVariable<unsigned char>("u8", adios::Dims{2, 4});
+            auto &var_u16 =
+                adios.DefineVariable<unsigned short>("u16", adios::Dims{2, 4});
+            auto &var_u32 =
+                adios.DefineVariable<unsigned int>("u32", adios::Dims{2, 4});
+            auto &var_u64 =
+                adios.DefineVariable<unsigned long>("u64", adios::Dims{2, 4});
+            auto &var_r32 =
+                adios.DefineVariable<float>("r32", adios::Dims{2, 4});
+            auto &var_r64 =
+                adios.DefineVariable<double>("r64", adios::Dims{2, 4});
+        }
+
+        // Create the ADIOS 1 Engine
+        auto method = adios.DeclareMethod("TestMethod");
+        method.SetEngine("ADIOS1Writer");
+        method.AddTransport("File");
+
+        auto engine =
+            adios.Open("ADIOS2ADIOS1WriteADIOS1Read2D2x4Test.bp", "w", method);
+        ASSERT_NE(engine, nullptr);
+
+        for (size_t step = 0; step < 3; ++step)
+        {
+            // Retrieve the variables that previously went out of scope
+            auto &var_i8 = adios.GetVariable<char>("i8");
+            auto &var_i16 = adios.GetVariable<short>("i16");
+            auto &var_i32 = adios.GetVariable<int>("i32");
+            auto &var_i64 = adios.GetVariable<long>("i64");
+            auto &var_u8 = adios.GetVariable<unsigned char>("u8");
+            auto &var_u16 = adios.GetVariable<unsigned short>("u16");
+            auto &var_u32 = adios.GetVariable<unsigned int>("u32");
+            auto &var_u64 = adios.GetVariable<unsigned long>("u64");
+            auto &var_r32 = adios.GetVariable<float>("r32");
+            auto &var_r64 = adios.GetVariable<double>("r64");
+
+            // Write each one
+            engine->Write(var_i8, m_TestData.I8.cbegin() + step);
+            engine->Write(var_i16, m_TestData.I16.cbegin() + step);
+            engine->Write(var_i32, m_TestData.I32.cbegin() + step);
+            engine->Write(var_i64, m_TestData.I64.cbegin() + step);
+            engine->Write(var_u8, m_TestData.U8.cbegin() + step);
+            engine->Write(var_u16, m_TestData.U16.cbegin() + step);
+            engine->Write(var_u32, m_TestData.U32.cbegin() + step);
+            engine->Write(var_u64, m_TestData.U64.cbegin() + step);
+            engine->Write(var_r32, m_TestData.R32.cbegin() + step);
+            engine->Write(var_r64, m_TestData.R64.cbegin() + step);
+
+            // Advance to the next time step
+            engine->Advance();
+        }
+
+        // Close the file
+        engine->Close();
+    }
+}
 
 //******************************************************************************
 // 2D 4x2 test data
 //******************************************************************************
-
-class ADIOS1WriteRead2D4x2Test : public EngineWriteRead2D4x2Test
+TEST_F(ADIOS1WriteReadTest, ADIOS2ADIOS2WriteADIOS1Read2D4x2)
 {
-public:
-    ADIOS1WriteRead2D4x2Test()
-    : EngineWriteRead2D4x2Test("ADIOS1Writer", "ADIOS1WriteRead2D4x2Test.bp")
     {
-    }
-};
+        adios::ADIOS adios(adios::Verbose::WARN, true);
 
-TEST_F(ADIOS1WriteRead2D4x2Test, WriteReadTest) { WriteRead(); }
+        // Declare 1D variables
+        {
+            auto &var_i8 = adios.DefineVariable<char>("i8", adios::Dims{4, 2});
+            auto &var_i16 =
+                adios.DefineVariable<short>("i16", adios::Dims{4, 2});
+            auto &var_i32 = adios.DefineVariable<int>("i32", adios::Dims{4, 2});
+            auto &var_i64 =
+                adios.DefineVariable<long>("i64", adios::Dims{4, 2});
+            auto &var_u8 =
+                adios.DefineVariable<unsigned char>("u8", adios::Dims{4, 2});
+            auto &var_u16 =
+                adios.DefineVariable<unsigned short>("u16", adios::Dims{4, 2});
+            auto &var_u32 =
+                adios.DefineVariable<unsigned int>("u32", adios::Dims{4, 2});
+            auto &var_u64 =
+                adios.DefineVariable<unsigned long>("u64", adios::Dims{4, 2});
+            auto &var_r32 =
+                adios.DefineVariable<float>("r32", adios::Dims{4, 2});
+            auto &var_r64 =
+                adios.DefineVariable<double>("r64", adios::Dims{4, 2});
+        }
+
+        // Create the ADIOS 1 Engine
+        auto method = adios.DeclareMethod("TestMethod");
+        method.SetEngine("ADIOS1Writer");
+        method.AddTransport("File");
+
+        auto engine =
+            adios.Open("ADIOS2ADIOS1WriteADIOS1Read2D4x2Test.bp", "w", method);
+        ASSERT_NE(engine, nullptr);
+
+        for (size_t step = 0; step < 3; ++step)
+        {
+            // Retrieve the variables that previously went out of scope
+            auto &var_i8 = adios.GetVariable<char>("i8");
+            auto &var_i16 = adios.GetVariable<short>("i16");
+            auto &var_i32 = adios.GetVariable<int>("i32");
+            auto &var_i64 = adios.GetVariable<long>("i64");
+            auto &var_u8 = adios.GetVariable<unsigned char>("u8");
+            auto &var_u16 = adios.GetVariable<unsigned short>("u16");
+            auto &var_u32 = adios.GetVariable<unsigned int>("u32");
+            auto &var_u64 = adios.GetVariable<unsigned long>("u64");
+            auto &var_r32 = adios.GetVariable<float>("r32");
+            auto &var_r64 = adios.GetVariable<double>("r64");
+
+            // Write each one
+            engine->Write(var_i8, m_TestData.I8.cbegin() + step);
+            engine->Write(var_i16, m_TestData.I16.cbegin() + step);
+            engine->Write(var_i32, m_TestData.I32.cbegin() + step);
+            engine->Write(var_i64, m_TestData.I64.cbegin() + step);
+            engine->Write(var_u8, m_TestData.U8.cbegin() + step);
+            engine->Write(var_u16, m_TestData.U16.cbegin() + step);
+            engine->Write(var_u32, m_TestData.U32.cbegin() + step);
+            engine->Write(var_u64, m_TestData.U64.cbegin() + step);
+            engine->Write(var_r32, m_TestData.R32.cbegin() + step);
+            engine->Write(var_r64, m_TestData.R64.cbegin() + step);
+
+            // Advance to the next time step
+            engine->Advance();
+        }
+
+        // Close the file
+        engine->Close();
+    }
+}
+
+//******************************************************************************
+// main
+//******************************************************************************
+
+int main(int argc, char **argv)
+{
+#ifdef ADIOS2_HAVE_MPI
+    MPI_Init(nullptr, nullptr);
+#endif
+
+    ::testing::InitGoogleTest(&argc, argv);
+    int result = RUN_ALL_TESTS();
+
+#ifdef ADIOS2_HAVE_MPI
+    MPI_Finalize();
+#endif
+
+    return result;
+}
--- testing/adios2/engine/bp/TestBPWriteRead.cpp
+++ testing/adios2/engine/bp/TestBPWriteRead.cpp
@@ -7,45 +7,250 @@
 
 #include <gtest/gtest.h>
 
-#include "../EngineWriteReadTest.h"
+#include "../SmallTestData.h"
 
-class BPWriteRead1DTest : public EngineWriteRead1DTest
+class BPWriteReadTest : public ::testing::Test
 {
 public:
-    BPWriteRead1DTest()
-    : EngineWriteRead1DTest("BPFileWriter", "BPWriteRead1DTest.bp")
-    {
-    }
+    BPWriteReadTest() = default;
+
+    SmallTestData m_TestData;
 };
 
-TEST_F(BPWriteRead1DTest, WriteReadTest) { WriteRead(); }
+//******************************************************************************
+// 1D 1x8 test data
+//******************************************************************************
+TEST_F(BPWriteReadTest, ADIOS2ADIOS2WriteADIOS1Read1D8)
+{
+    {
+        adios::ADIOS adios(adios::Verbose::WARN, true);
+
+        // Declare 1D variables
+        {
+            auto &var_i8 = adios.DefineVariable<char>("i8", adios::Dims{8});
+            auto &var_i16 = adios.DefineVariable<short>("i16", adios::Dims{8});
+            auto &var_i32 = adios.DefineVariable<int>("i32", adios::Dims{8});
+            auto &var_i64 = adios.DefineVariable<long>("i64", adios::Dims{8});
+            auto &var_u8 =
+                adios.DefineVariable<unsigned char>("u8", adios::Dims{8});
+            auto &var_u16 =
+                adios.DefineVariable<unsigned short>("u16", adios::Dims{8});
+            auto &var_u32 =
+                adios.DefineVariable<unsigned int>("u32", adios::Dims{8});
+            auto &var_u64 =
+                adios.DefineVariable<unsigned long>("u64", adios::Dims{8});
+            auto &var_r32 = adios.DefineVariable<float>("r32", adios::Dims{8});
+            auto &var_r64 = adios.DefineVariable<double>("r64", adios::Dims{8});
+        }
+
+        // Create the ADIOS 1 Engine
+        auto method = adios.DeclareMethod("TestMethod");
+        method.SetEngine("BPFileWriter");
+        method.AddTransport("File");
+
+        auto engine =
+            adios.Open("ADIOS2ADIOS2WriteADIOS1Read1D8.bp", "w", method);
+        ASSERT_NE(engine, nullptr);
+
+        for (size_t step = 0; step < 3; ++step)
+        {
+            // Retrieve the variables that previously went out of scope
+            auto &var_i8 = adios.GetVariable<char>("i8");
+            auto &var_i16 = adios.GetVariable<short>("i16");
+            auto &var_i32 = adios.GetVariable<int>("i32");
+            auto &var_i64 = adios.GetVariable<long>("i64");
+            auto &var_u8 = adios.GetVariable<unsigned char>("u8");
+            auto &var_u16 = adios.GetVariable<unsigned short>("u16");
+            auto &var_u32 = adios.GetVariable<unsigned int>("u32");
+            auto &var_u64 = adios.GetVariable<unsigned long>("u64");
+            auto &var_r32 = adios.GetVariable<float>("r32");
+            auto &var_r64 = adios.GetVariable<double>("r64");
+
+            // Write each one
+            engine->Write(var_i8, m_TestData.I8.cbegin() + step);
+            engine->Write(var_i16, m_TestData.I16.cbegin() + step);
+            engine->Write(var_i32, m_TestData.I32.cbegin() + step);
+            engine->Write(var_i64, m_TestData.I64.cbegin() + step);
+            engine->Write(var_u8, m_TestData.U8.cbegin() + step);
+            engine->Write(var_u16, m_TestData.U16.cbegin() + step);
+            engine->Write(var_u32, m_TestData.U32.cbegin() + step);
+            engine->Write(var_u64, m_TestData.U64.cbegin() + step);
+            engine->Write(var_r32, m_TestData.R32.cbegin() + step);
+            engine->Write(var_r64, m_TestData.R64.cbegin() + step);
+
+            // Advance to the next time step
+            engine->Advance();
+        }
+
+        // Close the file
+        engine->Close();
+    }
+}
 
 //******************************************************************************
 // 2D 2x4 test data
 //******************************************************************************
-
-class BPWriteRead2D2x4Test : public EngineWriteRead2D2x4Test
+TEST_F(BPWriteReadTest, ADIOS2ADIOS2WriteADIOS1Read2D2x4)
 {
-public:
-    BPWriteRead2D2x4Test()
-    : EngineWriteRead2D2x4Test("BPFileWriter", "BPWriteRead2D2x4Test.bp")
     {
-    }
-};
+        adios::ADIOS adios(adios::Verbose::WARN, true);
 
-TEST_F(BPWriteRead2D2x4Test, WriteReadTest) { WriteRead(); }
+        // Declare 1D variables
+        {
+            auto &var_i8 = adios.DefineVariable<char>("i8", adios::Dims{2, 4});
+            auto &var_i16 =
+                adios.DefineVariable<short>("i16", adios::Dims{2, 4});
+            auto &var_i32 = adios.DefineVariable<int>("i32", adios::Dims{2, 4});
+            auto &var_i64 =
+                adios.DefineVariable<long>("i64", adios::Dims{2, 4});
+            auto &var_u8 =
+                adios.DefineVariable<unsigned char>("u8", adios::Dims{2, 4});
+            auto &var_u16 =
+                adios.DefineVariable<unsigned short>("u16", adios::Dims{2, 4});
+            auto &var_u32 =
+                adios.DefineVariable<unsigned int>("u32", adios::Dims{2, 4});
+            auto &var_u64 =
+                adios.DefineVariable<unsigned long>("u64", adios::Dims{2, 4});
+            auto &var_r32 =
+                adios.DefineVariable<float>("r32", adios::Dims{2, 4});
+            auto &var_r64 =
+                adios.DefineVariable<double>("r64", adios::Dims{2, 4});
+        }
+
+        // Create the ADIOS 1 Engine
+        auto method = adios.DeclareMethod("TestMethod");
+        method.SetEngine("BPFileWriter");
+        method.AddTransport("File");
+
+        auto engine =
+            adios.Open("ADIOS2BPWriteADIOS1Read2D2x4Test.bp", "w", method);
+        ASSERT_NE(engine, nullptr);
+
+        for (size_t step = 0; step < 3; ++step)
+        {
+            // Retrieve the variables that previously went out of scope
+            auto &var_i8 = adios.GetVariable<char>("i8");
+            auto &var_i16 = adios.GetVariable<short>("i16");
+            auto &var_i32 = adios.GetVariable<int>("i32");
+            auto &var_i64 = adios.GetVariable<long>("i64");
+            auto &var_u8 = adios.GetVariable<unsigned char>("u8");
+            auto &var_u16 = adios.GetVariable<unsigned short>("u16");
+            auto &var_u32 = adios.GetVariable<unsigned int>("u32");
+            auto &var_u64 = adios.GetVariable<unsigned long>("u64");
+            auto &var_r32 = adios.GetVariable<float>("r32");
+            auto &var_r64 = adios.GetVariable<double>("r64");
+
+            // Write each one
+            engine->Write(var_i8, m_TestData.I8.cbegin() + step);
+            engine->Write(var_i16, m_TestData.I16.cbegin() + step);
+            engine->Write(var_i32, m_TestData.I32.cbegin() + step);
+            engine->Write(var_i64, m_TestData.I64.cbegin() + step);
+            engine->Write(var_u8, m_TestData.U8.cbegin() + step);
+            engine->Write(var_u16, m_TestData.U16.cbegin() + step);
+            engine->Write(var_u32, m_TestData.U32.cbegin() + step);
+            engine->Write(var_u64, m_TestData.U64.cbegin() + step);
+            engine->Write(var_r32, m_TestData.R32.cbegin() + step);
+            engine->Write(var_r64, m_TestData.R64.cbegin() + step);
+
+            // Advance to the next time step
+            engine->Advance();
+        }
+
+        // Close the file
+        engine->Close();
+    }
+}
 
 //******************************************************************************
 // 2D 4x2 test data
 //******************************************************************************
-
-class BPWriteRead2D4x2Test : public EngineWriteRead2D4x2Test
+TEST_F(BPWriteReadTest, ADIOS2ADIOS2WriteADIOS1Read2D4x2)
 {
-public:
-    BPWriteRead2D4x2Test()
-    : EngineWriteRead2D4x2Test("BPFileWriter", "BPWriteRead2D4x2Test.bp")
     {
-    }
-};
+        adios::ADIOS adios(adios::Verbose::WARN, true);
 
-TEST_F(BPWriteRead2D4x2Test, WriteReadTest) { WriteRead(); }
+        // Declare 1D variables
+        {
+            auto &var_i8 = adios.DefineVariable<char>("i8", adios::Dims{4, 2});
+            auto &var_i16 =
+                adios.DefineVariable<short>("i16", adios::Dims{4, 2});
+            auto &var_i32 = adios.DefineVariable<int>("i32", adios::Dims{4, 2});
+            auto &var_i64 =
+                adios.DefineVariable<long>("i64", adios::Dims{4, 2});
+            auto &var_u8 =
+                adios.DefineVariable<unsigned char>("u8", adios::Dims{4, 2});
+            auto &var_u16 =
+                adios.DefineVariable<unsigned short>("u16", adios::Dims{4, 2});
+            auto &var_u32 =
+                adios.DefineVariable<unsigned int>("u32", adios::Dims{4, 2});
+            auto &var_u64 =
+                adios.DefineVariable<unsigned long>("u64", adios::Dims{4, 2});
+            auto &var_r32 =
+                adios.DefineVariable<float>("r32", adios::Dims{4, 2});
+            auto &var_r64 =
+                adios.DefineVariable<double>("r64", adios::Dims{4, 2});
+        }
+
+        // Create the ADIOS 1 Engine
+        auto method = adios.DeclareMethod("TestMethod");
+        method.SetEngine("BPFileWriter");
+        method.AddTransport("File");
+
+        auto engine =
+            adios.Open("ADIOS2BPWriteADIOS1Read2D4x2Test.bp", "w", method);
+        ASSERT_NE(engine, nullptr);
+
+        for (size_t step = 0; step < 3; ++step)
+        {
+            // Retrieve the variables that previously went out of scope
+            auto &var_i8 = adios.GetVariable<char>("i8");
+            auto &var_i16 = adios.GetVariable<short>("i16");
+            auto &var_i32 = adios.GetVariable<int>("i32");
+            auto &var_i64 = adios.GetVariable<long>("i64");
+            auto &var_u8 = adios.GetVariable<unsigned char>("u8");
+            auto &var_u16 = adios.GetVariable<unsigned short>("u16");
+            auto &var_u32 = adios.GetVariable<unsigned int>("u32");
+            auto &var_u64 = adios.GetVariable<unsigned long>("u64");
+            auto &var_r32 = adios.GetVariable<float>("r32");
+            auto &var_r64 = adios.GetVariable<double>("r64");
+
+            // Write each one
+            engine->Write(var_i8, m_TestData.I8.cbegin() + step);
+            engine->Write(var_i16, m_TestData.I16.cbegin() + step);
+            engine->Write(var_i32, m_TestData.I32.cbegin() + step);
+            engine->Write(var_i64, m_TestData.I64.cbegin() + step);
+            engine->Write(var_u8, m_TestData.U8.cbegin() + step);
+            engine->Write(var_u16, m_TestData.U16.cbegin() + step);
+            engine->Write(var_u32, m_TestData.U32.cbegin() + step);
+            engine->Write(var_u64, m_TestData.U64.cbegin() + step);
+            engine->Write(var_r32, m_TestData.R32.cbegin() + step);
+            engine->Write(var_r64, m_TestData.R64.cbegin() + step);
+
+            // Advance to the next time step
+            engine->Advance();
+        }
+
+        // Close the file
+        engine->Close();
+    }
+}
+
+//******************************************************************************
+// main
+//******************************************************************************
+
+int main(int argc, char **argv)
+{
+#ifdef ADIOS2_HAVE_MPI
+    MPI_Init(nullptr, nullptr);
+#endif
+
+    ::testing::InitGoogleTest(&argc, argv);
+    int result = RUN_ALL_TESTS();
+
+#ifdef ADIOS2_HAVE_MPI
+    MPI_Finalize();
+#endif
+
+    return result;
+}
