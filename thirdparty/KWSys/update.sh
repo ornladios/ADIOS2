7788d560d6baba8ecaf64b06962d538fea13b6da
Chuck Atkins <chuck.atkins@kitware.com>
2017-06-22 12:08:16 -0400

    Point KWSys to a patched branch

--- thirdparty/KWSys/update.sh
+++ thirdparty/KWSys/update.sh
@@ -8,19 +8,25 @@ readonly name="KWSys"
 readonly ownership="KWSys Upstream <kwrobot@kitware.com>"
 readonly subtree="thirdparty/KWSys/adios2sys"
 #readonly repo="https://gitlab.kitware.com/utils/kwsys.git"
-#readonly tag="0c4e58ec"
+#readonly tag="master"
 
-# This commit contains a patch so suppress noisy warnings.  Use 'master' off
-# the main repo instead once its been merged
+# This commit contains a patch to allow object libraries for kwsys.
+# Use 'master' off the main repo instead once its been merged
 readonly repo="https://gitlab.kitware.com/chuck.atkins/kwsys.git"
-readonly tag="0c4e58ec"
+readonly tag="ab645c9"
 
 readonly shortlog="true"
 readonly paths="
 "
 
+git_archive_no_attributes () {
+    find . -name gitattributes | xargs git rm -f
+    git archive --prefix="$name-reduced/" HEAD -- $paths | \
+        tar -C "$extractdir" -x
+}
+
 extract_source () {
-    git_archive
+    git_archive_no_attributes
 }
 
 . "${BASH_SOURCE%/*}/../update-common.sh"
