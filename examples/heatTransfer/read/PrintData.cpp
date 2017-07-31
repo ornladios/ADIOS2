e51ced3e81aec4155207fc6355e15d0f19377137
Ruonan Wang <jason.ruonan.wang@gmail.com>
2017-07-31 14:46:19 -0400

    overloaded printData() in heatTransfer read example to accept both size_t and uint64_t

--- examples/heatTransfer/read/PrintData.cpp
+++ examples/heatTransfer/read/PrintData.cpp
@@ -52,3 +52,42 @@ void printData(double *xy, size_t *size, size_t *offset, int rank, int steps)
     }
     myfile.close();
 }
+
+void printData(double *xy, uint64_t *size, uint64_t *offset, int rank,
+               int steps)
+{
+    std::ofstream myfile;
+    std::string filename = "data." + std::to_string(rank);
+    myfile.open(filename);
+    double *data = xy;
+    uint64_t nelems = size[0] * size[1];
+    for (int step = 0; step < steps; step++)
+    {
+        myfile << "rank=" << rank << " size=" << size[0] << "x" << size[1]
+               << " offsets=" << offset[0] << ":" << offset[1]
+               << " step=" << step << std::endl;
+
+        myfile << " time   row   columns " << offset[1] << "..."
+               << offset[1] + size[1] - 1 << std::endl;
+        myfile << "        ";
+        for (int j = 0; j < size[1]; j++)
+        {
+            myfile << std::setw(9) << offset[1] + j;
+        }
+        myfile << std::endl;
+        myfile << "------------------------------------------------------------"
+                  "--\n";
+        for (int i = 0; i < size[0]; i++)
+        {
+            myfile << std::setw(5) << step << std::setw(5) << offset[0] + i;
+            for (int j = 0; j < size[1]; j++)
+            {
+                myfile << std::setw(9) << std::setprecision(2)
+                       << data[i * size[1] + j];
+            }
+            myfile << std::endl;
+        }
+        data += nelems;
+    }
+    myfile.close();
+}
