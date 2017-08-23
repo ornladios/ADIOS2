9ddb135f2bcb1198772c8b560bac3bb6bcba2ad7
Chuck Atkins <chuck.atkins@kitware.com>
2017-08-23 11:56:19 -0400

    Testing: Use fixed width integer types instead of natural types

--- testing/adios2/engine/SmallTestData.h
+++ testing/adios2/engine/SmallTestData.h
@@ -5,6 +5,8 @@
 #ifndef TESTING_ADIOS2_ENGINE_SMALLTESTDATA_H_
 #define TESTING_ADIOS2_ENGINE_SMALLTESTDATA_H_
 
+#include <cstdint>
+
 #include <array>
 #include <limits>
 
@@ -17,23 +19,22 @@
 struct SmallTestData
 {
     // TODO: Fix the right initial value for char array
-    std::array<char, 10> I8 = {{0, 1, -2, 3, -4, 5, -6, 7, -8, 9}};
-    std::array<signed char, 10> SI8 = {{0, -1, 2, -3, 4, -5, 6, -7, 8, -9}};
-    std::array<short, 10> I16 = {
+    std::array<int8_t, 10> I8 = {{0, 1, -2, 3, -4, 5, -6, 7, -8, 9}};
+    std::array<int16_t, 10> I16 = {
         {512, 513, -510, 515, -508, 517, -506, 519, -504, 521}};
-    std::array<int, 10> I32 = {{131072, 131073, -131070, 131075, -131068,
+    std::array<int32_t, 10> I32 = {{131072, 131073, -131070, 131075, -131068,
                                 131077, -131066, 131079, -131064, 131081}};
-    std::array<long int, 10> I64 = {
+    std::array<int64_t, 10> I64 = {
         {8589934592, 8589934593, -8589934590, 8589934595, -8589934588,
          8589934597, -8589934586, 8589934599, -8589934584, 8589934601}};
-    std::array<unsigned char, 10> U8 = {
+    std::array<uint8_t, 10> U8 = {
         {128, 129, 130, 131, 132, 133, 134, 135, 136, 137}};
-    std::array<unsigned short, 10> U16 = {
+    std::array<uint16_t, 10> U16 = {
         {32768, 32769, 32770, 32771, 32772, 32773, 32774, 32775, 32776, 32777}};
-    std::array<unsigned int, 10> U32 = {
+    std::array<uint32_t, 10> U32 = {
         {2147483648, 2147483649, 2147483650, 2147483651, 2147483652, 2147483653,
          2147483654, 2147483655, 2147483656, 2147483657}};
-    std::array<unsigned long int, 10> U64 = {
+    std::array<uint64_t, 10> U64 = {
         {9223372036854775808UL, 9223372036854775809UL, 9223372036854775810UL,
          9223372036854775811UL, 9223372036854775812UL, 9223372036854775813UL,
          9223372036854775814UL, 9223372036854775815UL, 9223372036854775816UL,
--- testing/adios2/engine/hdf5/TestHDF5WriteRead.cpp
+++ testing/adios2/engine/hdf5/TestHDF5WriteRead.cpp
@@ -196,20 +196,20 @@ TEST_F(HDF5WriteReadTest, ADIOS2HDF5WriteHDF5Read1D8)
         // Declare 1D variables
         {
             auto &var_i8 =
-                io.DefineVariable<char>("i8", {}, {}, adios2::Dims{8});
+                io.DefineVariable<int8_t>("i8", {}, {}, adios2::Dims{8});
             auto &var_i16 =
-                io.DefineVariable<short>("i16", {}, {}, adios2::Dims{8});
+                io.DefineVariable<int16_t>("i16", {}, {}, adios2::Dims{8});
             auto &var_i32 =
-                io.DefineVariable<int>("i32", {}, {}, adios2::Dims{8});
+                io.DefineVariable<int32_t>("i32", {}, {}, adios2::Dims{8});
             auto &var_i64 =
-                io.DefineVariable<long>("i64", {}, {}, adios2::Dims{8});
+                io.DefineVariable<int64_t>("i64", {}, {}, adios2::Dims{8});
             auto &var_u8 =
-                io.DefineVariable<unsigned char>("u8", {}, {}, adios2::Dims{8});
-            auto &var_u16 = io.DefineVariable<unsigned short>("u16", {}, {},
+                io.DefineVariable<uint8_t>("u8", {}, {}, adios2::Dims{8});
+            auto &var_u16 = io.DefineVariable<uint16_t>("u16", {}, {},
                                                               adios2::Dims{8});
             auto &var_u32 =
-                io.DefineVariable<unsigned int>("u32", {}, {}, adios2::Dims{8});
-            auto &var_u64 = io.DefineVariable<unsigned long>("u64", {}, {},
+                io.DefineVariable<uint32_t>("u32", {}, {}, adios2::Dims{8});
+            auto &var_u64 = io.DefineVariable<uint64_t>("u64", {}, {},
                                                              adios2::Dims{8});
             auto &var_r32 =
                 io.DefineVariable<float>("r32", {}, {}, adios2::Dims{8});
@@ -226,14 +226,14 @@ TEST_F(HDF5WriteReadTest, ADIOS2HDF5WriteHDF5Read1D8)
         for (size_t step = 0; step < 3; ++step)
         {
             // Retrieve the variables that previously went out of scope
-            auto &var_i8 = io.GetVariable<char>("i8");
-            auto &var_i16 = io.GetVariable<short>("i16");
-            auto &var_i32 = io.GetVariable<int>("i32");
-            auto &var_i64 = io.GetVariable<long>("i64");
-            auto &var_u8 = io.GetVariable<unsigned char>("u8");
-            auto &var_u16 = io.GetVariable<unsigned short>("u16");
-            auto &var_u32 = io.GetVariable<unsigned int>("u32");
-            auto &var_u64 = io.GetVariable<unsigned long>("u64");
+            auto &var_i8 = io.GetVariable<int8_t>("i8");
+            auto &var_i16 = io.GetVariable<int16_t>("i16");
+            auto &var_i32 = io.GetVariable<int32_t>("i32");
+            auto &var_i64 = io.GetVariable<int64_t>("i64");
+            auto &var_u8 = io.GetVariable<uint8_t>("u8");
+            auto &var_u16 = io.GetVariable<uint16_t>("u16");
+            auto &var_u32 = io.GetVariable<uint32_t>("u32");
+            auto &var_u64 = io.GetVariable<uint64_t>("u64");
             auto &var_r32 = io.GetVariable<float>("r32");
             auto &var_r64 = io.GetVariable<double>("r64");
 
@@ -265,11 +265,11 @@ TEST_F(HDF5WriteReadTest, ADIOS2HDF5WriteHDF5Read1D8)
     if (rank == 0)
 #endif
     {
-        std::array<char, 8> I8;
+        std::array<int8_t, 8> I8;
         std::array<int16_t, 8> I16;
         std::array<int32_t, 8> I32;
         std::array<int64_t, 8> I64;
-        std::array<unsigned char, 8> U8;
+        std::array<uint8_t, 8> U8;
         std::array<uint16_t, 8> U16;
         std::array<uint32_t, 8> U32;
         std::array<uint64_t, 8> U64;
@@ -402,20 +402,20 @@ TEST_F(HDF5WriteReadTest, ADIOS2HDF5WriteHDF5Read2D2x4)
         // Declare 1D variables
         {
             auto &var_i8 =
-                io.DefineVariable<char>("i8", {}, {}, adios2::Dims{2, 4});
+                io.DefineVariable<int8_t>("i8", {}, {}, adios2::Dims{2, 4});
             auto &var_i16 =
-                io.DefineVariable<short>("i16", {}, {}, adios2::Dims{2, 4});
+                io.DefineVariable<int16_t>("i16", {}, {}, adios2::Dims{2, 4});
             auto &var_i32 =
-                io.DefineVariable<int>("i32", {}, {}, adios2::Dims{2, 4});
+                io.DefineVariable<int32_t>("i32", {}, {}, adios2::Dims{2, 4});
             auto &var_i64 =
-                io.DefineVariable<long>("i64", {}, {}, adios2::Dims{2, 4});
-            auto &var_u8 = io.DefineVariable<unsigned char>("u8", {}, {},
+                io.DefineVariable<int64_t>("i64", {}, {}, adios2::Dims{2, 4});
+            auto &var_u8 = io.DefineVariable<uint8_t>("u8", {}, {},
                                                             adios2::Dims{2, 4});
-            auto &var_u16 = io.DefineVariable<unsigned short>(
+            auto &var_u16 = io.DefineVariable<uint16_t>(
                 "u16", {}, {}, adios2::Dims{2, 4});
-            auto &var_u32 = io.DefineVariable<unsigned int>("u32", {}, {},
+            auto &var_u32 = io.DefineVariable<uint32_t>("u32", {}, {},
                                                             adios2::Dims{2, 4});
-            auto &var_u64 = io.DefineVariable<unsigned long>(
+            auto &var_u64 = io.DefineVariable<uint64_t>(
                 "u64", {}, {}, adios2::Dims{2, 4});
             auto &var_r32 =
                 io.DefineVariable<float>("r32", {}, {}, adios2::Dims{2, 4});
@@ -433,14 +433,14 @@ TEST_F(HDF5WriteReadTest, ADIOS2HDF5WriteHDF5Read2D2x4)
         for (size_t step = 0; step < 3; ++step)
         {
             // Retrieve the variables that previously went out of scope
-            auto &var_i8 = io.GetVariable<char>("i8");
-            auto &var_i16 = io.GetVariable<short>("i16");
-            auto &var_i32 = io.GetVariable<int>("i32");
-            auto &var_i64 = io.GetVariable<long>("i64");
-            auto &var_u8 = io.GetVariable<unsigned char>("u8");
-            auto &var_u16 = io.GetVariable<unsigned short>("u16");
-            auto &var_u32 = io.GetVariable<unsigned int>("u32");
-            auto &var_u64 = io.GetVariable<unsigned long>("u64");
+            auto &var_i8 = io.GetVariable<int8_t>("i8");
+            auto &var_i16 = io.GetVariable<int16_t>("i16");
+            auto &var_i32 = io.GetVariable<int32_t>("i32");
+            auto &var_i64 = io.GetVariable<int64_t>("i64");
+            auto &var_u8 = io.GetVariable<uint8_t>("u8");
+            auto &var_u16 = io.GetVariable<uint16_t>("u16");
+            auto &var_u32 = io.GetVariable<uint32_t>("u32");
+            auto &var_u64 = io.GetVariable<uint64_t>("u64");
             auto &var_r32 = io.GetVariable<float>("r32");
             auto &var_r64 = io.GetVariable<double>("r64");
 
@@ -474,11 +474,11 @@ TEST_F(HDF5WriteReadTest, ADIOS2HDF5WriteHDF5Read2D2x4)
     {
         HDF5NativeReader hdf5Reader(fname);
 
-        std::array<char, 8> I8;
+        std::array<int8_t, 8> I8;
         std::array<int16_t, 8> I16;
         std::array<int32_t, 8> I32;
         std::array<int64_t, 8> I64;
-        std::array<unsigned char, 8> U8;
+        std::array<uint8_t, 8> U8;
         std::array<uint16_t, 8> U16;
         std::array<uint32_t, 8> U32;
         std::array<uint64_t, 8> U64;
@@ -617,20 +617,20 @@ TEST_F(HDF5WriteReadTest, ADIOS2HDF5WriteHDF5Read2D4x2)
         // Declare 1D variables
         {
             auto &var_i8 =
-                io.DefineVariable<char>("i8", {}, {}, adios2::Dims{4, 2});
+                io.DefineVariable<int8_t>("i8", {}, {}, adios2::Dims{4, 2});
             auto &var_i16 =
-                io.DefineVariable<short>("i16", {}, {}, adios2::Dims{4, 2});
+                io.DefineVariable<int16_t>("i16", {}, {}, adios2::Dims{4, 2});
             auto &var_i32 =
-                io.DefineVariable<int>("i32", {}, {}, adios2::Dims{4, 2});
+                io.DefineVariable<int32_t>("i32", {}, {}, adios2::Dims{4, 2});
             auto &var_i64 =
-                io.DefineVariable<long>("i64", {}, {}, adios2::Dims{4, 2});
-            auto &var_u8 = io.DefineVariable<unsigned char>("u8", {}, {},
+                io.DefineVariable<int64_t>("i64", {}, {}, adios2::Dims{4, 2});
+            auto &var_u8 = io.DefineVariable<uint8_t>("u8", {}, {},
                                                             adios2::Dims{4, 2});
-            auto &var_u16 = io.DefineVariable<unsigned short>(
+            auto &var_u16 = io.DefineVariable<uint16_t>(
                 "u16", {}, {}, adios2::Dims{4, 2});
-            auto &var_u32 = io.DefineVariable<unsigned int>("u32", {}, {},
+            auto &var_u32 = io.DefineVariable<uint32_t>("u32", {}, {},
                                                             adios2::Dims{4, 2});
-            auto &var_u64 = io.DefineVariable<unsigned long>(
+            auto &var_u64 = io.DefineVariable<uint64_t>(
                 "u64", {}, {}, adios2::Dims{4, 2});
             auto &var_r32 =
                 io.DefineVariable<float>("r32", {}, {}, adios2::Dims{4, 2});
@@ -648,14 +648,14 @@ TEST_F(HDF5WriteReadTest, ADIOS2HDF5WriteHDF5Read2D4x2)
         for (size_t step = 0; step < 3; ++step)
         {
             // Retrieve the variables that previously went out of scope
-            auto &var_i8 = io.GetVariable<char>("i8");
-            auto &var_i16 = io.GetVariable<short>("i16");
-            auto &var_i32 = io.GetVariable<int>("i32");
-            auto &var_i64 = io.GetVariable<long>("i64");
-            auto &var_u8 = io.GetVariable<unsigned char>("u8");
-            auto &var_u16 = io.GetVariable<unsigned short>("u16");
-            auto &var_u32 = io.GetVariable<unsigned int>("u32");
-            auto &var_u64 = io.GetVariable<unsigned long>("u64");
+            auto &var_i8 = io.GetVariable<int8_t>("i8");
+            auto &var_i16 = io.GetVariable<int16_t>("i16");
+            auto &var_i32 = io.GetVariable<int32_t>("i32");
+            auto &var_i64 = io.GetVariable<int64_t>("i64");
+            auto &var_u8 = io.GetVariable<uint8_t>("u8");
+            auto &var_u16 = io.GetVariable<uint16_t>("u16");
+            auto &var_u32 = io.GetVariable<uint32_t>("u32");
+            auto &var_u64 = io.GetVariable<uint64_t>("u64");
             auto &var_r32 = io.GetVariable<float>("r32");
             auto &var_r64 = io.GetVariable<double>("r64");
 
@@ -690,11 +690,11 @@ TEST_F(HDF5WriteReadTest, ADIOS2HDF5WriteHDF5Read2D4x2)
 
         HDF5NativeReader hdf5Reader(fname);
 
-        std::array<char, 8> I8;
+        std::array<int8_t, 8> I8;
         std::array<int16_t, 8> I16;
         std::array<int32_t, 8> I32;
         std::array<int64_t, 8> I64;
-        std::array<unsigned char, 8> U8;
+        std::array<uint8_t, 8> U8;
         std::array<uint16_t, 8> U16;
         std::array<uint32_t, 8> U32;
         std::array<uint64_t, 8> U64;
