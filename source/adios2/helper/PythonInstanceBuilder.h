197799a97a3c66ce805c700bff302ab5a9b3ef70
Scott Wittenburg <scott.wittenburg@kitware.com>
2017-10-06 12:49:37 -0600

    Add a python interpreter, an instance helper, and a new engine type

---
+++ source/adios2/helper/PythonInstanceBuilder.h
@@ -0,0 +1,58 @@
+/*
+ * Distributed under the OSI-approved Apache License, Version 2.0.  See
+ * accompanying file Copyright.txt for details.
+ *
+ * PythonInstanceBuilder.h Instantiate some arbitrary python class
+ *
+ *  Created on: Oct 02, 2017
+ *      Author: Scott Wittenburg <scott.wittenburg@kitware.com>
+ */
+
+#ifndef ADIOS2_HELPER_PYTHONINSTANCEBUILDER_H_
+#define ADIOS2_HELPER_PYTHONINSTANCEBUILDER_H_
+
+#include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+
+namespace adios2
+{
+namespace PythonInstanceBuilder
+{
+    pybind11::object BuildInstance(const std::string& className,
+                                   const std::string* moduleName = nullptr)
+    {
+        pybind11::dict globals = pybind11::globals();
+
+        if (moduleName != nullptr)
+        {
+            pybind11::object moduleObject;
+
+            if (globals.contains((*moduleName).c_str()))
+            {
+                moduleObject = globals[(*moduleName).c_str()];
+            }
+            else
+            {
+                moduleObject = pybind11::module::import((*moduleName).c_str());
+            }
+
+            pybind11::object constructor =
+                moduleObject.attr(className.c_str());
+            return constructor();
+        }
+        else if (globals.contains(className.c_str()))
+        {
+            pybind11::object constructor = globals[className.c_str()];
+            return constructor();
+        }
+
+        // Unable to instantiate the object in this case
+        throw std::runtime_error("PythonInstanceBuilder: Specified class was "
+                                 "not present in main module, nor was a "
+                                 "module name provided in the parameters.  "
+                                 "Unable to instantiate python class.");
+    }
+}
+}
+
+#endif /* ADIOS2_HELPER_PYTHONINSTANCEBUILDER_H_ */
