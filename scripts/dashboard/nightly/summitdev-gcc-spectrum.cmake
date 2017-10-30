bf5dd98fc5be746c410aee07d9ddd9e7acecbf5e
Chuck Atkins <chuck.atkins@kitware.com>
2017-10-30 11:25:35 -0400

    Change sitename for summitdev to reflect hostname

--- scripts/dashboard/nightly/summitdev-gcc-spectrum.cmake
+++ scripts/dashboard/nightly/summitdev-gcc-spectrum.cmake
@@ -1,5 +1,5 @@
 # Client maintainer: chuck.atkins@kitware.com
-set(CTEST_SITE "SummitDev")
+set(CTEST_SITE "summitdev.ccs.ornl.gov")
 set(CTEST_BUILD_CONFIGURATION Release)
 set(CTEST_CMAKE_GENERATOR "Unix Makefiles")
 set(CTEST_BUILD_FLAGS "-k -j10")
