13bb6641a967db2f38b8263aca6ae9a31c7b70b2
Chuck Atkins <chuck.atkins@kitware.com>
2017-12-18 12:05:15 -0500

    Change EVPATH -> EVPath in CMake for consistency and best practices

--- cmake/FindEVPath.cmake
+++ cmake/FindEVPath.cmake
@@ -2,40 +2,40 @@
 #   https://www.cc.gatech.edu/systems/projects/EVPath
 #
 # Use this module by invoking find_package with the form:
-#   find_package(EVPATH
+#   find_package(EVPath
 #     [version] [EXACT]     # Minimum or EXACT version, e.g. 1.6.0
-#     [REQUIRED]            # Fail with an error if EVPATH or a required
+#     [REQUIRED]            # Fail with an error if EVPath or a required
 #                           #   component is not found
 #     [QUIET]               # ...
 #     [COMPONENTS <...>]    # Compiled in components: fortran, readonly, 
                             # sequential (all are case insentative) 
 #   )
 #
-# Module that finds the includes and libraries for a working EVPATH install.
+# Module that finds the includes and libraries for a working EVPath install.
 # This module invokes the `evpath_config` script that should be installed with
-# the other EVPATH tools.
+# the other EVPath tools.
 #
-# To provide a hint to the module where to find the EVPATH installation,
-# set the EVPATH_ROOT or EVPATH_DIR environment variable.
+# To provide a hint to the module where to find the EVPath installation,
+# set the EVPath_ROOT or EVPath_DIR environment variable.
 #
 # If this variable is not set, make sure that at least the according `bin/`
-# directory of EVPATH is in your PATH environment variable.
+# directory of EVPath is in your PATH environment variable.
 #
 # Set the following CMake variables BEFORE calling find_packages to
 # influence this module:
-#   EVPATH_USE_STATIC_LIBS - Set to ON to force the use of static
+#   EVPath_USE_STATIC_LIBS - Set to ON to force the use of static
 #                           libraries.  Default: OFF
 #
 # This module will define the following variables:
-#   EVPATH_INCLUDE_DIRS    - Include directories for the EVPATH headers.
-#   EVPATH_LIBRARY_PATH    - Full path of the libevpath library (.a or .so file)
-#   EVPATH_DEPENDENCY_LIBRARIES       - EVPATH dependency libraries.
-#   EVPATH_FOUND           - TRUE if FindEVPATH found a working install
-#   EVPATH_VERSION         - Version in format Major.Minor.Patch
+#   EVPath_INCLUDE_DIRS    - Include directories for the EVPath headers.
+#   EVPath_LIBRARY_PATH    - Full path of the libevpath library (.a or .so file)
+#   EVPath_DEPENDENCY_LIBRARIES       - EVPath dependency libraries.
+#   EVPath_FOUND           - TRUE if FindEVPath found a working install
+#   EVPath_VERSION         - Version in format Major.Minor.Patch
 #
 # Not used for now:
-#   EVPATH_DEFINITIONS     - Compiler definitions you should add with
-#                           add_definitions(${EVPATH_DEFINITIONS})
+#   EVPath_DEFINITIONS     - Compiler definitions you should add with
+#                           add_definitions(${EVPath_DEFINITIONS})
 #
 ###############################################################################
 #Copyright (c) 2014, Axel Huebl and Felix Schmitt from http://picongpu.hzdr.de
@@ -69,12 +69,12 @@
 
 
 ###############################################################################
-# EVPATH
+# EVPath
 ###############################################################################
 # get flags for evpath_config, -s (static) is the default
 
 set(evpath_config_hints)
-foreach(PREFIX_VAR IN ITEMS EVPATH_ROOT INSTALL_PREFIX)
+foreach(PREFIX_VAR IN ITEMS EVPath_ROOT INSTALL_PREFIX)
   if(${PREFIX_VAR})
     list(APPEND evpath_config_hints "${${PREFIX_VAR}}/bin")
   elseif(NOT ("$ENV{${PREFIX_VAR}}" STREQUAL ""))
@@ -83,11 +83,11 @@ foreach(PREFIX_VAR IN ITEMS EVPATH_ROOT INSTALL_PREFIX)
 endforeach()
 
 # find `evpath_config` program #################################################
-find_program(EVPATH_CONFIG NAME evpath_config HINTS ${evpath_config_hints})
+find_program(EVPath_CONFIG NAME evpath_config HINTS ${evpath_config_hints})
 
 # check `evpath_config` program ################################################
-if(EVPATH_CONFIG)
-  execute_process(COMMAND ${EVPATH_CONFIG} -c 
+if(EVPath_CONFIG)
+  execute_process(COMMAND ${EVPath_CONFIG} -c 
     OUTPUT_VARIABLE evpath_config_out
     RESULT_VARIABLE evpath_config_ret
     OUTPUT_STRIP_TRAILING_WHITESPACE
@@ -95,17 +95,17 @@ if(EVPATH_CONFIG)
   if(evpath_config_ret EQUAL 0)
     string(REPLACE " " ";" evpath_match "${evpath_config_out}")
     set(evpath_include_hints)
-    set(EVPATH_COMPILE_OPTIONS)
+    set(EVPath_COMPILE_OPTIONS)
     foreach(OPT IN LISTS evpath_match)
       if(OPT MATCHES "^-I(.*)")
         list(APPEND evpath_include_hints "${CMAKE_MATCH_1}")
       else()
-        list(APPEND EVPATH_COMPILE_OPTIONS ${OPT})
+        list(APPEND EVPath_COMPILE_OPTIONS ${OPT})
       endif()
     endforeach()
   endif()
 
-  execute_process(COMMAND ${EVPATH_CONFIG} -l 
+  execute_process(COMMAND ${EVPath_CONFIG} -l 
     OUTPUT_VARIABLE evpath_config_out
     RESULT_VARIABLE evpath_config_ret
     OUTPUT_STRIP_TRAILING_WHITESPACE
@@ -131,8 +131,8 @@ if(EVPATH_CONFIG)
   endif()
 
   # add the version string
-  execute_process(COMMAND ${EVPATH_CONFIG} -v
-    OUTPUT_VARIABLE EVPATH_VERSION
+  execute_process(COMMAND ${EVPath_CONFIG} -v
+    OUTPUT_VARIABLE EVPath_VERSION
     OUTPUT_STRIP_TRAILING_WHITESPACE
   )
 endif()
@@ -143,25 +143,25 @@ if(NOT evpath_libs)
 endif()
 
 # Search for the actual libs and headers to use based on hints from the config
-find_path(EVPATH_INCLUDE_DIR evpath.h HINTS ${evpath_include_hints})
+find_path(EVPath_INCLUDE_DIR evpath.h HINTS ${evpath_include_hints})
 
-set(EVPATH_LIBRARY)
-set(EVPATH_DEPENDENCIES)
+set(EVPath_LIBRARY)
+set(EVPath_DEPENDENCIES)
 foreach(lib IN LISTS evpath_libs)
-  find_library(EVPATH_${lib}_LIBRARY NAME ${lib} HINTS ${evpath_lib_hints})
-  if(EVPATH_${lib}_LIBRARY)
+  find_library(EVPath_${lib}_LIBRARY NAME ${lib} HINTS ${evpath_lib_hints})
+  if(EVPath_${lib}_LIBRARY)
     if(lib MATCHES "^evpath")
-      set(EVPATH_LIBRARY ${EVPATH_${lib}_LIBRARY})
+      set(EVPath_LIBRARY ${EVPath_${lib}_LIBRARY})
     else()
-      list(APPEND EVPATH_DEPENDENCIES ${EVPATH_${lib}_LIBRARY})
+      list(APPEND EVPath_DEPENDENCIES ${EVPath_${lib}_LIBRARY})
     endif()
   else()
-    list(APPEND EVPATH_DEPENDENCIES ${lib})
+    list(APPEND EVPath_DEPENDENCIES ${lib})
   endif()
 endforeach()
 
 find_package(Threads REQUIRED)
-list(APPEND EVPATH_DEPENDENCIES ${evpath_lib_flags} ${CMAKE_THREAD_LIBS_INIT})
+list(APPEND EVPath_DEPENDENCIES ${evpath_lib_flags} ${CMAKE_THREAD_LIBS_INIT})
 
 ###############################################################################
 # FindPackage Options
@@ -169,24 +169,24 @@ list(APPEND EVPATH_DEPENDENCIES ${evpath_lib_flags} ${CMAKE_THREAD_LIBS_INIT})
 
 # handles the REQUIRED, QUIET and version-related arguments for find_package
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(EVPATH
-  REQUIRED_VARS EVPATH_LIBRARY EVPATH_INCLUDE_DIR
-  VERSION_VAR EVPATH_VERSION
+find_package_handle_standard_args(EVPath
+  REQUIRED_VARS EVPath_LIBRARY EVPath_INCLUDE_DIR
+  VERSION_VAR EVPath_VERSION
 )
 
-if(EVPATH_FOUND)
-  set(EVPATH_INCLUDE_DIRS ${EVPATH_INCLUDE_DIR})
-  set(EVPATH_LIBRARIES ${EVPATH_LIBRARY} ${EVPATH_DEPENDENCIES})
+if(EVPath_FOUND)
+  set(EVPath_INCLUDE_DIRS ${EVPath_INCLUDE_DIR})
+  set(EVPath_LIBRARIES ${EVPath_LIBRARY} ${EVPath_DEPENDENCIES})
   ##########################################################################
   # Add target and dependencies to ADIOS2
   ##########################################################################
-  if(NOT TARGET evpath::evpath)
-    add_library(evpath::evpath UNKNOWN IMPORTED)
-    set_target_properties(evpath::evpath PROPERTIES
-      IMPORTED_LOCATION "${EVPATH_LIBRARY}"
-      INTERFACE_LINK_LIBRARIES "${EVPATH_DEPENDENCIES}"
-      INTERFACE_INCLUDE_DIRECTORIES "${EVPATH_INCLUDE_DIRS}"
-      INTERFACE_COMPILE_OPTIONS "${EVPATH_COMPILE_OPTIONS}"
+  if(NOT TARGET EVPath::EVPath)
+    add_library(EVPath::EVPath UNKNOWN IMPORTED)
+    set_target_properties(EVPath::EVPath PROPERTIES
+      IMPORTED_LOCATION "${EVPath_LIBRARY}"
+      INTERFACE_LINK_LIBRARIES "${EVPath_DEPENDENCIES}"
+      INTERFACE_INCLUDE_DIRECTORIES "${EVPath_INCLUDE_DIRS}"
+      INTERFACE_COMPILE_OPTIONS "${EVPath_COMPILE_OPTIONS}"
     )
   endif()
 endif()
