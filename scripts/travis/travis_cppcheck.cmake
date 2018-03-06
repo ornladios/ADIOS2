888ce51aaca5eccee32680fa15dfbf4730add216
Scott Wittenburg <scott.wittenburg@kitware.com>
2018-03-05 17:13:05 -0700

    Add static analyses to Travis build matrix
    This topic adds two static checks (cppcheck, clang-analyzer) to
    the Travis CI build matrix that gets run on all PRs.  These checks
    are treated like normals builds and the results are pushed to the
    CDash ADIOS project.

---
+++ scripts/travis/travis_cppcheck.cmake
@@ -0,0 +1,20 @@
+# Client maintainer: chuck.atkins@kitware.com
+set(CTEST_SITE "TravisCI")
+set(CTEST_BUILD_CONFIGURATION Release)
+set(CTEST_CMAKE_GENERATOR "Unix Makefiles")
+set(CTEST_BUILD_FLAGS "-k -j8")
+set(CTEST_TEST_ARGS PARALLEL_LEVEL 1)
+
+set(dashboard_model Experimental)
+set(dashboard_binary_name "build_cppcheck")
+set(dashboard_track "Analysis")
+
+set(CTEST_GIT_COMMAND "/usr/bin/git")
+set(CTEST_UPDATE_VERSION_ONLY TRUE)
+set(CTEST_SOURCE_DIRECTORY "$ENV{SOURCE_DIR}")
+set(CTEST_DASHBOARD_ROOT "$ENV{HOME}")
+
+set(ENV{CC}  "$ENV{WRAPPED_CC}")
+set(ENV{CXX} "$ENV{WRAPPED_CXX}")
+
+include(${CMAKE_CURRENT_LIST_DIR}/../dashboard/adios_common.cmake)
