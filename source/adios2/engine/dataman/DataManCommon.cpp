682b7816cee6b651125c9849cedaf778a71672c3
Jason Wang <wangr1@ornl.gov>
2018-03-15 12:57:07 -0400

    added new files to git

---
+++ source/adios2/engine/dataman/DataManCommon.cpp
@@ -0,0 +1,93 @@
+/*
+ * Distributed under the OSI-approved Apache License, Version 2.0.  See
+ * accompanying file Copyright.txt for details.
+ *
+ * DataManReader.cpp
+ *
+ *  Created on: Feb 12, 2018
+ *      Author: Jason Wang
+ */
+
+#include "DataManCommon.h"
+
+namespace adios2
+{
+
+DataManCommon::DataManCommon(const std::string engineType, IO &io,
+                             const std::string &name, const Mode mode,
+                             MPI_Comm mpiComm)
+: Engine(engineType, io, name, mode, mpiComm)
+{
+
+    MPI_Comm_rank(mpiComm, &m_MPIRank);
+    MPI_Comm_size(mpiComm, &m_MPISize);
+
+    GetIntParameter(m_IO.m_Parameters, "TransportChannels",
+                    m_TransportChannels);
+}
+
+bool DataManCommon::GetBoolParameter(Params &params, std::string key,
+                                     bool &value)
+{
+    auto itKey = params.find(key);
+    if (itKey != params.end())
+    {
+        std::transform(itKey->second.begin(), itKey->second.end(),
+                       itKey->second.begin(), ::tolower);
+        if (itKey->second == "yes" || itKey->second == "true")
+        {
+            value = true;
+            return true;
+        }
+        if (itKey->second == "no" || itKey->second == "false")
+        {
+            value = false;
+            return true;
+        }
+    }
+    return false;
+}
+
+bool DataManCommon::GetStringParameter(Params &params, std::string key,
+                                       std::string &value)
+{
+    auto it = params.find(key);
+    if (it != params.end())
+    {
+        value = it->second;
+        std::transform(value.begin(), value.end(), value.begin(), ::tolower);
+        return true;
+    }
+    return false;
+}
+
+bool DataManCommon::GetIntParameter(Params &params, std::string key, int &value)
+{
+    auto it = params.find(key);
+    if (it != params.end())
+    {
+        value = std::stoi(it->second);
+        return true;
+    }
+    return false;
+}
+
+std::vector<std::string> DataManCommon::ParseAddress(std::string input,
+                                                     std::string protocol)
+{
+    std::vector<std::string> strings;
+    std::istringstream f(input);
+    std::string s;
+    while (getline(f, s, ','))
+    {
+        s.erase(remove_if(s.begin(), s.end(), isspace), s.end());
+        if (protocol == "tcp")
+        {
+            s = protocol + "://" + s;
+        }
+        strings.push_back(s);
+    }
+    return std::move(strings);
+}
+
+} // end namespace adios2
---
+++ source/adios2/engine/dataman/DataManCommon.h
@@ -0,0 +1,65 @@
+/*
+ * Distributed under the OSI-approved Apache License, Version 2.0.  See
+ * accompanying file Copyright.txt for details.
+ *
+ * DataManCommon.h
+ *
+ *  Created on: Feb 12, 2018
+ *      Author: Jason Wang
+ */
+
+#ifndef ADIOS2_ENGINE_DATAMAN_DATAMANCOMMON_H_
+#define ADIOS2_ENGINE_DATAMAN_DATAMANCOMMON_H_
+
+#include "adios2/ADIOSConfig.h"
+#include "adios2/ADIOSMacros.h"
+#include "adios2/core/Engine.h"
+#include "adios2/toolkit/format/bp3/BP3.h"
+#include "adios2/toolkit/transportman/dataman/DataMan.h"
+
+#include <json.hpp>
+
+namespace adios2
+{
+
+class DataManCommon : public Engine
+{
+
+public:
+    DataManCommon(const std::string engineType, IO &io, const std::string &name,
+                  const Mode mode, MPI_Comm mpiComm);
+
+    virtual ~DataManCommon() = default;
+
+protected:
+    int m_MPIRank;
+    int m_MPISize;
+    int m_RemoteMPISize;
+    int m_TransportChannels = 1;
+    std::string m_UseFormat = "json";
+    std::string m_TransportMode = "subscribe"; // subscribe | push | query
+    size_t m_BufferSize = 1024 * 1024 * 1024;
+    bool m_DoMonitor = false;
+
+    void InitCommon();
+
+    std::shared_ptr<transportman::DataMan> m_DataMan;
+    std::shared_ptr<std::thread> m_DataThread;
+
+    std::shared_ptr<transportman::DataMan> m_ControlMan;
+    std::shared_ptr<std::thread> m_ControlThread;
+
+    virtual void IOThread(std::shared_ptr<transportman::DataMan> man) = 0;
+
+    bool GetBoolParameter(Params &params, std::string key, bool &value);
+    bool GetStringParameter(Params &params, std::string key,
+                            std::string &value);
+    bool GetIntParameter(Params &params, std::string key, int &value);
+
+    std::vector<std::string> ParseAddress(std::string input,
+                                          std::string protocol = "");
+
+}; // end class DataManCommon
+} // end namespace adios2
+
+#endif /* ADIOS2_ENGINE_DATAMAN_DATAMANCOMMON_H_ */
