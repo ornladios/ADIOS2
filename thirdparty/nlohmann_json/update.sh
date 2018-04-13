5641235e395af272003e4f683970fd66b828bccf
Chuck Atkins <chuck.atkins@kitware.com>
2018-04-13 07:34:34 -0400

    Move json to upstream master

--- thirdparty/nlohmann_json/update.sh
+++ thirdparty/nlohmann_json/update.sh
@@ -7,12 +7,15 @@ shopt -s dotglob
 readonly name="nlohmann_json"
 readonly ownership="JSON For Modern C++ Upstream <robot@adios2>"
 readonly subtree="thirdparty/NLohmannJson/json"
-#readonly repo="https://github.com/nlohmann/json.git"
-#readonly tag="v3.1.2"
-readonly repo="https://github.com/chuckatkins/json.git"
-readonly tag="misc-cmake-packaging-enhancements"
+readonly repo="https://github.com/nlohmann/json.git"
+readonly tag="master"
 readonly shortlog="true"
 readonly paths="
+  CMakeLists.txt
+  LICENSE.MIT
+  cmake
+  include
+  single_include
 "
 
 extract_source () {
