5ec0cf13fc7e0f32f6bf6d3a69b6b06932a7b555
William F Godoy <williamfgc@yahoo.com>
2018-04-18 15:50:33 -0400

    Forcing adios2_cxx03.h inclusion
    Solves Windows VS preprocessor bug

--- source/adios2.h
+++ source/adios2.h
@@ -8,15 +8,11 @@
 
 #include "adios2/ADIOSConfig.h"
 
-#if __cplusplus >= 201103L
 #include "adios2/ADIOSTypes.h"
 #include "adios2/core/ADIOS.h"
 #include "adios2/core/Engine.h"
 #include "adios2/core/IO.h"
 #include "adios2/core/Operator.h"
 #include "adios2/highlevelapi/fstream/ADIOS2fstream.h"
-#else
-#include "adios2_cxx03.h"
-#endif
 
 #endif /* ADIOS2_H_ */
