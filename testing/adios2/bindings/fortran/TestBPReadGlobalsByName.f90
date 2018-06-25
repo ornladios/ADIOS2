42026406d871d6a8928d363f972904e06225c6b2
williamfgc <williamfgc@yahoo.com>
2018-06-25 16:36:01 -0400

    Added Multiblock and Multistep tests
    Satisfies enhancement #335
    Solves bug #682 , bug #573, bug #675, bug #613

---
+++ testing/adios2/bindings/fortran/TestBPReadGlobalsByName.f90
@@ -0,0 +1,63 @@
+program TestBPReadGlobalsByName
+    use mpi
+    use adios2
+    implicit none
+
+    integer(kind=8), dimension(2) :: sel_start, sel_count
+    real, dimension(:,:), allocatable :: data
+    integer :: i, j, inx, iny, irank, isize, ierr
+    integer :: diag_1d_isp=999,diag_1d_nsp=999  ! same as ptl_isp/nsp
+    real (8) :: sml_inpsi=999.0D0, sml_outpsi=999.0D0  !! Inner and outer boundary for initial loading.
+
+
+    ! adios2 handlers
+    type(adios2_adios):: adios
+    type(adios2_io):: ioWrite, ioRead
+    type(adios2_variable):: var
+    type(adios2_engine):: writer, reader
+
+    ! Launch MPI
+    call MPI_Init(ierr)
+    call MPI_Comm_rank(MPI_COMM_WORLD, irank, ierr)
+    call MPI_Comm_size(MPI_COMM_WORLD, isize, ierr)
+
+    ! Start adios2
+    call adios2_init(adios, MPI_COMM_SELF, adios2_debug_mode_on, ierr)
+
+    ! writer
+    call adios2_declare_io(ioWrite, adios, "FWriter", ierr)
+
+    call adios2_define_variable(var, ioWrite, "diag_1d_nsp", &
+                                adios2_type_integer4, ierr)
+    call adios2_define_variable(var, ioWrite, "sml_outpsi", adios2_type_dp, &
+                                ierr)
+
+    call adios2_open(writer, ioWrite, "xgc.f0analysis.static.bp", &
+                     adios2_mode_write, ierr)
+    call adios2_put(writer, "diag_1d_nsp", 1, ierr)
+    call adios2_put(writer, "sml_outpsi", 0.295477_8, ierr)
+    call adios2_close(writer, ierr)
+
+    ! reader
+    call adios2_declare_io(ioRead, adios, "FReader", ierr)
+    call adios2_open(reader, ioRead, "xgc.f0analysis.static.bp", adios2_mode_read, ierr)
+    call adios2_get(reader, "diag_1d_nsp", diag_1d_nsp, ierr)
+    call adios2_get(reader, "sml_outpsi", sml_outpsi, ierr)
+    call adios2_close(reader, ierr)
+
+    call adios2_finalize(adios, ierr)
+
+    !! Expect 1 and 0.295477 for diag_1d_nsp and sml_outpsi
+    if( diag_1d_nsp /= 1 ) then
+        print *, irank, 'diag_1d_nsp', diag_1d_nsp
+        stop 'diag_1d_nsp is not 1'
+    end if
+
+    if( sml_outpsi /= 0.295477_8 ) then
+        print *, irank, 'sml_outpsi', sml_outpsi
+        stop 'sml_outpsi is not 0.295477'
+    end if
+
+    call MPI_Finalize(ierr)
+
+end program TestBPReadGlobalsByName
--- testing/adios2/bindings/python/TestBPReadMultisteps.py
+++ testing/adios2/bindings/python/TestBPReadMultisteps.py
@@ -11,6 +11,7 @@
 
 from adios2NPTypes import SmallTestData
 from mpi4py import MPI
+import numpy as np
 import adios2
 
 
@@ -42,8 +43,6 @@ data = SmallTestData()
 
 # ADIOS Variable name, shape, start, offset, constant dims
 # All local variables
-varStr = ioWriter.DefineVariable("varStr", data.Str)
-
 varI8 = ioWriter.DefineVariable(
     "varI8", shape, start, count, adios2.ConstantDims, data.I8)
 varI16 = ioWriter.DefineVariable(
@@ -76,22 +75,30 @@ writer = ioWriter.Open("npTypes.bp", adios2.Mode.Write)
 
 for i in range(0, 3):
 
-    data.update(rank, i, size)
+    npi8 = np.full((Nx), i, dtype=np.int8)
+    npi16 = np.full((Nx), i, dtype=np.int16)
+    npi32 = np.full((Nx), i, dtype=np.int32)
+    npi64 = np.full((Nx), i, dtype=np.int64)
+    npu8 = np.full((Nx), i, dtype=np.uint8)
+    npu16 = np.full((Nx), i, dtype=np.uint16)
+    npu32 = np.full((Nx), i, dtype=np.uint32)
+    npu64 = np.full((Nx), i, dtype=np.uint64)
+    npr32 = np.full((Nx), i, dtype=np.float32)
+    npr64 = np.full((Nx), i, dtype=np.float64)
 
     writer.BeginStep()
-    writer.Put(varStr, data.Str)
-    writer.Put(varI8, data.I8)
-    writer.Put(varI16, data.I16)
-    writer.Put(varI32, data.I32)
-    writer.Put(varI64, data.I64)
+    writer.Put(varI8, npi8)
+    writer.Put(varI16, npi16)
+    writer.Put(varI32, npi32)
+    writer.Put(varI64, npi64)
 
-    writer.Put(varU8, data.U8)
-    writer.Put(varU16, data.U16)
-    writer.Put(varU32, data.U32)
-    writer.Put(varU64, data.U64)
+    writer.Put(varU8, npu8)
+    writer.Put(varU16, npu16)
+    writer.Put(varU32, npu32)
+    writer.Put(varU64, npu64)
 
-    writer.Put(varR32, data.R32)
-    writer.Put(varR64, data.R64)
+    writer.Put(varR32, npr32)
+    writer.Put(varR64, npr64)
     writer.EndStep()
 
 writer.Close()
@@ -104,7 +111,6 @@ reader = ioReader.Open("npTypes.bp", adios2.Mode.Read)
 attrString = ioReader.InquireAttribute("attrString")
 attrI8 = ioReader.InquireAttribute("attrI8")
 
-varStr = ioReader.InquireVariable("varStr")
 varI8 = ioReader.InquireVariable("varI8")
 varI16 = ioReader.InquireVariable("varI16")
 varI32 = ioReader.InquireVariable("varI32")
@@ -119,7 +125,6 @@ varR64 = ioReader.InquireVariable("varR64")
 check_object(attrString, "attrString")
 check_object(attrString, "attrI8")
 
-check_object(varStr, "varStr")
 check_object(varI8, "varI8")
 check_object(varI16, "varI16")
 check_object(varI32, "varI32")
@@ -154,6 +159,102 @@ for name, info in variablesInfo.items():
         for key, value in info.items():
             print("\t" + key + ": " + value)
         print("\n")
+
+
+if varI8 is not None:
+    varI8.SetSelection([[0], [size*Nx]])
+    varI8.SetStepSelection([0, 3])
+    inI8 = np.zeros((3, size*Nx), dtype=np.int8)
+    reader.Get(varI8, inI8)
+
+if varI16 is not None:
+    varI16.SetSelection([[0], [size*Nx]])
+    varI16.SetStepSelection([0, 3])
+    inI16 = np.zeros((3, size*Nx), dtype=np.int16)
+    reader.Get(varI16, inI16)
+
+if varI32 is not None:
+    varI32.SetSelection([[0], [size*Nx]])
+    varI32.SetStepSelection([0, 3])
+    inI32 = np.zeros((3, size*Nx), dtype=np.int32)
+    reader.Get(varI32, inI32)
+
+if varI64 is not None:
+    varI64.SetSelection([[0], [size*Nx]])
+    varI64.SetStepSelection([0, 3])
+    inI64 = np.zeros((3, size*Nx), dtype=np.int64)
+    reader.Get(varI64, inI64)
+
+if varU8 is not None:
+    varU8.SetSelection([[0], [size*Nx]])
+    varU8.SetStepSelection([0, 3])
+    inU8 = np.zeros((3, size*Nx), dtype=np.uint8)
+    reader.Get(varU8, inU8)
+
+if varU16 is not None:
+    varU16.SetSelection([[0], [size*Nx]])
+    varU16.SetStepSelection([0, 3])
+    inU16 = np.zeros((3, size*Nx), dtype=np.uint16)
+    reader.Get(varU16, inU16)
+
+if varU32 is not None:
+    varU32.SetSelection([[0], [size*Nx]])
+    varU32.SetStepSelection([0, 3])
+    inU32 = np.zeros((3, size*Nx), dtype=np.uint32)
+    reader.Get(varU32, inU32)
+
+if varU64 is not None:
+    varU64.SetSelection([[0], [size*Nx]])
+    varU64.SetStepSelection([0, 3])
+    inU64 = np.zeros((3, size*Nx), dtype=np.uint64)
+    reader.Get(varU64, inU64)
+
+if varR32 is not None:
+    varR32.SetSelection([[0], [size*Nx]])
+    varR32.SetStepSelection([0, 3])
+    inR32 = np.zeros((3, size*Nx), dtype=np.float32)
+    reader.Get(varR32, inR32)
+
+if varR64 is not None:
+    varR64.SetSelection([[0], [size*Nx]])
+    varR64.SetStepSelection([0, 3])
+    inR64 = np.zeros((3, size*Nx), dtype=np.float64)
+    reader.Get(varR64, inR64)
+
+reader.PerformGets()
+
+for i in range(0, 3):
+    for j in range(0, Nx):
+        if(inI8[i][j] != i):
+            raise ValueError('failed reading I8')
+
+        if(inI16[i][j] != i):
+            raise ValueError('failed reading I16')
+
+        if(inI32[i][j] != i):
+            raise ValueError('failed reading I32')
+
+        if(inI64[i][j] != i):
+            raise ValueError('failed reading I64')
+
+        if(inU8[i][j] != i):
+            raise ValueError('failed reading U8')
+
+        if(inU16[i][j] != i):
+            raise ValueError('failed reading U16')
+
+        if(inU32[i][j] != i):
+            raise ValueError('failed reading U32')
+
+        if(inU64[i][j] != i):
+            raise ValueError('failed reading U64')
+
+        if(inR32[i][j] != i):
+            raise ValueError('failed reading R32')
+
+        if(inR64[i][j] != i):
+            raise ValueError('failed reading R64')
+
 
 # here tests reader data
 reader.Close()
