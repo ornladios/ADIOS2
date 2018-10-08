c8962d77e8b3051a0c8034e836b1e385e3dbe3f5
William F Godoy <williamfgc@yahoo.com>
2018-10-08 15:37:01 -0400

    Test passing

--- source/adios2/core/AttributeBase.cpp
+++ source/adios2/core/AttributeBase.cpp
@@ -15,14 +15,15 @@ namespace adios2
 namespace core
 {
 
+AttributeBase::AttributeBase(const std::string &name, const std::string type)
+: m_Name(name), m_Type(type), m_Elements(1), m_IsSingleValue(true)
+{
+}
+
 AttributeBase::AttributeBase(const std::string &name, const std::string type,
                              const size_t elements)
-: m_Name(name), m_Type(type), m_Elements(elements)
+: m_Name(name), m_Type(type), m_Elements(elements), m_IsSingleValue(false)
 {
-    if (m_Elements == 1)
-    {
-        m_IsSingleValue = true;
-    }
 }
 
 } // end namespace core
