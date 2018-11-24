d3d20617bde41984a6c80721b35a19c0710c0fe4
Chuck Atkins <chuck.atkins@kitware.com>
2018-11-24 01:45:57 -0500

    Use generator expression for VisualStudio compatibility

--- examples/heatTransfer/TestSSTFFSMxN.cmake
+++ examples/heatTransfer/TestSSTFFSMxN.cmake
@@ -20,7 +20,7 @@ add_test(NAME HeatTransfer.SST.FFS.Dump.MxN
     -DARGS=-d 
     -DINPUT_FILE=HeatTransfer.SST.FFS.Read.MxN.bp
     -DOUTPUT_FILE=HeatTransfer.SST.FFS.Dump.MxN.txt
-    -P "${PROJECT_BINARY_DIR}/${CMAKE_CFG_INTDIR}/bpls.cmake"
+    -P "${PROJECT_BINARY_DIR}/$<CONFIG>/bpls.cmake"
 )
 set_property(TEST HeatTransfer.SST.FFS.Dump.MxN
   PROPERTY DEPENDS HeatTransfer.SST.FFS.Read.MxN
