5fdca52ea5e812f1487ffdd2186786e6b1f03af4
Chuck Atkins <chuck.atkins@kitware.com>
2019-01-03 12:17:27 -0500

    Convert CircleCI EL7 images from GCC7 -> GCC8

--- scripts/circle/circle_el7-gnu8-openmpi.cmake
+++ scripts/circle/circle_el7-gnu8-openmpi.cmake
@@ -4,7 +4,7 @@ set(CTEST_SITE "CircleCI")
 set(CTEST_BUILD_CONFIGURATION Debug)
 set(CTEST_CMAKE_GENERATOR "Unix Makefiles")
 set(CTEST_BUILD_FLAGS "-k -j4")
-set(CTEST_TEST_ARGS PARALLEL_LEVEL 4)
+set(CTEST_TEST_ARGS PARALLEL_LEVEL 1)
 
 set(dashboard_model Experimental)
 set(dashboard_binary_name "build_$ENV{CIRCLE_JOB}")
@@ -17,9 +17,11 @@ set(CTEST_DASHBOARD_ROOT "$ENV{HOME}")
 
 include(${CMAKE_CURRENT_LIST_DIR}/EnvironmentModules.cmake)
 module(purge)
-module(load gnu7)
+module(load gnu8)
 module(load py3-numpy)
-module(load hdf5)
+module(load openmpi3)
+module(load py3-mpi4py)
+module(load phdf5)
 
 set(ENV{CC}  gcc)
 set(ENV{CXX} g++)
@@ -34,11 +36,12 @@ ADIOS2_USE_BZip2:STRING=ON
 ADIOS2_USE_DataMan:STRING=ON
 ADIOS2_USE_Fortran:STRING=ON
 ADIOS2_USE_HDF5:STRING=ON
-ADIOS2_USE_MPI:STRING=OFF
+ADIOS2_USE_MPI:STRING=ON
 ADIOS2_USE_Python:STRING=ON
 #ADIOS2_USE_ZFP:STRING=ON
 ADIOS2_USE_ZeroMQ:STRING=ON
-#ZFP_ROOT:PATH=/opt/zfp/install
+
+MPIEXEC_MAX_NUMPROCS:STRING=8
 
 PYTHON_EXECUTABLE:FILEPATH=${PYTHON_EXECUTABLE}
 ")
