9fe37e2e05314c2afabcb8d8fe4043c79126ea18
Kai Germaschewski <kai.germaschewski@unh.edu>
2019-02-14 09:46:36 -0500

    stdtype: engine/bp4

--- source/adios2/engine/bp4/BP4Reader.h
+++ source/adios2/engine/bp4/BP4Reader.h
@@ -66,7 +66,7 @@ private:
 #define declare_type(T)                                                        \
     void DoGetSync(Variable<T> &, T *) final;                                  \
     void DoGetDeferred(Variable<T> &, T *) final;
-    ADIOS2_FOREACH_TYPE_1ARG(declare_type)
+    ADIOS2_FOREACH_STDTYPE_1ARG(declare_type)
 #undef declare_type
 
     void DoClose(const int transportIndex = -1) final;
@@ -87,7 +87,7 @@ private:
     std::vector<typename Variable<T>::Info> DoBlocksInfo(                      \
         const Variable<T> &variable, const size_t step) const final;
 
-    ADIOS2_FOREACH_TYPE_1ARG(declare_type)
+    ADIOS2_FOREACH_STDTYPE_1ARG(declare_type)
 #undef declare_type
 };
 
--- source/adios2/engine/bp4/BP4Writer.cpp
+++ source/adios2/engine/bp4/BP4Writer.cpp
@@ -82,7 +82,7 @@ void BP4Writer::PerformPuts()
         variable.m_BlocksInfo.clear();                                         \
     }
 
-        ADIOS2_FOREACH_TYPE_1ARG(declare_template_instantiation)
+        ADIOS2_FOREACH_STDTYPE_1ARG(declare_template_instantiation)
 #undef declare_template_instantiation
     }
     m_BP4Serializer.m_DeferredVariables.clear();
@@ -137,7 +137,7 @@ void BP4Writer::Init()
         PutDeferredCommon(variable, data);                                     \
     }
 
-ADIOS2_FOREACH_TYPE_1ARG(declare_type)
+ADIOS2_FOREACH_STDTYPE_1ARG(declare_type)
 #undef declare_type
 
 void BP4Writer::InitParameters()
