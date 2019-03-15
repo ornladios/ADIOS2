a71a7ab74cc93d2eef47fb4bf4939943ad561fab
William F Godoy <williamfgc@yahoo.com>
2019-03-15 13:50:51 -0400

    Added test for filling span value

--- testing/adios2/engine/bp/TestBPWriteReadVariableSpan.cpp
+++ testing/adios2/engine/bp/TestBPWriteReadVariableSpan.cpp
@@ -1174,6 +1174,278 @@ TEST_F(BPWriteReadSpan, BPWriteRead2D2x4Local)
     }
 }
 
+TEST_F(BPWriteReadSpan, BPWriteRead1D8FillValue)
+{
+    // Each process would write a 1x8 array and all processes would
+    // form a mpiSize * Nx 1D array
+    const std::string fname("BPWriteReadSpan1D8FillValue.bp");
+
+    int mpiRank = 0, mpiSize = 1;
+    // Number of rows
+    const size_t Nx = 8;
+
+    // Number of steps
+    const size_t NSteps = 5;
+
+#ifdef ADIOS2_HAVE_MPI
+    MPI_Comm_rank(MPI_COMM_WORLD, &mpiRank);
+    MPI_Comm_size(MPI_COMM_WORLD, &mpiSize);
+#endif
+
+// Write test data using BP
+
+#ifdef ADIOS2_HAVE_MPI
+    adios2::ADIOS adios(MPI_COMM_WORLD, adios2::DebugON);
+#else
+    adios2::ADIOS adios(true);
+#endif
+    {
+        adios2::IO io = adios.DeclareIO("TestIO");
+
+        //        if (!engineName.empty())
+        //        {
+        //            io.SetEngine(engineName);
+        //        }
+
+        const adios2::Dims shape{static_cast<size_t>(Nx * mpiSize)};
+        const adios2::Dims start{static_cast<size_t>(Nx * mpiRank)};
+        const adios2::Dims count{Nx};
+
+        auto var_String = io.DefineVariable<std::string>("iString");
+        auto var_i8 = io.DefineVariable<int8_t>("i8", shape, start, count,
+                                                adios2::ConstantDims);
+        auto var_i16 = io.DefineVariable<int16_t>("i16", shape, start, count,
+                                                  adios2::ConstantDims);
+        auto var_i32 = io.DefineVariable<int32_t>("i32", shape, start, count,
+                                                  adios2::ConstantDims);
+        auto var_i64 = io.DefineVariable<int64_t>("i64", shape, start, count,
+                                                  adios2::ConstantDims);
+        auto var_u8 = io.DefineVariable<uint8_t>("u8", shape, start, count,
+                                                 adios2::ConstantDims);
+        auto var_u16 = io.DefineVariable<uint16_t>("u16", shape, start, count,
+                                                   adios2::ConstantDims);
+        auto var_u32 = io.DefineVariable<uint32_t>("u32", shape, start, count,
+                                                   adios2::ConstantDims);
+        auto var_u64 = io.DefineVariable<uint64_t>("u64", shape, start, count,
+                                                   adios2::ConstantDims);
+        auto var_r32 = io.DefineVariable<float>("r32", shape, start, count,
+                                                adios2::ConstantDims);
+        auto var_r64 = io.DefineVariable<double>("r64", shape, start, count,
+                                                 adios2::ConstantDims);
+        auto var_cr32 = io.DefineVariable<std::complex<float>>(
+            "cr32", shape, start, count, adios2::ConstantDims);
+        auto var_cr64 = io.DefineVariable<std::complex<double>>(
+            "cr64", shape, start, count, adios2::ConstantDims);
+
+        adios2::Engine bpWriter = io.Open(fname, adios2::Mode::Write);
+
+        for (size_t step = 0; step < NSteps; ++step)
+        {
+            bpWriter.BeginStep();
+            EXPECT_EQ(bpWriter.CurrentStep(), step);
+            bpWriter.Put<std::string>("iString", std::to_string(step));
+
+            adios2::Variable<int8_t>::Span i8Span =
+                bpWriter.Put(var_i8, NULL, static_cast<int8_t>(step));
+            adios2::Variable<int16_t>::Span i16Span =
+                bpWriter.Put(var_i16, NULL, static_cast<int16_t>(step));
+            adios2::Variable<int32_t>::Span i32Span =
+                bpWriter.Put(var_i32, NULL, static_cast<int32_t>(step));
+            adios2::Variable<int64_t>::Span i64Span =
+                bpWriter.Put(var_i64, NULL, static_cast<int64_t>(step));
+
+            adios2::Variable<uint8_t>::Span u8Span =
+                bpWriter.Put(var_u8, NULL, static_cast<uint8_t>(step));
+            adios2::Variable<uint16_t>::Span u16Span =
+                bpWriter.Put(var_u16, NULL, static_cast<uint16_t>(step));
+            adios2::Variable<uint32_t>::Span u32Span =
+                bpWriter.Put(var_u32, NULL, static_cast<uint32_t>(step));
+            adios2::Variable<uint64_t>::Span u64Span =
+                bpWriter.Put(var_u64, NULL, static_cast<uint64_t>(step));
+
+            adios2::Variable<float>::Span r32Span =
+                bpWriter.Put(var_r32, NULL, static_cast<float>(step));
+
+            adios2::Variable<double>::Span r64Span =
+                bpWriter.Put(var_r64, NULL, static_cast<double>(step));
+
+            adios2::Variable<std::complex<float>>::Span cr32Span =
+                bpWriter.Put(var_cr32, NULL, {static_cast<float>(step),
+                                              static_cast<float>(step)});
+            adios2::Variable<std::complex<double>>::Span cr64Span =
+                bpWriter.Put(var_cr64, NULL, {static_cast<double>(step),
+                                              static_cast<double>(step)});
+
+            bpWriter.EndStep();
+        }
+
+        bpWriter.Close();
+    }
+
+    {
+        adios2::IO io = adios.DeclareIO("ReadIO");
+
+        //        if (!engineName.empty())
+        //        {
+        //            io.SetEngine(engineName);
+        //        }
+
+        adios2::Engine bpReader = io.Open(fname, adios2::Mode::Read);
+
+        std::string IString;
+        std::array<int8_t, Nx> I8;
+        std::array<int16_t, Nx> I16;
+        std::array<int32_t, Nx> I32;
+        std::array<int64_t, Nx> I64;
+        std::array<uint8_t, Nx> U8;
+        std::array<uint16_t, Nx> U16;
+        std::array<uint32_t, Nx> U32;
+        std::array<uint64_t, Nx> U64;
+        std::array<float, Nx> R32;
+        std::array<double, Nx> R64;
+        std::array<std::complex<float>, Nx> CR32;
+        std::array<std::complex<double>, Nx> CR64;
+
+        const adios2::Dims start{mpiRank * Nx};
+        const adios2::Dims count{Nx};
+
+        const adios2::Box<adios2::Dims> sel(start, count);
+
+        size_t t = 0;
+
+        while (bpReader.BeginStep() == adios2::StepStatus::OK)
+        {
+            const size_t currentStep = bpReader.CurrentStep();
+            EXPECT_EQ(currentStep, static_cast<size_t>(t));
+
+            auto var_iString = io.InquireVariable<std::string>("iString");
+            auto var_i8 = io.InquireVariable<int8_t>("i8");
+            auto var_i16 = io.InquireVariable<int16_t>("i16");
+            auto var_i32 = io.InquireVariable<int32_t>("i32");
+            auto var_i64 = io.InquireVariable<int64_t>("i64");
+            auto var_u8 = io.InquireVariable<uint8_t>("u8");
+            auto var_u16 = io.InquireVariable<uint16_t>("u16");
+            auto var_u32 = io.InquireVariable<uint32_t>("u32");
+            auto var_u64 = io.InquireVariable<uint64_t>("u64");
+            auto var_r32 = io.InquireVariable<float>("r32");
+            auto var_r64 = io.InquireVariable<double>("r64");
+            auto var_cr32 = io.InquireVariable<std::complex<float>>("cr32");
+            auto var_cr64 = io.InquireVariable<std::complex<double>>("cr64");
+
+            ASSERT_EQ(var_i8.ShapeID(), adios2::ShapeID::GlobalArray);
+            ASSERT_EQ(var_i8.Steps(), NSteps);
+            ASSERT_EQ(var_i8.Shape()[0], static_cast<size_t>(mpiSize * Nx));
+
+            ASSERT_EQ(var_i16.ShapeID(), adios2::ShapeID::GlobalArray);
+            ASSERT_EQ(var_i16.Steps(), NSteps);
+            ASSERT_EQ(var_i16.Shape()[0], static_cast<size_t>(mpiSize * Nx));
+
+            ASSERT_EQ(var_i32.ShapeID(), adios2::ShapeID::GlobalArray);
+            ASSERT_EQ(var_i32.Steps(), NSteps);
+            ASSERT_EQ(var_i32.Shape()[0], static_cast<size_t>(mpiSize * Nx));
+
+            ASSERT_EQ(var_i64.ShapeID(), adios2::ShapeID::GlobalArray);
+            ASSERT_EQ(var_i64.Steps(), NSteps);
+            ASSERT_EQ(var_i64.Shape()[0], static_cast<size_t>(mpiSize * Nx));
+
+            ASSERT_EQ(var_u8.ShapeID(), adios2::ShapeID::GlobalArray);
+            ASSERT_EQ(var_u8.Steps(), NSteps);
+            ASSERT_EQ(var_u8.Shape()[0], static_cast<size_t>(mpiSize * Nx));
+
+            ASSERT_EQ(var_u16.ShapeID(), adios2::ShapeID::GlobalArray);
+            ASSERT_EQ(var_u16.Steps(), NSteps);
+            ASSERT_EQ(var_u16.Shape()[0], static_cast<size_t>(mpiSize * Nx));
+
+            ASSERT_EQ(var_u32.ShapeID(), adios2::ShapeID::GlobalArray);
+            ASSERT_EQ(var_u32.Steps(), NSteps);
+            ASSERT_EQ(var_u32.Shape()[0], static_cast<size_t>(mpiSize * Nx));
+
+            ASSERT_EQ(var_u64.ShapeID(), adios2::ShapeID::GlobalArray);
+            ASSERT_EQ(var_u64.Steps(), NSteps);
+            ASSERT_EQ(var_u64.Shape()[0], static_cast<size_t>(mpiSize * Nx));
+
+            ASSERT_EQ(var_r32.ShapeID(), adios2::ShapeID::GlobalArray);
+            ASSERT_EQ(var_r32.Steps(), NSteps);
+            ASSERT_EQ(var_r32.Shape()[0], static_cast<size_t>(mpiSize * Nx));
+
+            ASSERT_EQ(var_r64.ShapeID(), adios2::ShapeID::GlobalArray);
+            ASSERT_EQ(var_r64.Steps(), NSteps);
+            ASSERT_EQ(var_r64.Shape()[0], static_cast<size_t>(mpiSize * Nx));
+
+            ASSERT_EQ(var_cr32.ShapeID(), adios2::ShapeID::GlobalArray);
+            ASSERT_EQ(var_cr32.Steps(), NSteps);
+            ASSERT_EQ(var_cr32.Shape()[0], static_cast<size_t>(mpiSize * Nx));
+
+            ASSERT_EQ(var_cr64.ShapeID(), adios2::ShapeID::GlobalArray);
+            ASSERT_EQ(var_cr64.Steps(), NSteps);
+            ASSERT_EQ(var_cr64.Shape()[0], static_cast<size_t>(mpiSize * Nx));
+
+            var_i8.SetSelection(sel);
+            var_i16.SetSelection(sel);
+            var_i32.SetSelection(sel);
+            var_i64.SetSelection(sel);
+            var_u8.SetSelection(sel);
+            var_u16.SetSelection(sel);
+            var_u32.SetSelection(sel);
+            var_u64.SetSelection(sel);
+            var_r32.SetSelection(sel);
+            var_r64.SetSelection(sel);
+            var_cr32.SetSelection(sel);
+            var_cr64.SetSelection(sel);
+
+            bpReader.Get(var_iString, IString);
+            bpReader.Get(var_i8, I8.data());
+            bpReader.Get(var_i16, I16.data());
+            bpReader.Get(var_i32, I32.data());
+            bpReader.Get(var_i64, I64.data());
+            bpReader.Get(var_u8, U8.data());
+            bpReader.Get(var_u16, U16.data());
+            bpReader.Get(var_u32, U32.data());
+            bpReader.Get(var_u64, U64.data());
+            bpReader.Get(var_r32, R32.data());
+            bpReader.Get(var_r64, R64.data());
+            bpReader.Get(var_cr32, CR32.data());
+            bpReader.Get(var_cr64, CR64.data());
+
+            bpReader.EndStep();
+
+            EXPECT_EQ(IString, std::to_string(currentStep));
+
+            for (size_t i = 0; i < Nx; ++i)
+            {
+                std::stringstream ss;
+                ss << "t=" << t << " i=" << i << " rank=" << mpiRank;
+                std::string msg = ss.str();
+
+                EXPECT_EQ(I8[i], static_cast<int8_t>(currentStep)) << msg;
+                EXPECT_EQ(I16[i], static_cast<int16_t>(currentStep)) << msg;
+                EXPECT_EQ(I32[i], static_cast<int32_t>(currentStep)) << msg;
+                EXPECT_EQ(I64[i], static_cast<int64_t>(currentStep)) << msg;
+                EXPECT_EQ(U8[i], static_cast<uint8_t>(currentStep)) << msg;
+                EXPECT_EQ(U16[i], static_cast<uint16_t>(currentStep)) << msg;
+                EXPECT_EQ(U32[i], static_cast<uint32_t>(currentStep)) << msg;
+                EXPECT_EQ(U64[i], static_cast<uint64_t>(currentStep)) << msg;
+                EXPECT_EQ(R32[i], static_cast<float>(currentStep)) << msg;
+                EXPECT_EQ(R64[i], static_cast<double>(currentStep)) << msg;
+                EXPECT_EQ(CR32[i],
+                          std::complex<float>(static_cast<float>(currentStep),
+                                              static_cast<float>(currentStep)))
+                    << msg;
+                EXPECT_EQ(CR64[i], std::complex<double>(
+                                       static_cast<double>(currentStep),
+                                       static_cast<double>(currentStep)))
+                    << msg;
+            }
+
+            ++t;
+        }
+
+        EXPECT_EQ(t, NSteps);
+
+        bpReader.Close();
+    }
+}
+
 int main(int argc, char **argv)
 {
 #ifdef ADIOS2_HAVE_MPI
