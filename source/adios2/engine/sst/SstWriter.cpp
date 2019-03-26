12ed3bf8b28cfcad77f79a9b60985e87c6eb8e00
Greg Eisenhauer <eisen@cc.gatech.edu>
2019-03-25 22:05:59 -0400

    Fix size of marshalled BP buffer

--- source/adios2/engine/sst/SstWriter.cpp
+++ source/adios2/engine/sst/SstWriter.cpp
@@ -158,8 +158,7 @@ void SstWriter::EndStep()
         m_BP3Serializer->AggregateCollectiveMetadata(
             m_MPIComm, m_BP3Serializer->m_Metadata, true);
         BP3DataBlock *newblock = new BP3DataBlock;
-        newblock->metadata.DataSize =
-            m_BP3Serializer->m_Metadata.m_Buffer.size();
+        newblock->metadata.DataSize = m_BP3Serializer->m_Metadata.m_Position;
         newblock->metadata.block = m_BP3Serializer->m_Metadata.m_Buffer.data();
         newblock->data.DataSize = m_BP3Serializer->m_Data.m_Buffer.size();
         newblock->data.block = m_BP3Serializer->m_Data.m_Buffer.data();
