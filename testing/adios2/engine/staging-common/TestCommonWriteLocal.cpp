0eff80591c498f5ef44441c81fc77d43627951ca
Greg Eisenhauer <eisen@cc.gatech.edu>
2019-05-22 18:27:29 -0400

    Hope to make VC++ happier

--- testing/adios2/engine/staging-common/TestCommonWriteLocal.cpp
+++ testing/adios2/engine/staging-common/TestCommonWriteLocal.cpp
@@ -149,7 +149,7 @@ TEST_F(CommonWriteTest, ADIOS2CommonWrite)
     for (size_t step = 0; step < NSteps; ++step)
     {
         // Generate test data for each process uniquely
-        generateCommonTestData((int)step, mpiRank, mpiSize, Nx, Nx);
+        generateCommonTestData((int)step, mpiRank, mpiSize, (int)Nx, (int)Nx);
 
         engine.BeginStep();
         // Retrieve the variables that previously went out of scope
