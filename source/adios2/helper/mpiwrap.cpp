69ba0ab8d75042ccc895aedaaaf96d69531c1ddf
Kai Germaschewski <kai.germaschewski@unh.edu>
2019-06-04 15:22:42 -0400

    mpiwrap: revamp the way the wrappers are generated
    There's just no pretty way of doing it (that I can come up with),
    but this way makes it clearer how we switch based on ADIOS2_HAVE_MPI,
    and it doesn't required the namespaced mpidummy in the
    !ADIOS2_HAVE_MPI case.

--- source/adios2/helper/mpiwrap.cpp
+++ source/adios2/helper/mpiwrap.cpp
@@ -9,7 +9,6 @@
 
 static inline bool SMPI_Available()
 {
-#ifdef ADIOS2_HAVE_MPI
     int flag;
     ::MPI_Initialized(&flag);
     if (!flag)
@@ -18,118 +17,71 @@ static inline bool SMPI_Available()
     }
     ::MPI_Finalized(&flag);
     return !flag;
-#else
-    // if compiled without MPI, always fall back to mpidummy
-    return false;
-#endif
 }
 
-#define MAKE_MPI_WRAPPER1(NAME, TARG1, ARG1)                                   \
-    int S##NAME(TARG1 ARG1)                                                    \
+#ifdef ADIOS2_HAVE_MPI
+
+// if MPI is available use it when possible, otherwise fall back to mpidummy
+#define DISPATCH(FUNC, ...)                                                    \
+    do                                                                         \
     {                                                                          \
         if (SMPI_Available())                                                  \
         {                                                                      \
-            return ::NAME(ARG1);                                               \
+            return ::FUNC(__VA_ARGS__);                                        \
         }                                                                      \
-        else                                                                   \
-        {                                                                      \
-            return adios2::helper::mpidummy::NAME(ARG1);                       \
-        }                                                                      \
-    }
+        return adios2::helper::mpidummy::FUNC(__VA_ARGS__);                    \
+    } while (0)
 
-#define MAKE_MPI_WRAPPER2(NAME, TARG1, ARG1, TARG2, ARG2)                      \
-    int S##NAME(TARG1 ARG1, TARG2 ARG2)                                        \
-    {                                                                          \
-        if (SMPI_Available())                                                  \
-        {                                                                      \
-            return ::NAME(ARG1, ARG2);                                         \
-        }                                                                      \
-        else                                                                   \
-        {                                                                      \
-            return adios2::helper::mpidummy::NAME(ARG1, ARG2);                 \
-        }                                                                      \
-    }
+#else
 
-#define MAKE_MPI_WRAPPER5(NAME, TARG1, ARG1, TARG2, ARG2, TARG3, ARG3, TARG4,  \
-                          ARG4, TARG5, ARG5)                                   \
-    int S##NAME(TARG1 ARG1, TARG2 ARG2, TARG3 ARG3, TARG4 ARG4, TARG5 ARG5)    \
-    {                                                                          \
-        if (SMPI_Available())                                                  \
-        {                                                                      \
-            return ::NAME(ARG1, ARG2, ARG3, ARG4, ARG5);                       \
-        }                                                                      \
-        else                                                                   \
-        {                                                                      \
-            return adios2::helper::mpidummy::NAME(ARG1, ARG2, ARG3, ARG4,      \
-                                                  ARG5);                       \
-        }                                                                      \
-    }
+// if compiled without MPI, always dispatch to mpidummy
+#define DISPATCH(FUNC, ...) return ::FUNC(__VA_ARGS__)
 
-#define MAKE_MPI_WRAPPER7(NAME, TARG1, ARG1, TARG2, ARG2, TARG3, ARG3, TARG4,  \
-                          ARG4, TARG5, ARG5, TARG6, ARG6, TARG7, ARG7)         \
-    int S##NAME(TARG1 ARG1, TARG2 ARG2, TARG3 ARG3, TARG4 ARG4, TARG5 ARG5,    \
-                TARG6 ARG6, TARG7 ARG7)                                        \
-    {                                                                          \
-        if (SMPI_Available())                                                  \
-        {                                                                      \
-            return ::NAME(ARG1, ARG2, ARG3, ARG4, ARG5, ARG6, ARG7);           \
-        }                                                                      \
-        else                                                                   \
-        {                                                                      \
-            return adios2::helper::mpidummy::NAME(ARG1, ARG2, ARG3, ARG4,      \
-                                                  ARG5, ARG6, ARG7);           \
-        }                                                                      \
-    }
+#endif
 
-#define MAKE_MPI_WRAPPER8(NAME, TARG1, ARG1, TARG2, ARG2, TARG3, ARG3, TARG4,  \
-                          ARG4, TARG5, ARG5, TARG6, ARG6, TARG7, ARG7, TARG8,  \
-                          ARG8)                                                \
-    int S##NAME(TARG1 ARG1, TARG2 ARG2, TARG3 ARG3, TARG4 ARG4, TARG5 ARG5,    \
-                TARG6 ARG6, TARG7 ARG7, TARG8 ARG8)                            \
-    {                                                                          \
-        if (SMPI_Available())                                                  \
-        {                                                                      \
-            return ::NAME(ARG1, ARG2, ARG3, ARG4, ARG5, ARG6, ARG7, ARG8);     \
-        }                                                                      \
-        else                                                                   \
-        {                                                                      \
-            return adios2::helper::mpidummy::NAME(ARG1, ARG2, ARG3, ARG4,      \
-                                                  ARG5, ARG6, ARG7, ARG8);     \
-        }                                                                      \
-    }
+int SMPI_Comm_dup(MPI_Comm comm, MPI_Comm *newcomm)
+{
+    DISPATCH(MPI_Comm_dup, comm, newcomm);
+}
 
-#define MAKE_MPI_WRAPPER9(NAME, TARG1, ARG1, TARG2, ARG2, TARG3, ARG3, TARG4,  \
-                          ARG4, TARG5, ARG5, TARG6, ARG6, TARG7, ARG7, TARG8,  \
-                          ARG8, TARG9, ARG9)                                   \
-    int S##NAME(TARG1 ARG1, TARG2 ARG2, TARG3 ARG3, TARG4 ARG4, TARG5 ARG5,    \
-                TARG6 ARG6, TARG7 ARG7, TARG8 ARG8, TARG9 ARG9)                \
-    {                                                                          \
-        if (SMPI_Available())                                                  \
-        {                                                                      \
-            return ::NAME(ARG1, ARG2, ARG3, ARG4, ARG5, ARG6, ARG7, ARG8,      \
-                          ARG9);                                               \
-        }                                                                      \
-        else                                                                   \
-        {                                                                      \
-            return adios2::helper::mpidummy::NAME(                             \
-                ARG1, ARG2, ARG3, ARG4, ARG5, ARG6, ARG7, ARG8, ARG9);         \
-        }                                                                      \
-    }
+int SMPI_Comm_free(MPI_Comm *comm) { DISPATCH(MPI_Comm_free, comm); }
 
-MAKE_MPI_WRAPPER2(MPI_Comm_dup, MPI_Comm, comm, MPI_Comm *, newcomm)
-MAKE_MPI_WRAPPER1(MPI_Comm_free, MPI_Comm *, newcomm)
-MAKE_MPI_WRAPPER2(MPI_Comm_rank, MPI_Comm, comm, int *, rank)
-MAKE_MPI_WRAPPER2(MPI_Comm_size, MPI_Comm, comm, int *, size)
-MAKE_MPI_WRAPPER1(MPI_Barrier, MPI_Comm, comm)
-MAKE_MPI_WRAPPER5(MPI_Bcast, void *, buffer, int, count, MPI_Datatype, datatype,
-                  int, root, MPI_Comm, comm)
-MAKE_MPI_WRAPPER8(MPI_Gather, const void *, sendbuf, int, sendcount,
-                  MPI_Datatype, sendtype, void *, recvbuf, int, recvcount,
-                  MPI_Datatype, recvtype, int, root, MPI_Comm, comm)
-MAKE_MPI_WRAPPER9(MPI_Gatherv, const void *, sendbuf, int, sendcount,
-                  MPI_Datatype, sendtype, void *, recvbuf, const int *,
-                  recvcounts, const int *, displs, MPI_Datatype, recvtype, int,
-                  root, MPI_Comm, comm)
-MAKE_MPI_WRAPPER7(MPI_Reduce, const void *, sendbuf, void *, recvbuf, int,
-                  count, MPI_Datatype, datatype, MPI_Op, op, int, root,
-                  MPI_Comm, comm)
+int SMPI_Comm_rank(MPI_Comm comm, int *rank)
+{
+    DISPATCH(MPI_Comm_rank, comm, rank);
+}
+
+int SMPI_Comm_size(MPI_Comm comm, int *size)
+{
+    DISPATCH(MPI_Comm_size, comm, size);
+}
+
+int SMPI_Barrier(MPI_Comm comm) { DISPATCH(MPI_Barrier, comm); }
+
+int SMPI_Bcast(void *buffer, int count, MPI_Datatype datatype, int root,
+               MPI_Comm comm)
+{
+    DISPATCH(MPI_Bcast, buffer, count, datatype, root, comm);
+}
+
+int SMPI_Gather(const void *sendbuf, int sendcount, MPI_Datatype sendtype,
+                void *recvbuf, int recvcount, MPI_Datatype recvtype, int root,
+                MPI_Comm comm)
+{
+    DISPATCH(MPI_Gather, sendbuf, sendcount, sendtype, recvbuf, recvcount,
+             recvtype, root, comm);
+}
+
+int SMPI_Gatherv(const void *sendbuf, int sendcount, MPI_Datatype sendtype,
+                 void *recvbuf, const int *recvcounts, const int *displs,
+                 MPI_Datatype recvtype, int root, MPI_Comm comm)
+{
+    DISPATCH(MPI_Gatherv, sendbuf, sendcount, sendtype, recvbuf, recvcounts,
+             displs, recvtype, root, comm);
+}
+
+int SMPI_Reduce(const void *sendbuf, void *recvbuf, int count,
+                MPI_Datatype datatype, MPI_Op op, int root, MPI_Comm comm)
+{
+    DISPATCH(MPI_Reduce, sendbuf, recvbuf, count, datatype, op, root, comm);
+}
