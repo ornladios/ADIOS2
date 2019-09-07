0d315d883914e5b6892729a8a0c0b81db67dc5c6
Ruonan Wang <jason.ruonan.wang@gmail.com>
2019-09-06 22:21:10 -0400

    added python examples for dataman, unified C++ exmaples and python examples

--- examples/hello/datamanReader/helloDataManReader.cpp
+++ examples/hello/datamanReader/helloDataManReader.cpp
@@ -2,7 +2,7 @@
  * Distributed under the OSI-approved Apache License, Version 2.0.  See
  * accompanying file Copyright.txt for details.
  *
- * helloDataManReader_nompi.cpp
+ * helloDataManReader.cpp
  *
  *  Created on: Jan 9, 2017
  *      Author: Jason Wang
@@ -15,9 +15,6 @@
 #include <numeric>
 #include <thread>
 #include <vector>
-
-adios2::Dims start({2, 3});
-adios2::Dims count({2, 3});
 
 int rank, size;
 
@@ -39,38 +36,36 @@ int main(int argc, char *argv[])
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    // initialize data
-    size_t datasize = std::accumulate(count.begin(), count.end(), 1,
-                                      std::multiplies<size_t>());
-    std::vector<float> myFloats(datasize);
-
     // initialize adios2
     adios2::ADIOS adios(MPI_COMM_WORLD, adios2::DebugON);
-    adios2::IO dataManIO = adios.DeclareIO("WAN");
+    adios2::IO dataManIO = adios.DeclareIO("whatever");
     dataManIO.SetEngine("DataMan");
-    dataManIO.SetParameters({{{"Library", "ZMQ"},
-                              {"IPAddress", "127.0.0.1"},
-                              {"Port", "12306"},
-                              {"Timeout", "5"}}});
+    dataManIO.SetParameters(
+        {{"IPAddress", "127.0.0.1"}, {"Port", "12306"}, {"Timeout", "5"}});
 
     // open stream
-    adios2::Engine dataManReader = dataManIO.Open("stream", adios2::Mode::Read);
+    adios2::Engine dataManReader =
+        dataManIO.Open("HelloDataMan", adios2::Mode::Read);
 
     // define variable
-    adios2::Variable<float> bpFloats;
+    adios2::Variable<float> floatArrayVar;
 
     // read data
+    std::vector<float> floatVector;
     while (true)
     {
         auto status = dataManReader.BeginStep();
         if (status == adios2::StepStatus::OK)
         {
-            bpFloats = dataManIO.InquireVariable<float>("bpFloats");
-            bpFloats.SetSelection({start, count});
-            dataManReader.Get<float>(bpFloats, myFloats.data(),
+            floatArrayVar = dataManIO.InquireVariable<float>("FloatArray");
+            auto shape = floatArrayVar.Shape();
+            size_t datasize = std::accumulate(shape.begin(), shape.end(), 1,
+                                              std::multiplies<size_t>());
+            floatVector.resize(datasize);
+            dataManReader.Get<float>(floatArrayVar, floatVector.data(),
                                      adios2::Mode::Sync);
-            PrintData(myFloats, dataManReader.CurrentStep());
             dataManReader.EndStep();
+            PrintData(floatVector, dataManReader.CurrentStep());
         }
         else if (status == adios2::StepStatus::EndOfStream)
         {
@@ -79,6 +74,7 @@ int main(int argc, char *argv[])
         }
     }
 
+    // clean up
     dataManReader.Close();
     MPI_Finalize();
 
