8c4846bf1a2b961f6cbe23c3bb46c5990c51a8b1
Ruonan Wang <jason.ruonan.wang@gmail.com>
2019-09-24 00:26:01 -0400

    cleaned up ssc

--- source/adios2/engine/ssc/SscReader.cpp
+++ source/adios2/engine/ssc/SscReader.cpp
@@ -8,15 +8,7 @@
  *      Author: Jason Wang
  */
 
-#include "SscReader.h"
 #include "SscReader.tcc"
-
-#include "adios2/helper/adiosFunctions.h"
-#include "adios2/toolkit/transport/file/FileFStream.h"
-
-#include <iostream>
-
-#include <zmq.h>
 
 namespace adios2
 {
@@ -27,307 +19,29 @@ namespace engine
 
 SscReader::SscReader(IO &io, const std::string &name, const Mode mode,
                      helper::Comm comm)
-: Engine("SscReader", io, name, mode, std::move(comm)),
-  m_DataManSerializer(m_Comm, helper::IsRowMajor(io.m_HostLanguage)),
-  m_RepliedMetadata(std::make_shared<std::vector<char>>())
+: Engine("SscReader", io, name, mode, std::move(comm))
 {
     TAU_SCOPED_TIMER_FUNC();
-
-    m_MpiRank = m_Comm.Rank();
-    srand(time(NULL));
-
-    // initialize parameters from IO
-    for (const auto &pair : m_IO.m_Parameters)
-    {
-        std::string key(pair.first);
-        std::transform(key.begin(), key.end(), key.begin(), ::tolower);
-        std::string value(pair.second);
-        std::transform(value.begin(), value.end(), value.begin(), ::tolower);
-        if (key == "verbose")
-        {
-            m_Verbosity = std::stoi(value);
-        }
-        else if (key == "timeout")
-        {
-            m_Timeout = std::stoi(value);
-        }
-    }
-
-    // retrieve all writer addresses
-    helper::HandshakeReader(m_Comm, m_AppID, m_FullAddresses, m_Name, "ssc");
-
-    // initialize transports
-    m_DataTransport.OpenRequester(m_Timeout, m_DataReceiverBufferSize);
-    m_MetadataTransport.OpenRequester(m_Timeout, m_MetadataReceiverBufferSize);
-
-    // request attributes from writer
-    auto reply = std::make_shared<std::vector<char>>();
-    if (m_MpiRank == 0)
-    {
-        std::vector<char> request(2 * sizeof(int64_t));
-        reinterpret_cast<int64_t *>(request.data())[0] = m_AppID;
-        reinterpret_cast<int64_t *>(request.data())[1] = -3;
-        std::string address = m_FullAddresses[rand() % m_FullAddresses.size()];
-        while (reply->size() < 6)
-        {
-            reply = m_MetadataTransport.Request(request.data(), request.size(),
-                                                address);
-        }
-    }
-    m_DataManSerializer.PutAggregatedMetadata(reply, m_Comm);
-    m_DataManSerializer.GetAttributes(m_IO);
-
-    if (m_Verbosity >= 5)
-    {
-        std::cout << "SscReader " << m_MpiRank << " Open(" << m_Name
-                  << ") in constructor." << std::endl;
-    }
 }
 
-SscReader::~SscReader()
-{
-    TAU_SCOPED_TIMER_FUNC();
-    Log(5, "SscReader::~SscReader()", true, true);
-}
-
-StepStatus SscReader::BeginStepIterator(StepMode stepMode,
-                                        format::DmvVecPtr &vars)
-{
-    TAU_SCOPED_TIMER_FUNC();
-
-    RequestMetadata();
-    m_MetaDataMap = m_DataManSerializer.GetFullMetadataMap();
-
-    if (m_MetaDataMap.empty())
-    {
-        Log(5,
-            "SscReader::BeginStepIterator() returned NotReady because the "
-            "metadata map is empty",
-            true, true);
-        return StepStatus::NotReady;
-    }
-
-    size_t maxStep = std::numeric_limits<size_t>::min();
-    for (auto &i : m_MetaDataMap)
-    {
-        if (i.first > maxStep)
-        {
-            maxStep = i.first;
-        }
-    }
-    if (maxStep <= m_CurrentStep)
-    {
-        Log(50,
-            "SscReader::BeginStepIterator() returned NotReady because no new "
-            "step is received yet",
-            true, true);
-        return StepStatus::NotReady;
-    }
-    else
-    {
-        m_CurrentStep = maxStep;
-    }
-
-    auto currentStepIt = m_MetaDataMap.find(m_CurrentStep);
-    if (currentStepIt == m_MetaDataMap.end())
-    {
-        Log(5,
-            "SscReader::BeginStepIterator() returned NotReady because the "
-            "current step is not existed in metadata map",
-            true, true);
-        return StepStatus::NotReady;
-    }
-    else
-    {
-        vars = currentStepIt->second;
-    }
-
-    if (vars == nullptr)
-    {
-        Log(5,
-            "SscReader::BeginStepIterator() returned NotReady because vars == "
-            "nullptr",
-            true, true);
-        return StepStatus::NotReady;
-    }
-
-    return StepStatus::OK;
-}
+SscReader::~SscReader() { TAU_SCOPED_TIMER_FUNC(); }
 
 StepStatus SscReader::BeginStep(const StepMode stepMode,
                                 const float timeoutSeconds)
 {
-
     TAU_SCOPED_TIMER_FUNC();
-    Log(5,
-        "SscReader::BeginStep() start. Last step " +
-            std::to_string(m_CurrentStep),
-        true, true);
-
-    ++m_CurrentStep;
-
-    format::DmvVecPtr vars = nullptr;
-
-    auto startTime = std::chrono::system_clock::now();
-
-    while (vars == nullptr)
-    {
-        auto stepStatus = BeginStepIterator(stepMode, vars);
-        if (stepStatus == StepStatus::OK)
-        {
-            break;
-        }
-        else if (stepStatus == StepStatus::EndOfStream)
-        {
-            return StepStatus::EndOfStream;
-        }
-        if (timeoutSeconds >= 0)
-        {
-            auto nowTime = std::chrono::system_clock::now();
-            auto duration = std::chrono::duration_cast<std::chrono::seconds>(
-                nowTime - startTime);
-            if (duration.count() > timeoutSeconds)
-            {
-                Log(5,
-                    "SscReader::BeginStep() returned EndOfStream because of "
-                    "timeout.",
-                    true, true);
-                --m_CurrentStep;
-                return StepStatus::EndOfStream;
-            }
-        }
-        else
-        {
-            return StepStatus::NotReady;
-        }
-    }
-
-    for (const auto &i : *vars)
-    {
-        if (i.step == m_CurrentStep)
-        {
-            if (i.type == "compound")
-            {
-                throw("Compound type is not supported yet.");
-            }
-#define declare_type(T)                                                        \
-    else if (i.type == helper::GetType<T>())                                   \
-    {                                                                          \
-        CheckIOVariable<T>(i.name, i.shape);                                   \
-    }
-            ADIOS2_FOREACH_STDTYPE_1ARG(declare_type)
-#undef declare_type
-            else
-            {
-                throw("Unknown type caught in "
-                      "DataManReader::BeginStepSubscribe.");
-            }
-        }
-    }
-
-    Log(5,
-        "SscReader::BeginStep() start. Last step " +
-            std::to_string(m_CurrentStep),
-        true, true);
-
     return StepStatus::OK;
 }
 
-void SscReader::PerformGets()
-{
+void SscReader::PerformGets() { TAU_SCOPED_TIMER_FUNC(); }
 
-    TAU_SCOPED_TIMER_FUNC();
-    Log(5, "SscReader::PerformGets() begin", true, true);
-
-    auto requests = m_DataManSerializer.GetDeferredRequest();
-
-    if (m_Verbosity >= 10)
-    {
-        Log(10, "SscReader::PerformGets() processing deferred requests ", true,
-            true);
-        for (const auto &i : *requests)
-        {
-            std::cout << i.first << ": ";
-            for (auto j : *i.second)
-            {
-                std::cout << j;
-            }
-            std::cout << std::endl;
-        }
-    }
-
-    for (const auto &i : *requests)
-    {
-        auto reply = m_DataTransport.Request(i.second->data(), i.second->size(),
-                                             i.first);
-        if (reply == nullptr or reply->size() <= 16)
-        {
-            m_ConnectionLost = true;
-            Log(1,
-                "Lost connection to writer. Data for the final step is "
-                "corrupted!",
-                true, true);
-        }
-        else
-        {
-            Log(6,
-                "SscReader::PerformGets() put reply of size " +
-                    std::to_string(reply->size()) + " into serializer",
-                true, true);
-            m_DataManSerializer.PutPack(reply);
-        }
-    }
-
-    auto varsVec = m_MetaDataMap.find(m_CurrentStep);
-    if (varsVec == m_MetaDataMap.end())
-    {
-        return;
-    }
-    if (varsVec->second == nullptr)
-    {
-        return;
-    }
-
-    for (const auto &req : m_DeferredRequests)
-    {
-        if (req.type == "compound")
-        {
-            throw("Compound type is not supported yet.");
-        }
-#define declare_type(T)                                                        \
-    else if (req.type == helper::GetType<T>())                                 \
-    {                                                                          \
-        m_DataManSerializer.GetVar(reinterpret_cast<T *>(req.data),            \
-                                   req.variable, req.start, req.count,         \
-                                   req.step);                                  \
-        Log(6,                                                                 \
-            "SscReader::PerformGets() get variable " + req.variable +          \
-                " from serializer",                                            \
-            true, true);                                                       \
-    }
-        ADIOS2_FOREACH_STDTYPE_1ARG(declare_type)
-#undef declare_type
-    }
-
-    m_DeferredRequests.clear();
-
-    Log(5, "SscReader::PerformGets() end", true, true);
-}
-
-size_t SscReader::CurrentStep() const { return m_CurrentStep; }
-
-void SscReader::EndStep()
+size_t SscReader::CurrentStep() const
 {
     TAU_SCOPED_TIMER_FUNC();
-    Log(5, "SscReader::EndStep() start. Step " + std::to_string(m_CurrentStep),
-        true, true);
-
-    PerformGets();
-    m_DataManSerializer.Erase(CurrentStep(), true);
-
-    Log(5, "SscReader::EndStep() end. Step " + std::to_string(m_CurrentStep),
-        true, true);
+    return m_CurrentStep;
 }
+
+void SscReader::EndStep() { TAU_SCOPED_TIMER_FUNC(); }
 
 // PRIVATE
 
@@ -353,45 +67,7 @@ void SscReader::EndStep()
 ADIOS2_FOREACH_STDTYPE_1ARG(declare_type)
 #undef declare_type
 
-void SscReader::RequestMetadata(const int64_t step)
-{
-    TAU_SCOPED_TIMER_FUNC();
-    format::VecPtr reply = std::make_shared<std::vector<char>>();
-    if (m_MpiRank == 0)
-    {
-        std::vector<char> request(2 * sizeof(int64_t));
-        reinterpret_cast<int64_t *>(request.data())[0] = m_AppID;
-        reinterpret_cast<int64_t *>(request.data())[1] = step;
-        std::string address = m_FullAddresses[rand() % m_FullAddresses.size()];
-        reply = m_MetadataTransport.Request(request.data(), request.size(),
-                                            address);
-    }
-    m_DataManSerializer.PutAggregatedMetadata(reply, m_Comm);
-}
-
-void SscReader::DoClose(const int transportIndex)
-{
-    TAU_SCOPED_TIMER_FUNC();
-    Log(5, "SscReader::DoClose()", true, true);
-}
-
-void SscReader::Log(const int level, const std::string &message, const bool mpi,
-                    const bool endline)
-{
-    TAU_SCOPED_TIMER_FUNC();
-    if (m_Verbosity >= level)
-    {
-        if (mpi)
-        {
-            std::cout << "[Rank " << m_MpiRank << "] ";
-        }
-        std::cout << message;
-        if (endline)
-        {
-            std::cout << std::endl;
-        }
-    }
-}
+void SscReader::DoClose(const int transportIndex) { TAU_SCOPED_TIMER_FUNC(); }
 
 } // end namespace engine
 } // end namespace core
--- source/adios2/engine/ssc/SscWriter.cpp
+++ source/adios2/engine/ssc/SscWriter.cpp
@@ -8,15 +8,7 @@
  *      Author: Jason Wang
  */
 
-#include "SscWriter.h"
 #include "SscWriter.tcc"
-
-#include "adios2/helper/adiosFunctions.h"
-#include "adios2/toolkit/transport/file/FileFStream.h"
-
-#include <iostream>
-
-#include <zmq.h>
 
 namespace adios2
 {
@@ -27,70 +19,28 @@ namespace engine
 
 SscWriter::SscWriter(IO &io, const std::string &name, const Mode mode,
                      helper::Comm comm)
-: Engine("SscWriter", io, name, mode, std::move(comm)),
-  m_DataManSerializer(m_Comm, helper::IsRowMajor(io.m_HostLanguage))
+: Engine("SscWriter", io, name, mode, std::move(comm))
 {
     TAU_SCOPED_TIMER_FUNC();
-    Init();
-    Log(5, "SscWriter::SscWriter()", true, true);
 }
 
 StepStatus SscWriter::BeginStep(StepMode mode, const float timeoutSeconds)
 {
     TAU_SCOPED_TIMER_FUNC();
-    Log(5,
-        "SscWriter::BeginStep() begin. Last step " +
-            std::to_string(m_CurrentStep),
-        true, true);
-
-    ++m_CurrentStep;
-    if (m_CurrentStep % m_StepsPerAggregation == 0)
-    {
-        m_CurrentStepActive = true;
-        m_DataManSerializer.NewWriterBuffer(m_SerializationBufferSize);
-        if (not m_AttributesSet)
-        {
-            m_DataManSerializer.PutAttributes(m_IO);
-            m_AttributesSet = true;
-        }
-    }
-    else
-    {
-        m_CurrentStepActive = false;
-    }
-
-    Log(5,
-        "SscWriter::BeginStep() end. New step " + std::to_string(m_CurrentStep),
-        true, true);
     return StepStatus::OK;
 }
 
-size_t SscWriter::CurrentStep() const { return m_CurrentStep; }
-
-void SscWriter::PerformPuts() {}
-
-void SscWriter::EndStep()
+size_t SscWriter::CurrentStep() const
 {
     TAU_SCOPED_TIMER_FUNC();
-    Log(5, "SscWriter::EndStep() begin. Step " + std::to_string(m_CurrentStep),
-        true, false);
-
-    if (m_CurrentStepActive)
-    {
-        m_DataManSerializer.PutPack(m_DataManSerializer.GetLocalPack());
-        m_DataManSerializer.AggregateMetadata();
-    }
-
-    if (m_CurrentStep > 5)
-    {
-        m_DataManSerializer.Erase(m_CurrentStep - 5, true);
-    }
-
-    Log(5, "SscWriter::EndStep() end. Step " + std::to_string(m_CurrentStep),
-        true, true);
+    return m_CurrentStep;
 }
 
-void SscWriter::Flush(const int transportIndex) {}
+void SscWriter::PerformPuts() { TAU_SCOPED_TIMER_FUNC(); }
+
+void SscWriter::EndStep() { TAU_SCOPED_TIMER_FUNC(); }
+
+void SscWriter::Flush(const int transportIndex) { TAU_SCOPED_TIMER_FUNC(); }
 
 // PRIVATE
 
@@ -106,168 +56,7 @@ void SscWriter::Flush(const int transportIndex) {}
 ADIOS2_FOREACH_STDTYPE_1ARG(declare_type)
 #undef declare_type
 
-void SscWriter::Init()
-{
-    TAU_SCOPED_TIMER_FUNC();
-    m_MpiRank = m_Comm.Rank();
-    m_MpiSize = m_Comm.Size();
-    srand(time(NULL));
-    InitParameters();
-    helper::HandshakeWriter(m_Comm, m_AppID, m_FullAddresses, m_Name, "ssc",
-                            m_Port, m_Channels, m_MaxRanksPerNode,
-                            m_MaxAppsPerNode);
-    InitTransports();
-}
-
-void SscWriter::InitParameters()
-{
-    TAU_SCOPED_TIMER_FUNC();
-    for (const auto &pair : m_IO.m_Parameters)
-    {
-        std::string key(pair.first);
-        std::transform(key.begin(), key.end(), key.begin(), ::tolower);
-        std::string value(pair.second);
-        std::transform(value.begin(), value.end(), value.begin(), ::tolower);
-        if (key == "verbose")
-        {
-            m_Verbosity = std::stoi(value);
-        }
-        else if (key == "port")
-        {
-            m_Port = std::stoi(value);
-        }
-        else if (key == "stepsperaggregation")
-        {
-            m_StepsPerAggregation = std::stoi(value);
-        }
-        else if (key == "timeout")
-        {
-            m_Timeout = std::stoi(value);
-        }
-    }
-}
-
-void SscWriter::InitTransports()
-{
-    TAU_SCOPED_TIMER_FUNC();
-    m_Listening = true;
-    for (const auto &address : m_FullAddresses)
-    {
-        m_ReplyThreads.emplace_back(
-            std::thread(&SscWriter::ReplyThread, this, address));
-    }
-}
-
-void SscWriter::ReplyThread(const std::string &address)
-{
-    adios2::zmq::ZmqReqRep replier;
-    replier.OpenReplier(address, m_Timeout, m_ReceiverBufferSize);
-    while (m_Listening)
-    {
-        auto request = replier.ReceiveRequest();
-        if (request == nullptr)
-        {
-            continue;
-        }
-        if (request->size() == 2 * sizeof(int64_t))
-        {
-            int64_t reader_id = reinterpret_cast<int64_t *>(request->data())[0];
-            int64_t stepRequested =
-                reinterpret_cast<int64_t *>(request->data())[1];
-            std::shared_ptr<std::vector<char>> aggMetadata = nullptr;
-            int64_t stepProvided = -1;
-            while (aggMetadata == nullptr)
-            {
-                if (stepRequested == -5) // let writer decide what to send
-                {
-                    aggMetadata = m_DataManSerializer.GetAggregatedMetadataPack(
-                        -2, stepProvided, m_AppID);
-                }
-                else
-                {
-                    aggMetadata = m_DataManSerializer.GetAggregatedMetadataPack(
-                        stepRequested, stepProvided, m_AppID);
-                }
-            }
-            replier.SendReply(aggMetadata);
-
-            if (m_Verbosity >= 100)
-            {
-                Log(100,
-                    "SscWriter::MetadataRepThread() sending metadata "
-                    "pack, size =  " +
-                        std::to_string(aggMetadata->size()),
-                    true, true);
-                std::cout << nlohmann::json::parse(*aggMetadata).dump(4)
-                          << std::endl;
-            }
-        }
-        else if (request->size() > 16)
-        {
-            size_t step;
-            m_CompressionParamsMutex.lock();
-            std::unordered_map<std::string, Params> p = m_CompressionParams;
-            m_CompressionParamsMutex.unlock();
-            auto reply = m_DataManSerializer.GenerateReply(*request, step, p);
-            replier.SendReply(reply);
-            if (reply->size() <= 16)
-            {
-                if (m_Tolerance)
-                {
-                    Log(1,
-                        "SscWriter::ReplyThread received data request but "
-                        "the step is already removed from buffer. Increase the"
-                        "buffer size to prevent this from happening again.",
-                        true, true);
-                }
-                else
-                {
-                    throw(std::runtime_error(
-                        "SscWriter::ReplyThread received data request but the "
-                        "step is already removed from buffer. Increase the "
-                        "buffer "
-                        "size to prevent this from happening again."));
-                }
-            }
-        }
-    }
-}
-
-void SscWriter::DoClose(const int transportIndex)
-{
-    std::this_thread::sleep_for(std::chrono::seconds(m_Timeout));
-    m_Comm.Barrier();
-    m_Listening = false;
-    for (auto &i : m_ReplyThreads)
-    {
-        if (i.joinable())
-        {
-            i.join();
-        }
-    }
-
-    remove(".staging");
-    remove(std::string(m_Name + ".staging").c_str());
-
-    Log(5, "SscWriter::DoClose(" + m_Name + ")", true, true);
-}
-void SscWriter::Log(const int level, const std::string &message, const bool mpi,
-                    const bool endline)
-{
-    TAU_SCOPED_TIMER_FUNC();
-    if (m_Verbosity >= level)
-    {
-        if (mpi)
-        {
-            std::cout << "[Rank " << m_MpiRank << "] ";
-        }
-        std::cout << message;
-        if (endline)
-        {
-            std::cout << std::endl;
-        }
-    }
-}
+void SscWriter::DoClose(const int transportIndex) { TAU_SCOPED_TIMER_FUNC(); }
 
 } // end namespace engine
 } // end namespace core
