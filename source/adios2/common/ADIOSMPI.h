1c1ea918a8241dc86f076e1843ef843316dc808f
Brad King <brad.king@kitware.com>
2019-10-15 14:49:32 -0400

    common: Remove unused ADIOS2_MPI_SIZE_T constant
    The `helper::Comm` encapsulation handles sending `size_t` data
    automatically.

--- source/adios2/common/ADIOSMPI.h
+++ source/adios2/common/ADIOSMPI.h
@@ -8,17 +8,4 @@
 
 #include "adios2/helper/mpiwrap.h"
 
-#include <climits> //UXXX_MAX
-#include <cstdint> //SIZE_MAX
-
-#if SIZE_MAX == UINT_MAX
-#define ADIOS2_MPI_SIZE_T MPI_UNSIGNED
-#elif SIZE_MAX == ULONG_MAX
-#define ADIOS2_MPI_SIZE_T MPI_UNSIGNED_LONG
-#elif SIZE_MAX == ULLONG_MAX
-#define ADIOS2_MPI_SIZE_T MPI_UNSIGNED_LONG_LONG
-#else
-#error "size_t could not be mapped to a MPI Type"
-#endif
-
 #endif /* ADIOS2_ADIOSMPI_H_ */
