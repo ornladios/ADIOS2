a8dbd2186ed0b37adb554a4dcb1a768386d47fe2
Greg Eisenhauer <eisen@cc.gatech.edu>
2019-12-15 05:59:05 -0500

    back out prior

--- source/adios2/toolkit/sst/cp/cp_common.c
+++ source/adios2/toolkit/sst/cp/cp_common.c
@@ -65,7 +65,7 @@ void CP_validateParams(SstStream Stream, SstParams Params, int Writer)
     if (Params->ControlTransport == NULL)
     {
         /* determine reasonable default, now "sockets" */
-        Params->ControlTransport = strdup("zplenet");
+        Params->ControlTransport = strdup("sockets");
     }
     else
     {
