6f31196e1711be5e2f8bfcdf5caf4ef6c7ba1189
Kyle Edwards <kyle.edwards@kitware.com>
2020-01-08 10:51:51 -0500

    Fix memory leak in QueryWorker

--- bindings/CXX11/adios2/cxx11/Query.cpp
+++ bindings/CXX11/adios2/cxx11/Query.cpp
@@ -1,6 +1,8 @@
 #include "Query.h"
 #include "adios2/toolkit/query/Worker.h"
 
+#include <utility>
+
 namespace adios2
 {
 QueryWorker::QueryWorker(const std::string &configFile, adios2::Engine &reader)
@@ -9,7 +11,8 @@ QueryWorker::QueryWorker(const std::string &configFile, adios2::Engine &reader)
         adios2::query::GetWorker(configFile, reader.m_Engine);
     if (m == nullptr)
         throw std::invalid_argument("ERROR: unable to construct query. ");
-    m_Worker = std::make_shared<adios2::query::Worker>(*m);
+    m_Worker = std::make_shared<adios2::query::Worker>(std::move(*m));
+    delete m;
 }
 
 void QueryWorker::GetResultCoverage(
