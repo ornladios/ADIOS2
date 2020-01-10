c5af3647e3e6bf5122a13f20500ca59d5e4d6a00
Jason Wang <wangr1@ornl.gov>
2020-01-10 15:12:04 -0500

    added test for ssc writer multiblock

--- testing/adios2/engine/ssc/TestSscWriterMultiblock.cpp
+++ testing/adios2/engine/ssc/TestSscWriterMultiblock.cpp
@@ -25,6 +25,34 @@ public:
     SscEngineTest() = default;
 };
 
+void PrintData(const char *data, const size_t step, const Dims &start,
+               const Dims &count)
+{
+    size_t size = std::accumulate(count.begin(), count.end(), 1,
+                                  std::multiplies<size_t>());
+    std::cout << "Rank: " << mpiRank << " Step: " << step << " Size:" << size
+              << "\n";
+    size_t printsize = 128;
+
+    if (size < printsize)
+    {
+        printsize = size;
+    }
+    int s = 0;
+    for (size_t i = 0; i < printsize; ++i)
+    {
+        ++s;
+        std::cout << (int)(data[i]) << " ";
+        if (s == count[1])
+        {
+            std::cout << std::endl;
+            s = 0;
+        }
+    }
+
+    std::cout << "]" << std::endl;
+}
+
 template <class T>
 void PrintData(const T *data, const size_t step, const Dims &start,
                const Dims &count)
@@ -57,7 +85,7 @@ void PrintData(const T *data, const size_t step, const Dims &start,
 template <class T>
 void GenDataRecursive(std::vector<size_t> start, std::vector<size_t> count,
                       std::vector<size_t> shape, size_t n0, size_t y,
-                      std::vector<T> &vec)
+                      std::vector<T> &vec, const size_t step)
 {
     for (size_t i = 0; i < count[0]; i++)
     {
@@ -76,12 +104,13 @@ void GenDataRecursive(std::vector<size_t> start, std::vector<size_t> count,
             for (size_t j = 0; j < count_next[0]; j++)
             {
                 vec[i0 * count_next[0] + j] =
-                    z * shape_next[0] + (j + start_next[0]);
+                    z * shape_next[0] + (j + start_next[0]) + step;
             }
         }
         else
         {
-            GenDataRecursive(start_next, count_next, shape_next, i0, z, vec);
+            GenDataRecursive(start_next, count_next, shape_next, i0, z, vec,
+                             step);
         }
     }
 }
@@ -94,47 +123,53 @@ void GenData(std::vector<T> &vec, const size_t step,
     size_t total_size = std::accumulate(count.begin(), count.end(), 1,
                                         std::multiplies<size_t>());
     vec.resize(total_size);
-    GenDataRecursive(start, count, shape, 0, 0, vec);
+    GenDataRecursive(start, count, shape, 0, 0, vec, step);
 }
 
 template <class T>
-void VerifyData(const std::complex<T> *data, size_t step, const Dims &start,
-                const Dims &count, const Dims &shape)
+void VerifyData(const std::complex<T> *received_data, size_t step,
+                const Dims &start, const Dims &count, const Dims &shape,
+                const std::string &varName)
 {
     size_t size = std::accumulate(count.begin(), count.end(), 1,
                                   std::multiplies<size_t>());
-    std::vector<std::complex<T>> tmpdata(size);
-    GenData(tmpdata, step, start, count, shape);
+    if (print_lines < 32)
+    {
+        std::cout << "Verifying Variable " << varName << " for Step " << step
+                  << std::endl;
+        PrintData(received_data, step, start, count);
+        ++print_lines;
+    }
+    std::vector<std::complex<T>> generated_data(size);
+    GenData(generated_data, step, start, count, shape);
     for (size_t i = 0; i < size; ++i)
     {
-        ASSERT_EQ(data[i], tmpdata[i]);
-    }
-    if (print_lines < 32)
-    {
-        PrintData(data, step, start, count);
-        ++print_lines;
+        ASSERT_EQ(received_data[i], generated_data[i]);
     }
 }
 
 template <class T>
-void VerifyData(const T *data, size_t step, const Dims &start,
-                const Dims &count, const Dims &shape)
+void VerifyData(const T *received_data, size_t step, const Dims &start,
+                const Dims &count, const Dims &shape,
+                const std::string &varName)
 {
     size_t size = std::accumulate(count.begin(), count.end(), 1,
                                   std::multiplies<size_t>());
-    bool compressed = false;
-    std::vector<T> tmpdata(size);
     if (print_lines < 32)
     {
-        PrintData(data, step, start, count);
+        std::cout << "Verifying Variable " << varName << " for Step " << step
+                  << std::endl;
+        PrintData(received_data, step, start, count);
         ++print_lines;
     }
-    GenData(tmpdata, step, start, count, shape);
+    bool compressed = false;
+    std::vector<T> generated_data(size);
+    GenData(generated_data, step, start, count, shape);
     for (size_t i = 0; i < size; ++i)
     {
         if (!compressed)
         {
-            ASSERT_EQ(data[i], tmpdata[i]);
+            ASSERT_EQ(received_data[i], generated_data[i]);
         }
     }
 }
@@ -178,11 +213,15 @@ void Writer(const Dims &shape, const Dims &start, const Dims &count,
         "bpComplexes", shape, start, count);
     auto bpDComplexes = dataManIO.DefineVariable<std::complex<double>>(
         "bpDComplexes", shape, start, count);
+    auto scalarInt = dataManIO.DefineVariable<int>("scalarInt");
     dataManIO.DefineAttribute<int>("AttInt", 110);
     adios2::Engine dataManWriter = dataManIO.Open(name, adios2::Mode::Write);
     for (int i = 0; i < steps; ++i)
     {
         dataManWriter.BeginStep();
+
+        Dims start = {(size_t)mpiRank * 2, 0};
+
         GenData(myChars, i, start, count, shape);
         GenData(myUChars, i, start, count, shape);
         GenData(myShorts, i, start, count, shape);
@@ -193,6 +232,18 @@ void Writer(const Dims &shape, const Dims &start, const Dims &count,
         GenData(myDoubles, i, start, count, shape);
         GenData(myComplexes, i, start, count, shape);
         GenData(myDComplexes, i, start, count, shape);
+
+        bpChars.SetSelection({start, count});
+        bpUChars.SetSelection({start, count});
+        bpShorts.SetSelection({start, count});
+        bpUShorts.SetSelection({start, count});
+        bpInts.SetSelection({start, count});
+        bpUInts.SetSelection({start, count});
+        bpFloats.SetSelection({start, count});
+        bpDoubles.SetSelection({start, count});
+        bpComplexes.SetSelection({start, count});
+        bpDComplexes.SetSelection({start, count});
+
         dataManWriter.Put(bpChars, myChars.data(), adios2::Mode::Sync);
         dataManWriter.Put(bpUChars, myUChars.data(), adios2::Mode::Sync);
         dataManWriter.Put(bpShorts, myShorts.data(), adios2::Mode::Sync);
@@ -204,6 +255,45 @@ void Writer(const Dims &shape, const Dims &start, const Dims &count,
         dataManWriter.Put(bpComplexes, myComplexes.data(), adios2::Mode::Sync);
         dataManWriter.Put(bpDComplexes, myDComplexes.data(),
                           adios2::Mode::Sync);
+        dataManWriter.Put(scalarInt, i);
+
+        start = {(size_t)mpiRank * 2 + 1, 0};
+
+        GenData(myChars, i, start, count, shape);
+        GenData(myUChars, i, start, count, shape);
+        GenData(myShorts, i, start, count, shape);
+        GenData(myUShorts, i, start, count, shape);
+        GenData(myInts, i, start, count, shape);
+        GenData(myUInts, i, start, count, shape);
+        GenData(myFloats, i, start, count, shape);
+        GenData(myDoubles, i, start, count, shape);
+        GenData(myComplexes, i, start, count, shape);
+        GenData(myDComplexes, i, start, count, shape);
+
+        bpChars.SetSelection({start, count});
+        bpUChars.SetSelection({start, count});
+        bpShorts.SetSelection({start, count});
+        bpUShorts.SetSelection({start, count});
+        bpInts.SetSelection({start, count});
+        bpUInts.SetSelection({start, count});
+        bpFloats.SetSelection({start, count});
+        bpDoubles.SetSelection({start, count});
+        bpComplexes.SetSelection({start, count});
+        bpDComplexes.SetSelection({start, count});
+
+        dataManWriter.Put(bpChars, myChars.data(), adios2::Mode::Sync);
+        dataManWriter.Put(bpUChars, myUChars.data(), adios2::Mode::Sync);
+        dataManWriter.Put(bpShorts, myShorts.data(), adios2::Mode::Sync);
+        dataManWriter.Put(bpUShorts, myUShorts.data(), adios2::Mode::Sync);
+        dataManWriter.Put(bpInts, myInts.data(), adios2::Mode::Sync);
+        dataManWriter.Put(bpUInts, myUInts.data(), adios2::Mode::Sync);
+        dataManWriter.Put(bpFloats, myFloats.data(), adios2::Mode::Sync);
+        dataManWriter.Put(bpDoubles, myDoubles.data(), adios2::Mode::Sync);
+        dataManWriter.Put(bpComplexes, myComplexes.data(), adios2::Mode::Sync);
+        dataManWriter.Put(bpDComplexes, myDComplexes.data(),
+                          adios2::Mode::Sync);
+        dataManWriter.Put(scalarInt, i);
+
         dataManWriter.EndStep();
     }
     dataManWriter.Close();
@@ -247,7 +337,7 @@ void Reader(const Dims &shape, const Dims &start, const Dims &count,
                 }
                 std::cout << std::endl;
             }
-            ASSERT_EQ(vars.size(), 10);
+            ASSERT_EQ(vars.size(), 11);
             size_t currentStep = dataManReader.CurrentStep();
             adios2::Variable<char> bpChars =
                 dataManIO.InquireVariable<char>("bpChars");
@@ -269,6 +359,7 @@ void Reader(const Dims &shape, const Dims &start, const Dims &count,
                 dataManIO.InquireVariable<std::complex<float>>("bpComplexes");
             adios2::Variable<std::complex<double>> bpDComplexes =
                 dataManIO.InquireVariable<std::complex<double>>("bpDComplexes");
+            auto scalarInt = dataManIO.InquireVariable<int>("scalarInt");
             auto charsBlocksInfo = dataManReader.AllStepsBlocksInfo(bpChars);
 
             dataManReader.Get(bpChars, myChars.data(), adios2::Mode::Sync);
@@ -283,26 +374,31 @@ void Reader(const Dims &shape, const Dims &start, const Dims &count,
                               adios2::Mode::Sync);
             dataManReader.Get(bpDComplexes, myDComplexes.data(),
                               adios2::Mode::Sync);
-            VerifyData(myChars.data(), currentStep, Dims(shape.size(), 0),
-                       shape, shape);
-            VerifyData(myUChars.data(), currentStep, Dims(shape.size(), 0),
-                       shape, shape);
-            VerifyData(myShorts.data(), currentStep, Dims(shape.size(), 0),
-                       shape, shape);
-            VerifyData(myUShorts.data(), currentStep, Dims(shape.size(), 0),
-                       shape, shape);
-            VerifyData(myInts.data(), currentStep, Dims(shape.size(), 0), shape,
-                       shape);
-            VerifyData(myUInts.data(), currentStep, Dims(shape.size(), 0),
-                       shape, shape);
-            VerifyData(myFloats.data(), currentStep, Dims(shape.size(), 0),
-                       shape, shape);
-            VerifyData(myDoubles.data(), currentStep, Dims(shape.size(), 0),
-                       shape, shape);
-            VerifyData(myComplexes.data(), currentStep, Dims(shape.size(), 0),
-                       shape, shape);
-            VerifyData(myDComplexes.data(), currentStep, Dims(shape.size(), 0),
-                       shape, shape);
+            int i;
+            dataManReader.Get(scalarInt, &i);
+
+            ASSERT_EQ(i, currentStep);
+
+            VerifyData(myChars.data(), currentStep, start, shape, shape,
+                       "bpChars");
+            VerifyData(myUChars.data(), currentStep, start, shape, shape,
+                       "bpUChars");
+            VerifyData(myShorts.data(), currentStep, start, shape, shape,
+                       "bpShorts");
+            VerifyData(myUShorts.data(), currentStep, start, shape, shape,
+                       "bpUShorts");
+            VerifyData(myInts.data(), currentStep, start, shape, shape,
+                       "bpInts");
+            VerifyData(myUInts.data(), currentStep, start, shape, shape,
+                       "bpUInts");
+            VerifyData(myFloats.data(), currentStep, start, shape, shape,
+                       "bpFloats");
+            VerifyData(myDoubles.data(), currentStep, start, shape, shape,
+                       "bpDoubles");
+            VerifyData(myComplexes.data(), currentStep, start, shape, shape,
+                       "bpComplexes");
+            VerifyData(myDComplexes.data(), currentStep, start, shape, shape,
+                       "bpDComplexes");
             dataManReader.EndStep();
         }
         else if (status == adios2::StepStatus::EndOfStream)
@@ -313,14 +409,19 @@ void Reader(const Dims &shape, const Dims &start, const Dims &count,
             break;
         }
     }
+    auto attInt = dataManIO.InquireAttribute<int>("AttInt");
+    std::cout << "[Rank " + std::to_string(mpiRank) + "] Attribute received "
+              << attInt.Data()[0] << ", expected 110" << std::endl;
+    ASSERT_EQ(110, attInt.Data()[0]);
+    ASSERT_NE(111, attInt.Data()[0]);
     dataManReader.Close();
     print_lines = 0;
 }
 
-TEST_F(SscEngineTest, TestSsc7d)
+TEST_F(SscEngineTest, TestSscWriterMultiblock)
 {
-    std::string filename = "TestSsc7d";
-    adios2::Params engineParams = {{"Port", "12306"}, {"Verbose", "0"}};
+    std::string filename = "TestSscWriterMultiblock";
+    adios2::Params engineParams = {{"Port", "12316"}, {"Verbose", "0"}};
 
     int worldRank, worldSize;
     MPI_Comm_rank(MPI_COMM_WORLD, &worldRank);
@@ -331,13 +432,13 @@ TEST_F(SscEngineTest, TestSsc7d)
     MPI_Comm_rank(mpiComm, &mpiRank);
     MPI_Comm_size(mpiComm, &mpiSize);
 
-    Dims shape = {10, 2, 2, (size_t)mpiSize, 2, 8, 10};
-    Dims start = {0, 0, 0, (size_t)mpiRank, 0, 0, 0};
-    Dims count = {10, 2, 2, 1, 2, 8, 10};
-    size_t steps = 200;
+    size_t steps = 100;
 
     if (mpiGroup == 0)
     {
+        Dims shape = {(size_t)mpiSize * 2, 10};
+        Dims start = {(size_t)mpiRank * 2, 0};
+        Dims count = {1, 10};
         Writer(shape, start, count, steps, engineParams, filename);
     }
 
@@ -345,6 +446,9 @@ TEST_F(SscEngineTest, TestSsc7d)
 
     if (mpiGroup == 1)
     {
+        Dims shape = {(size_t)mpiSize * 2, 10};
+        Dims start = {0, 0};
+        Dims count = {(size_t)mpiSize * 2, 10};
         Reader(shape, start, count, steps, engineParams, filename);
     }
 
