08032cf9c02d132f00289db5d6d7e8b398d28379
Xin Xu <xxu30@Xins-MacBook-Pro.local>
2020-01-22 20:27:50 -0500

    Add MPI communicator info for multiple applications for ssc engine.

--- source/adios2/engine/ssc/SscReader.cpp
+++ source/adios2/engine/ssc/SscReader.cpp
@@ -117,6 +117,242 @@ void SscReader::SyncMpiPattern()
         std::cout << "SscReader::SyncMpiPattern, World Rank " << m_WorldRank
                   << ", Reader Rank " << m_ReaderRank << std::endl;
     }
+
+
+    std::vector<int> lrbuf;
+    std::vector<int> grbuf;
+    std::vector<int> m_tmp;
+    int m_AppID = 0;
+    int m_AppSize = 0;  //Total number of applications.
+    std::vector<std::vector<int>> m_WriterGlobalMpiInfo;
+    std::vector<std::vector<int>> m_ReaderGlobalMpiInfo;
+
+
+    // Process m_WorldRank == 0 to gather all the local rank m_WriterRank, and find out all the m_WriterRank == 0
+    if (m_WorldRank == 0)
+    {
+        grbuf.resize(m_WorldSize);
+    }
+
+    m_tmp.push_back(m_ReaderRank);
+    MPI_Barrier(MPI_COMM_WORLD);
+    MPI_Gather( m_tmp.data(), 1, MPI_INT, grbuf.data(), 1, MPI_INT, 0, MPI_COMM_WORLD);
+    m_tmp.clear();
+    MPI_Barrier(MPI_COMM_WORLD);
+    /*if (m_WorldRank == 0)
+    {
+        //std::cout << "All local ranks:" << std::endl;
+        for (int i = 0; i < grbuf.size(); ++i)
+        {
+            std::cout << grbuf[i] << ", ";
+        }
+        std::cout << std::endl << std::endl;
+    }*/
+
+
+    std::vector<int> AppStart;  // m_WorldRank of the local rank 0 process
+    if (m_WorldRank == 0)
+    {
+        for ( int i = 0; i < m_WorldSize; ++i )
+        {
+            if (grbuf[i] == 0)
+            {
+                AppStart.push_back(i);
+            }
+        }
+        m_AppSize = AppStart.size();
+        //std::cout << "There are " << m_AppSize << " applications." << std::endl;
+    }
+
+
+    //Each local rank 0 process send their type (0 for writer, 1 for reader) to the world rank 0 process
+    //The AppStart are re-ordered to put all writers ahead of all the readers.
+    std::vector<int> AppType;  // Vector to record the type of the local rank 0 process
+    if (m_ReaderRank == 0) // Send type from each local rank 0 process to the world rank 0 process
+    {
+        if (m_WorldRank == 0) // App_ID
+        {
+            //std::cout << "ReaderReceiving!" << std::endl;
+            for (int i = 0; i < m_AppSize; ++i)
+            {
+                if (i == 0)
+                {
+                    AppType.push_back(1);
+                }
+                else
+                {
+                    m_tmp.resize(1);
+                    MPI_Recv(m_tmp.data(), 1, MPI_INT, AppStart[i], 96, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+                    AppType.push_back(m_tmp[0]);
+                    m_tmp.clear();
+                }
+            }
+        }
+        else
+        {
+            //std::cout << "Reader Sending!" << std::endl;
+            m_tmp.push_back(1); // type 1 for reader
+            MPI_Send( m_tmp.data(), 1, MPI_INT, 0, 96, MPI_COMM_WORLD); //
+            m_tmp.clear();
+        }
+    }
+
+    if (m_WorldRank == 0)
+    {
+        std::vector<int> AppWriter;
+        std::vector<int> AppReader;
+
+        for (int i = 0; i < m_AppSize; ++i)
+        {
+            if (AppType[i] == 0)
+            {
+                AppWriter.push_back(AppStart[i]);
+            }
+            else
+            {
+                AppReader.push_back(AppStart[i]);
+            }
+        }
+        m_WriterGlobalMpiInfo.resize(AppWriter.size());
+        m_ReaderGlobalMpiInfo.resize(AppReader.size());
+        AppStart = AppWriter;
+        AppStart.insert(AppStart.end(), AppReader.begin(), AppReader.end());
+
+        /*std::cout << "AppStart_Reader = ";
+        for (int i = 0;  i < m_AppSize; ++i)
+        {
+            std::cout << AppStart[i] << "   ";
+        }
+        std::cout << std::endl;*/
+    }
+
+
+    // Send the m_AppSize and m_AppID to each local rank 0 process
+    if (m_ReaderRank == 0) // Send m_AppID to each local rank 0 process
+    {
+        if (m_WorldRank == 0) // App_ID
+        {
+            for (int i = 0; i < m_AppSize; ++i)
+            {
+                m_tmp.push_back(i);
+                MPI_Send( m_tmp.data(), 1, MPI_INT, AppStart[i], 99, MPI_COMM_WORLD); //
+                m_tmp.clear();
+            }
+        }
+        else
+        {
+            m_tmp.push_back(m_AppID);
+            MPI_Recv(m_tmp.data(), 1, MPI_INT, 0, 99, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            m_AppID = m_tmp[0];
+            m_tmp.clear();
+        }
+    }
+
+    m_tmp.push_back(m_AppID);
+    m_Comm.Barrier();
+    m_Comm.Bcast(m_tmp.data() , sizeof(int), 0);  // Local rank 0 process broadcast the m_AppID within the local communicator.
+    m_AppID = m_tmp[0];
+    m_tmp.clear();
+    //std::cout << "World Process " << m_WorldRank << " is local Reader " << m_ReaderRank << ". AppID is " << m_AppID << ". " << std::endl;
+    m_Comm.Barrier();
+
+
+    m_tmp.push_back(m_AppSize);
+    MPI_Barrier(MPI_COMM_WORLD);
+    MPI_Bcast( m_tmp.data(), 1, MPI_INT, 0, MPI_COMM_WORLD); // Bcast the m_AppSize
+    m_AppSize = m_tmp[0];
+    m_tmp.clear();
+    //std::cout << "World Process " << m_WorldRank << " is local Reader " << m_ReaderRank << ". AppSize is " << m_AppSize << ". " << std::endl;
+    MPI_Barrier(MPI_COMM_WORLD);
+
+
+    // In each local communicator, each local rank 0 process gathers the world rank of all the rest local processes.
+    if (m_ReaderRank == 0)
+    {
+        //std::cout << "Reader size is " << m_ReaderSize << "." << std::endl;
+        lrbuf.resize(m_ReaderSize);
+    }
+
+    m_tmp.push_back(m_WorldRank);
+    //std::cout << "Reader Local Rank to send: " << m_tmp[0] << std::endl;
+    m_Comm.Gather(m_tmp.data(), 1, lrbuf.data(), 1, 0);
+    m_tmp.clear();
+
+    if (m_ReaderRank == 0)
+    {
+        std::cout << "App " << m_AppID << " has ";
+        for (int i = 0; i < lrbuf.size(); ++i)
+        {
+            std::cout << "world rank " << lrbuf[i] << ", ";
+        }
+        std::cout << " in it." << std::endl;
+    }
+
+
+    // Send the WorldRank vector of each local communicator to the m_WorldRank == 0 process.
+    int WriterInfoSize = 0;
+    int ReaderInfoSize = 0;
+    if (m_ReaderRank == 0)
+    {
+        if (m_WorldRank == 0) // App_ID
+        {
+            for (int i = 0; i < m_WriterGlobalMpiInfo.size(); ++i)
+            {
+                if (i == 0)
+                {
+                    m_WriterGlobalMpiInfo[i] = lrbuf;
+                    WriterInfoSize += lrbuf.size();
+                }
+                else
+                {
+                    m_tmp.resize(1);
+                    MPI_Recv( m_tmp.data(), 1, MPI_INT, AppStart[i], 96, MPI_COMM_WORLD, MPI_STATUS_IGNORE); //
+                    int j_writersize = m_tmp[0];
+                    WriterInfoSize += j_writersize;
+                    m_tmp.clear();
+
+                    m_tmp.resize(j_writersize);
+                    MPI_Recv( m_tmp.data(), j_writersize, MPI_INT, AppStart[i], 98, MPI_COMM_WORLD, MPI_STATUS_IGNORE); //
+                    m_WriterGlobalMpiInfo[i] = m_tmp;
+                    m_tmp.clear();
+                }
+            }
+
+            for (int i = m_WriterGlobalMpiInfo.size(); i < m_AppSize; ++i)
+            {
+                if (i == 0)
+                {
+                    m_ReaderGlobalMpiInfo[i] = lrbuf;
+                    ReaderInfoSize += lrbuf.size();
+                }
+                else
+                {
+                    m_tmp.resize(1);
+                    MPI_Recv( m_tmp.data(), 1, MPI_INT, AppStart[i], 95, MPI_COMM_WORLD, MPI_STATUS_IGNORE); //
+                    int j_readersize = m_tmp[0];
+                    ReaderInfoSize += j_readersize;
+                    m_tmp.clear();
+
+                    m_tmp.resize(j_readersize);
+                    MPI_Recv( m_tmp.data(), 1, MPI_INT, AppStart[i], 97, MPI_COMM_WORLD, MPI_STATUS_IGNORE); //
+                    m_ReaderGlobalMpiInfo[i] = m_tmp;
+                    m_tmp.clear();
+                }
+            }
+
+        }
+        else
+        {
+            m_tmp.push_back(m_ReaderSize);
+            MPI_Send(m_tmp.data(), 1, MPI_INT, 0, 95, MPI_COMM_WORLD);
+            m_tmp.clear();
+
+            MPI_Send(lrbuf.data(), lrbuf.size(), MPI_INT, 0, 97, MPI_COMM_WORLD);
+        }
+    }
+
+    std::cout << "Reader Reached???" << std::endl;
+
 }
 
 void SscReader::SyncWritePattern()
--- source/adios2/engine/ssc/SscWriter.cpp
+++ source/adios2/engine/ssc/SscWriter.cpp
@@ -13,6 +13,8 @@
 #include "adios2/helper/adiosJSONcomplex.h"
 #include "nlohmann/json.hpp"
 
+#include "adios2/helper/adiosCommMPI.h"
+
 namespace adios2
 {
 namespace core
@@ -34,6 +36,22 @@ SscWriter::SscWriter(IO &io, const std::string &name, const Mode mode,
     m_GlobalWritePattern.resize(m_WriterSize);
     m_GlobalReadPattern.resize(m_ReaderSize);
 
+    /*
+    int lsize;
+    int *rbuf;
+    ...
+    if (m_WriterRank == 0)
+    {
+        MPI_Comm_size(m_Comm, &lsize);
+        rbuf = (int*)malloc(lsize*1*sizeof(int));
+    }
+
+
+    //MPI_Comm_size( m_Comm, &lsize);
+    // MPI_Gather(sendarray, 100, MPI_INT, rbuf, 100, MPI_INT,  root = 0, m_Comm);
+    MPI_Gather(m_WriterRank, 1, MPI_INT, rbuf, 1, MPI_INT, 0, m_Comm);
+
+    */
     SyncMpiPattern();
 }
 
@@ -120,6 +138,304 @@ void SscWriter::SyncMpiPattern()
         std::cout << "SscWriter::SyncMpiPattern, World Rank " << m_WorldRank
                   << ", Writer Rank " << m_WriterRank << std::endl;
     }
+
+
+    std::vector<int> lrbuf;
+    std::vector<int> grbuf;
+    std::vector<int> m_tmp;
+    int m_AppID = 0;
+    int m_AppSize = 0;  //Total number of applications.
+    std::vector<std::vector<int>> m_WriterGlobalMpiInfo;
+    std::vector<std::vector<int>> m_ReaderGlobalMpiInfo;
+    //int WriterNum;
+
+
+    // Process m_WorldRank == 0 to gather all the local rank m_WriterRank, and find out all the m_WriterRank == 0
+    if (m_WorldRank == 0)
+    {
+        grbuf.resize(m_WorldSize);
+    }
+
+    m_tmp.push_back(m_WriterRank);
+    MPI_Barrier(MPI_COMM_WORLD);
+    MPI_Gather( m_tmp.data(), 1, MPI_INT, grbuf.data(), 1, MPI_INT, 0, MPI_COMM_WORLD);
+    m_tmp.clear();
+    MPI_Barrier(MPI_COMM_WORLD);
+    if (m_WorldRank == 0)
+    {
+        //std::cout << "All local ranks:" << std::endl;
+        for (int i = 0; i < grbuf.size(); ++i)
+        {
+            std::cout << grbuf[i] << ", ";
+        }
+        std::cout << std::endl << std::endl;
+    }
+
+
+    std::vector<int> AppStart;  // m_WorldRank of the local rank 0 process
+    if (m_WorldRank == 0)
+    {
+        for ( int i = 0; i < m_WorldSize; ++i )
+        {
+            if (grbuf[i] == 0)
+            {
+                AppStart.push_back(i);
+            }
+        }
+        m_AppSize = AppStart.size();
+        //std::cout << "There are " << m_AppSize << " applications." << std::endl;
+    }
+
+
+    //Each local rank 0 process send their type (0 for writer, 1 for reader) to the world rank 0 process
+    //The AppStart are re-ordered to put all writers ahead of all the readers.
+    std::vector<int> AppType;  // Vector to record the type of the local rank 0 process
+    if (m_WriterRank == 0) // Send type from each local rank 0 process to the world rank 0 process
+    {
+        if (m_WorldRank == 0) // App_ID
+        {
+            //std::cout << "Writer Receiving!" << std::endl;
+            for (int i = 0; i < m_AppSize; ++i)
+            {
+                if (i == 0)
+                {
+                    AppType.push_back(0);
+                }
+                else
+                {
+                    m_tmp.resize(1);
+                    MPI_Recv(m_tmp.data(), 1, MPI_INT, AppStart[i], 96, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+                    AppType.push_back(m_tmp[0]);
+                    m_tmp.clear();
+                }
+            }
+            /*
+            std::cout << "AppType = ";
+            for (int i = 0;  i < m_AppSize; ++i)
+            {
+                std::cout << AppStart[i] << "   " << AppType[i] << std::endl;
+            }
+            std::cout << std::endl;
+            */
+        }
+        else
+        {
+            //std::cout << "Writer Sending!" << std::endl;
+            m_tmp.push_back(0); // type 0 for writer
+            MPI_Send( m_tmp.data(), 1, MPI_INT, 0, 96, MPI_COMM_WORLD); //
+            m_tmp.clear();
+        }
+    }
+
+    if (m_WorldRank == 0)
+    {
+        std::vector<int> AppWriter;
+        std::vector<int> AppReader;
+
+        for (int i = 0; i < m_AppSize; ++i)
+        {
+            if (AppType[i] == 0)
+            {
+                AppWriter.push_back(AppStart[i]);
+                //std::cout << "Writer push!" << std::endl;
+            }
+            else
+            {
+                AppReader.push_back(AppStart[i]);
+                //std::cout << "Reader push!" << std::endl;
+            }
+        }
+        m_WriterGlobalMpiInfo.resize(AppWriter.size());
+        //std::cout << "WriterInfo" << m_WriterGlobalMpiInfo.size() << std::endl;
+        m_ReaderGlobalMpiInfo.resize(AppReader.size());
+        //std::cout << "ReaderInfo" << m_ReaderGlobalMpiInfo.size() << std::endl;
+        AppStart = AppWriter;
+        AppStart.insert(AppStart.end(), AppReader.begin(), AppReader.end());
+
+        /*std::cout << "AppStart_Writer = ";
+        for (int i = 0;  i < m_AppSize; ++i)
+        {
+            std::cout << AppStart[i] << "   ";
+        }
+        std::cout << std::endl;*/
+    }
+
+
+    // Send the m_AppSize and m_AppID to each local rank 0 process
+    if (m_WriterRank == 0) // Send m_AppID to each local rank 0 process
+    {
+        if (m_WorldRank == 0) // App_ID
+        {
+            for (int i = 0; i < m_AppSize; ++i)
+            {
+                m_tmp.push_back(i);
+                MPI_Send( m_tmp.data(), 1, MPI_INT, AppStart[i], 99, MPI_COMM_WORLD); //
+                m_tmp.clear();
+            }
+        }
+        else
+        {
+            m_tmp.push_back(m_AppID);
+            MPI_Recv(m_tmp.data(), 1, MPI_INT, 0, 99, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            m_AppID = m_tmp[0];
+            m_tmp.clear();
+        }
+    }
+
+    m_tmp.push_back(m_AppID);
+    m_Comm.Barrier();
+    m_Comm.Bcast(m_tmp.data() , sizeof(int), 0);  // Local rank 0 process broadcast the m_AppID within the local communicator.
+    m_AppID = m_tmp[0];
+    m_tmp.clear();
+    //std::cout << "World Process " << m_WorldRank << " is local Writer " << m_WriterRank << ". AppID is " << m_AppID << ". " << std::endl;
+    m_Comm.Barrier();
+
+    m_tmp.push_back(m_AppSize);
+    MPI_Barrier(MPI_COMM_WORLD);
+    MPI_Bcast( m_tmp.data(), 1, MPI_INT, 0, MPI_COMM_WORLD); // Bcast the m_AppSize
+    m_AppSize = m_tmp[0];
+    m_tmp.clear();
+    //std::cout << "World Process " << m_WorldRank << " is local Writer " << m_WriterRank << ". AppSize is " << m_AppSize << ". " << std::endl;
+    MPI_Barrier(MPI_COMM_WORLD);
+
+
+
+
+
+    // In each local communicator, each local rank 0 process gathers the world rank of all the rest local processes.
+    if (m_WriterRank == 0)
+    {
+        //std::cout << "Writer size is " << m_WriterSize << "." << std::endl;
+        lrbuf.resize(m_WriterSize);
+    }
+
+    m_tmp.push_back(m_WorldRank);
+    //std::cout << "Writer Local Rank to send: " << m_tmp[0] << std::endl;
+    //MPI_Gather(m_tmp.data(), 1, MPI_INT, lrbuf.data(), 1, MPI_INT, 0, CommAsMPI(m_Comm));
+    m_Comm.Gather(m_tmp.data(), 1, lrbuf.data(), 1, 0);
+    m_tmp.clear();
+
+    if (m_WriterRank == 0)
+    {
+        std::cout << "App " << m_AppID << " has ";
+        for (int i = 0; i < lrbuf.size(); ++i)
+        {
+            std::cout << "world rank " << lrbuf[i] << ", ";
+        }
+        std::cout << " in it." << std::endl;
+    }
+
+
+    // Send the WorldRank vector of each local communicator to the m_WorldRank == 0 process.
+    int WriterInfoSize = 0;
+    int ReaderInfoSize = 0;
+    if (m_WriterRank == 0)
+    {
+        if (m_WorldRank == 0) // App_ID
+        {
+            for (int i = 0; i < m_WriterGlobalMpiInfo.size(); ++i)
+            {
+                if (i == 0)
+                {
+                    m_WriterGlobalMpiInfo[i] = lrbuf;
+                    WriterInfoSize += lrbuf.size();
+                }
+                else
+                {
+                    m_tmp.resize(1);
+                    MPI_Recv( m_tmp.data(), 1, MPI_INT, AppStart[i], 96, MPI_COMM_WORLD, MPI_STATUS_IGNORE); //
+                    int j_writersize = m_tmp[0];
+                    std::cout << "Writer size of " << AppStart[i] << " is got, length = " << j_writersize<< "." << std::endl;
+                    WriterInfoSize += j_writersize;
+                    m_tmp.clear();
+
+                    m_tmp.resize(j_writersize);
+                    MPI_Recv( m_tmp.data(), j_writersize, MPI_INT, AppStart[i], 98, MPI_COMM_WORLD, MPI_STATUS_IGNORE); //
+                    std::cout << "Writer Recieved" << AppStart[i] << std::endl;
+                    m_WriterGlobalMpiInfo[i] = m_tmp;
+                    m_tmp.clear();
+                }
+            }
+
+            for (int i = m_WriterGlobalMpiInfo.size(); i < m_AppSize; ++i)
+            {
+                if (i == 0)
+                {
+                    m_ReaderGlobalMpiInfo[i] = lrbuf;
+                    ReaderInfoSize += lrbuf.size();
+                }
+                else
+                {
+                    m_tmp.resize(1);
+                    MPI_Recv( m_tmp.data(), 1, MPI_INT, AppStart[i], 95, MPI_COMM_WORLD, MPI_STATUS_IGNORE); //
+                    int j_readersize = m_tmp[0];
+                    std::cout << "Reader size of " << AppStart[i] << " is got, length = " << j_readersize<< "." << std::endl;
+                    ReaderInfoSize += j_readersize;
+                    m_tmp.clear();
+
+                    m_tmp.resize(j_readersize);
+                    MPI_Recv( m_tmp.data(), j_readersize, MPI_INT, AppStart[i], 97, MPI_COMM_WORLD, MPI_STATUS_IGNORE); //
+                    std::cout << "Reader Recieved" << AppStart[i] << std::endl;
+                    m_ReaderGlobalMpiInfo[i-m_WriterGlobalMpiInfo.size()] = m_tmp;
+                    m_tmp.clear();
+                }
+            }
+
+        }
+        else
+        {
+            m_tmp.push_back(m_WriterSize);
+            MPI_Send(m_tmp.data(), 1, MPI_INT, 0, 96, MPI_COMM_WORLD);
+            m_tmp.clear();
+
+            MPI_Send(lrbuf.data(), lrbuf.size(), MPI_INT, 0, 98, MPI_COMM_WORLD);
+        }
+    }
+
+    /*if (m_WorldRank == 0)
+    {
+        std::cout << "World Writer Reached???" << std::endl;
+    }
+    else
+    {
+        std::cout << "Writer Reached???" << std::endl;
+    }
+
+    if (m_WorldRank == 0)
+    {
+        std::cout << "m_WriterGlobalMpiInfo have:" << std::endl;
+        for (int i = 0; i < m_WriterGlobalMpiInfo.size(); ++i)
+        {
+            std::cout << "Vector " << i << ": ";
+            for (int j = 0; j < m_WriterGlobalMpiInfo[i].size(); ++j)
+            {
+                std::cout << m_WriterGlobalMpiInfo[i][j] << "  ";
+            }
+            std::cout << std::endl;
+        }
+
+        std::cout << "m_ReaderGlobalMpiInfo have:" << std::endl;
+        for (int i = 0; i < m_ReaderGlobalMpiInfo.size(); ++i)
+        {
+            std::cout << "Vector " << i << ": ";
+            for (int j = 0; j < m_ReaderGlobalMpiInfo[i].size(); ++j)
+            {
+                std::cout << m_ReaderGlobalMpiInfo[i][j] << "  ";
+            }
+            std::cout << std::endl;
+        }
+    }
+
+    if (m_WorldRank == 0)
+    {
+        std::cout << "size of WriterGlobal" << m_WriterGlobalMpiInfo.size() << std::endl;
+    }*/
+
+    // Broadcast m_WriterGlobalMpiInfo and m_ReaderGlobalMpiInfo to all the processes.
+    MPI_Bcast(WriterInfoSize, 1, MPI_INT, 0, MPI_COMM_WORLD); // Broadcast writerinfo size
+    MPI_Bcast(m_WriterGlobalMpiInfo, writersize, MPI_INT, 0, MPI_COMM_WORLD); // Broadcast readerinfo size
+    //MPI_Bcast(m_WriterGlobalMpiInfo, writersize, MPI_INT, 0, MPI_COMM_WORLD)
+    //MPI_Bcast(m_ReaderGlobalMpiInfo, readersize, MPI_INT, 0, MPI_COMM_WORLD)
 }
 
 void SscWriter::SyncWritePattern()
