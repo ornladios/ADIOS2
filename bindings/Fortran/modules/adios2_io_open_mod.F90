cd52036168d43c33e34b1fbec5473c33cad8c476
Brad King <brad.king@kitware.com>
2020-03-05 09:37:03 -0500

    bindings: Convert Fortran MPI- and non-MPI implementations to submodules
    Convert the separate modules implementing Fortran `adios2_init` and
    `adios2_open` generic procedure signatures for MPI and non-MPI variants
    into submodules.  This allows the primary modules to be built first and
    the implementation submodules built later in separate libraries.
    
    In order to support compilers that do not support Fortran submodules,
    use preprocessor conditions to optionally build without submodules as
    before.

--- bindings/Fortran/modules/adios2_io_open_mod.F90
+++ bindings/Fortran/modules/adios2_io_open_mod.F90
@@ -6,8 +6,43 @@
 !
 
 module adios2_io_open_mod
+
+#ifdef ADIOS2_HAVE_FORTRAN_SUBMODULES
+    use adios2_parameters_mod
+    implicit none
+
+    interface adios2_open
+
+        module subroutine adios2_open_old_comm(engine, io, name, adios2_mode, ierr)
+            type(adios2_engine), intent(out) :: engine
+            type(adios2_io), intent(in) :: io
+            character*(*), intent(in) :: name
+            integer, intent(in) :: adios2_mode
+            integer, intent(out) :: ierr
+        end subroutine
+
+# ifdef ADIOS2_HAVE_MPI_F
+
+        module subroutine adios2_open_new_comm(engine, io, name, adios2_mode, comm, ierr)
+            type(adios2_engine), intent(out) :: engine
+            type(adios2_io), intent(in) :: io
+            character*(*), intent(in) :: name
+            integer, intent(in) :: adios2_mode
+            integer, intent(in) :: comm
+            integer, intent(out) :: ierr
+        end subroutine
+
+# endif
+
+    end interface
+
+#else
+
     use adios2_io_open_mod_serial
-#ifdef ADIOS2_HAVE_MPI_F
+# ifdef ADIOS2_HAVE_MPI_F
     use adios2_io_open_mod_mpi
+# endif
+
 #endif
+
 end module
