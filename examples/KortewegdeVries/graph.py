12f97af807aebfd9b9ebb0737c71b453c6078f3e
Nick <nathompson7@protonmail.com>
2020-03-30 11:59:37 -0400

    Fix formatting errors.

--- examples/KortewegdeVries/graph.py
+++ examples/KortewegdeVries/graph.py
@@ -7,23 +7,21 @@ import adios2
 import time
 
 def run():
-    parser = argparse.ArgumentParser(description=('Graphically displays output of Kortweg-de Vries simulation'))
-    arg = parser.add_argument("-f", "--filename", help=".bp filename/directory (default: kdv.bp)", default="kdv.bp")
-
+    parser = argparse.ArgumentParser(description=('Displays solution of KdV equation'))
+    parser.add_argument("-f", help=".bp filename (default: kdv.bp)", default="kdv.bp")
     options = parser.parse_args()
     dgoptions = diagram.DOption()
     dgoptions.mode = 'g'
-    
+
     with adios2.open(options.filename, "r") as file_handle:
         for step in file_handle:
             os.system('cls' if os.name == 'nt' else 'clear')
-            points = file_handle.read("u")
+            points = step.read("u")
             values = [None]
             dg = diagram.DGWrapper(dg_option=dgoptions, data=[points, values])
             dg.show()
             time.sleep(0.1)
-        print("Done going through steps")
+
 
 if __name__ == '__main__':
     run()
-
