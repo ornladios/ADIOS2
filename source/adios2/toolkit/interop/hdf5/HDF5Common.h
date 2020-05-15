18429bfe0cf60a73ce42f35e335e856d7eb8b708
Junmin Gu <jgu@lbl.gov>
2020-05-14 21:20:13 -0700

    fixed error introduced from last pull request basically I removed the line that closes existing  group before opening a new one

--- source/adios2/toolkit/interop/hdf5/HDF5Common.h
+++ source/adios2/toolkit/interop/hdf5/HDF5Common.h
@@ -34,6 +34,7 @@ typedef enum
     E_H5_DATATYPE = 1,
     E_H5_GROUP = 2,
     E_H5_SPACE = 3,
+    E_H5_ATTRIBUTE = 4
 } ADIOS_ENUM_H5;
 
 class HDF5DatasetGuard
@@ -90,6 +91,10 @@ public:
         {
             H5Tclose(m_Key);
         }
+	else if (m_Type == E_H5_ATTRIBUTE)
+	  {
+	    H5Aclose(m_Key);
+	  }
         else
         {
             printf(" UNABLE to close \n");
