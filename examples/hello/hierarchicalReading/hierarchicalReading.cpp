8e96b4f336154950c808b8b404c335dbb5b26e31
Dmitry Ganyushin <ganyushin@gmail.com>
2020-07-29 11:30:05 -0400

    Group class with a bookkeeping info

--- examples/hello/hierarchicalReading/hierarchicalReading.cpp
+++ examples/hello/hierarchicalReading/hierarchicalReading.cpp
@@ -1,7 +1,21 @@
+//Group g   =  io("/a", separator="/");
+//g.AvailableGroups();    // [b,f] groups directly under /a/*, not recursive
+//g.AvailableVariables();    // [x] variables directly under /a/*, not recursive
+//g.AvailableAttributes();   // not recursive
+//
+//Group gb = g.InquireGroup("b");
+//Variable<T> v = g.InquireVariable("x");
+//Variable<T> v = g.InquireVariable("b/c"); // NOT ALLOWED
+//
+//Attribute<T> a = g.InquireAttribute("attr1");
+//
+//std::string VariableType(const std::string &name) const;
+//std::string AttributeType(const std::string &name) const;
 
-//Write data first
-#include <ios>      //std::ios_base::failure
-#include <iostream> //std::cout
+
+// Write data first
+#include <ios>       //std::ios_base::failure
+#include <iostream>  //std::cout
 #include <stdexcept> //std::invalid_argument std::exception
 #include <vector>
 
@@ -9,6 +23,140 @@
 #if ADIOS2_USE_MPI
 #include <mpi.h>
 #endif
+
+std::vector<std::string> split(const std::string& s, char delimiter)
+{
+    std::vector<std::string> tokens;
+    std::string token;
+    std::istringstream tokenStream(s);
+    while (std::getline(tokenStream, token, delimiter))
+    {
+        tokens.push_back(token);
+    }
+    return tokens;
+}
+
+class Group
+{
+private:
+    std::string current_path;
+    std::string meta_info;
+    std::vector<std::string> variables;
+    std::vector<std::string> groups;
+    std::vector<std::string> attributes;
+    adios2::IO bpIO;
+    adios2::Engine engine;
+
+public:
+    Group( adios2::IO&& in_bpIO,  adios2::Engine&& in_engine): bpIO(std::move(in_bpIO)), engine(std::move(in_engine)) {
+        //create meta info
+        adios2::Variable<std::string> meta_info =
+            bpIO.DefineVariable<std::string>("meta_info");
+
+    };
+    ~Group();
+    void getMetaInfo();
+    void updateMetaInfo();
+    void DefineGroup(std::string&);
+    void DefineVariable(const std::string&);
+    void DefineAttribute(const std::string&);
+    std::vector<std::string> AvailableGroups();    // [b,f] groups directly under /a/*, not recursive
+    std::vector<std::string> AvailableVariables();    // [x] variables directly under /a/*, not recursive
+    std::vector<std::string> AvailableAttributes();
+    std::string InquirePath(){
+        return current_path;
+    }
+    void setPath(std::string path){
+        current_path = path;
+    }
+};
+
+void Group::getMetaInfo() {
+    //meta info  is stored as absoulute path "group1,group2,group3,:group1/var1,var,group2/var2,:attr,group1/attr1,group2/attr2"
+    adios2::Variable<std::string> meta_info =
+        bpIO.InquireVariable<std::string>("meta_info");
+    std::string meta_info_str_read;
+    if (meta_info){
+        engine.Get<std::string>(meta_info, meta_info_str_read, adios2::Mode::Sync);
+        //split a string by a separator ":"
+        std::vector<std::string> groups_variables_attributes = split(meta_info_str_read, ':');
+        groups = split(groups_variables_attributes[0], ',');
+        variables = split(groups_variables_attributes[1], ',');
+        attributes = split(groups_variables_attributes[1], ',');
+        return;
+    }else{
+        throw std::exception();
+    }
+}
+void Group::updateMetaInfo(){
+    std::string meta_info_str;
+    for (std::string s : groups){
+        meta_info_str += s;
+        meta_info_str += ",";
+    }
+    meta_info_str +=":";
+    for (std::string s : variables){
+        meta_info_str += s;
+        meta_info_str += ",";
+    }
+    meta_info_str +=":";
+    for (std::string s : attributes){
+        meta_info_str += s;
+        meta_info_str += ",";
+    }
+    //alternatively we can implement "make transaction" at the end with Put
+    engine.Get<std::string>(meta_info, meta_info_str, adios2::Mode::Sync);
+    return;
+}
+
+void Group::DefineGroup(std::string &groupName) {
+    groups.push_back(groupName);
+    updateMetaInfo();
+    return;
+}
+
+void Group::DefineVariable(const std::string& variable_name) {
+
+   /* adios2::Variable<T> bpFloats = bpIO.DefineVariable<T>(
+        variableName, {size * Nx}, {rank * Nx}, {Nx},
+        adios2::ConstantDims);*/
+    variables.push_back(variable_name);
+    updateMetaInfo();
+    return;
+}
+
+void Group::DefineAttribute(const std::string& attribute_name) {
+
+    /* adios2::Variable<T> bpFloats = bpIO.DefineVariable<T>(
+         variableName, {size * Nx}, {rank * Nx}, {Nx},
+         adios2::ConstantDims);*/
+    attributes.push_back(attribute_name);
+    updateMetaInfo();
+    return;
+}
+
+std::vector<std::string> Group::AvailableGroups(){
+    std::vector<std::string> available_groups; 
+    for (std::string  s : groups ){
+            if (s.find(current_path) == 0)  available_groups.push_back(s);
+    }
+    return available_groups;
+}
+std::vector<std::string> Group::AvailableVariables(){
+    std::vector<std::string> available_variables;
+    for (std::string  s :  variables){
+        if (s.find(current_path) == 0)  available_variables.push_back(s);
+    }
+    return available_variables;
+}
+std::vector<std::string> Group::AvailableAttributes(){
+    std::vector<std::string> available_attributes;
+    for (std::string  s :  attributes){
+        if (s.find(current_path) == 0)  available_attributes.push_back(s);
+    }
+    return available_attributes;
+}
+
 
 int main(int argc, char *argv[])
 {
@@ -48,32 +196,44 @@ int main(int argc, char *argv[])
          * count
          * (local) }, all are constant dimensions */
         adios2::Variable<float> bpFloats = bpIO.DefineVariable<float>(
-            "/group1/subgroup1/bpFloats", {size * Nx}, {rank * Nx}, {Nx}, adios2::ConstantDims);
+            "/group1/subgroup1/bpFloats", {size * Nx}, {rank * Nx}, {Nx},
+            adios2::ConstantDims);
 
-        adios2::Variable<int> bpInts = bpIO.DefineVariable<int>(
-            "/group1/subgroup1/bpInts", {size * Nx}, {rank * Nx}, {Nx}, adios2::ConstantDims);
+        adios2::Variable<int> bpInts =
+            bpIO.DefineVariable<int>("/group1/subgroup1/bpInts", {size * Nx},
+                                     {rank * Nx}, {Nx}, adios2::ConstantDims);
 
-        adios2::Variable<std::string> bpString =
-            bpIO.DefineVariable<std::string>("bpString");
+        adios2::Variable<std::string> meta_info =
+            bpIO.DefineVariable<std::string>("meta_info");
+
+        // adios2::Variable<std::string> bpString =
+        //    bpIO.DefineVariable<std::string>("bpString");
 
         std::vector<std::string> myStrings = {"one", "two", "three"};
         bpIO.DefineAttribute<std::string>("Array_of_Strings", myStrings.data(),
                                           myStrings.size());
 
-
         /** Engine derived class, spawned to start IO operations */
         adios2::Engine bpFileWriter = bpIO.Open(filename, adios2::Mode::Append);
 
-        for (int i = 0; i < number_of_steps; i++){
-
-             bpFileWriter.BeginStep();
+        for (int i = 0; i < number_of_steps; i++)
+        {
+            std::string meta_info_str;
+            bpFileWriter.BeginStep();
             /** Put variables for buffering, template type is optional */
-             if (i%2 == 0)
+            if (i % 2 == 0)
+            {
                 bpFileWriter.Put<float>(bpFloats, myFloats.data());
-             else
-                 bpFileWriter.Put(bpInts, myInts.data());
-            // bpFileWriter.Put(bpString, myString);
-             bpFileWriter.EndStep();
+                meta_info_str = "/group1/subgroup1/bpFloats";
+            }
+            else
+            {
+                bpFileWriter.Put(bpInts, myInts.data());
+                meta_info_str = "/group1/subgroup1/bpInts";
+            }
+
+            bpFileWriter.Put<std::string>(meta_info, meta_info_str.data());
+            bpFileWriter.EndStep();
         }
         /** Create bp file, engine becomes unreachable after this*/
         bpFileWriter.Close();
@@ -112,7 +272,7 @@ int main(int argc, char *argv[])
     try
     {
         /** ADIOS class factory of IO class objects */
-       // adios2::ADIOS adios(MPI_COMM_WORLD);
+        // adios2::ADIOS adios(MPI_COMM_WORLD);
 #if ADIOS2_USE_MPI
         adios2::ADIOS adios(MPI_COMM_WORLD);
 #else
@@ -139,9 +299,12 @@ int main(int argc, char *argv[])
         /** Engine derived class, spawned to start IO operations */
         adios2::Engine bpReader = bpIO.Open(filename, adios2::Mode::Read);
 
+
         for (int step = 0; step < number_of_steps; step++)
         {
             bpReader.BeginStep();
+            adios2::Variable<std::string> meta_info =
+                bpIO.InquireVariable<std::string>("meta_info");
             const std::map<std::string, adios2::Params> variables =
                 bpIO.AvailableVariables();
 
@@ -156,50 +319,58 @@ int main(int argc, char *argv[])
                 }
             }
 
-        /** Write variable for buffering */
-        adios2::Variable<float> bpFloats =
-            bpIO.InquireVariable<float>("/group1/subgroup1/bpFloats");
-        adios2::Variable<int> bpInts = bpIO.InquireVariable<int>("/group1/subgroup1/bpInts");
-
-        const std::size_t Nx = 10;
-        if (bpFloats) // means found
-        {
-            std::vector<float> myFloats;
-
-            // read only the chunk corresponding to our rank
-            bpFloats.SetSelection({{Nx * rank}, {Nx}});
-            // myFloats.data is pre-allocated
-            bpReader.Get<float>(bpFloats, myFloats, adios2::Mode::Sync);
-
-            if (rank == 0)
-            {
-                std::cout << "MyFloats: \n";
-                for (const auto number : myFloats)
-                {
-                    std::cout << number << " ";
-                }
-                std::cout << "\n";
+            adios2::Variable<std::string> meta_info_str =
+                bpIO.InquireVariable<std::string>("meta_info");
+            std::string meta_info_str_read;
+            if (meta_info){
+                bpReader.Get<std::string>(meta_info, meta_info_str_read, adios2::Mode::Sync);
+                std::cout << "meta info " << meta_info_str_read << std::endl;
             }
-        }
+            /** Write variable for buffering */
+            adios2::Variable<float> bpFloats =
+                bpIO.InquireVariable<float>("/group1/subgroup1/bpFloats");
+            adios2::Variable<int> bpInts =
+                bpIO.InquireVariable<int>("/group1/subgroup1/bpInts");
 
-        if (bpInts) // means not found
-        {
-            std::vector<int> myInts;
-            // read only the chunk corresponding to our rank
-            bpInts.SetSelection({{Nx * rank}, {Nx}});
-
-            bpReader.Get<int>(bpInts, myInts, adios2::Mode::Sync);
-
-            if (rank == 0)
+            const std::size_t Nx = 10;
+            if (bpFloats) // means found
             {
-                std::cout << "myInts: \n";
-                for (const auto number : myInts)
+                std::vector<float> myFloats;
+
+                // read only the chunk corresponding to our rank
+                bpFloats.SetSelection({{Nx * rank}, {Nx}});
+                // myFloats.data is pre-allocated
+                bpReader.Get<float>(bpFloats, myFloats, adios2::Mode::Sync);
+
+                if (rank == 0)
                 {
-                    std::cout << number << " ";
+                    std::cout << "MyFloats: \n";
+                    for (const auto number : myFloats)
+                    {
+                        std::cout << number << " ";
+                    }
+                    std::cout << "\n";
                 }
-                std::cout << "\n";
             }
-        }
+
+            if (bpInts) // means not found
+            {
+                std::vector<int> myInts;
+                // read only the chunk corresponding to our rank
+                bpInts.SetSelection({{Nx * rank}, {Nx}});
+
+                bpReader.Get<int>(bpInts, myInts, adios2::Mode::Sync);
+
+                if (rank == 0)
+                {
+                    std::cout << "myInts: \n";
+                    for (const auto number : myInts)
+                    {
+                        std::cout << number << " ";
+                    }
+                    std::cout << "\n";
+                }
+            }
             bpReader.EndStep();
         }
         /** Close bp file, engine becomes unreachable after this*/
@@ -246,15 +417,12 @@ int main(int argc, char *argv[])
 #if ADIOS2_USE_MPI
         MPI_Abort(MPI_COMM_WORLD, 1);
 #endif
-
     }
 
 #if ADIOS2_USE_MPI
     MPI_Finalize();
 #endif
-//now read part
-
-
+    // now read part
 
     return 0;
 }
