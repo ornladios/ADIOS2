b0f5752b0e67c2b7533973ec2d07cc97f78286ee
Norbert Podhorszki <pnorbert@ornl.gov>
2020-08-04 12:54:55 -0400

    Process metadata really step-by-step in BP4 when StreamReader is on. So far, it was reading in metadata in chunks but it processed the whole chunk (multiple steps) at once. For true semantics, we want Variables and Attributes show up in their proper step and since the IO map holds all entries of all processed steps, the current processing can show variables in later steps too early. Now the chunks are kept in memory but only processed step by step.

--- source/adios2/toolkit/format/bp/bp4/BP4Deserializer.h
+++ source/adios2/toolkit/format/bp/bp4/BP4Deserializer.h
@@ -46,9 +46,8 @@ public:
 
     ~BP4Deserializer() = default;
 
-    void ParseMetadataIndex(const BufferSTL &bufferSTL,
-                            const size_t absoluteStartPos,
-                            const bool hasHeader);
+    void ParseMetadataIndex(BufferSTL &bufferSTL, const size_t absoluteStartPos,
+                            const bool hasHeader, const bool oneStepOnly);
 
     /* Return the position in the buffer where processing ends. The processing
      * is controlled by the number of records in the Index, which may be less
