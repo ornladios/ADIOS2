1021fcd9cf10a722fa460a256522a7ed0124e3c7
Jason Wang <jason.ruonan.wang@gmail.com>
2020-08-12 17:06:20 -0400

    added template specialization function for complex numbers

--- testing/adios2/engine/ssc/TestSscCommon.h
+++ testing/adios2/engine/ssc/TestSscCommon.h
@@ -40,6 +40,41 @@ void PrintData(const T *data, const size_t step, const Dims &start,
 template <class T>
 void GenDataRecursive(std::vector<size_t> start, std::vector<size_t> count,
                       std::vector<size_t> shape, size_t n0, size_t y,
+                      std::vector<std::complex<T>> &vec, const size_t step)
+{
+    for (size_t i = 0; i < count[0]; i++)
+    {
+        size_t i0 = n0 * count[0] + i;
+        size_t z = y * shape[0] + (i + start[0]);
+
+        auto start_next = start;
+        auto count_next = count;
+        auto shape_next = shape;
+        start_next.erase(start_next.begin());
+        count_next.erase(count_next.begin());
+        shape_next.erase(shape_next.begin());
+
+        if (start_next.size() == 1)
+        {
+            for (size_t j = 0; j < count_next[0]; j++)
+            {
+                vec[i0 * count_next[0] + j] = {
+                    static_cast<T>(z * shape_next[0] + (j + start_next[0]) +
+                                   step),
+                    1};
+            }
+        }
+        else
+        {
+            GenDataRecursive(start_next, count_next, shape_next, i0, z, vec,
+                             step);
+        }
+    }
+}
+
+template <class T>
+void GenDataRecursive(std::vector<size_t> start, std::vector<size_t> count,
+                      std::vector<size_t> shape, size_t n0, size_t y,
                       std::vector<T> &vec, const size_t step)
 {
     for (size_t i = 0; i < count[0]; i++)
