c0db4d3e27000ffc3f408481895260f47def35a0
dmitry-ganyushin <ganyushin@gmail.com>
2020-11-02 15:46:32 -0500

    Added a unittest for reading a string with high-level API in python

--- testing/adios2/bindings/python/TestBPWriteReadString.py
+++ testing/adios2/bindings/python/TestBPWriteReadString.py
@@ -11,35 +11,51 @@ import unittest
 from mpi4py import MPI
 import adios2
 
-class TestAdiosWriteReadString(unittest.TestCase):
+class TestAdiosWriteReadStringfullAPI(unittest.TestCase):
 
-    def TestAdiosWriteReadString(self):
-        # MPI
+    def test_write_read_string_fullAPI(self):
         comm = MPI.COMM_WORLD
         theString = 'hello adios'
-        bpFilename = 'string_test.bp'
+        bpFilename = 'string_test_fullAPI.bp'
         varname = 'mystringvar'
-        NSteps = 10
+        NSteps = 3
         adios = adios2.ADIOS(comm)
         ioWrite = adios.DeclareIO('ioWriter')
         adEngine = ioWrite.Open(bpFilename, adios2.Mode.Write)
-        varMyStringVar = ioWrite.DefineVariable(varname)
+        varMyString = ioWrite.DefineVariable(varname)
         for step in range(NSteps):
             adEngine.BeginStep()
-            adEngine.Put(varMyStringVar, theString + str(step))
+            adEngine.Put(varMyString, theString + str(step))
             adEngine.EndStep()
         adEngine.Close()
 
         ioRead = adios.DeclareIO('ioReader')
         adEngine = ioRead.Open(bpFilename, adios2.Mode.Read)
-        varReadMyStringVar = ioRead.InquireVariable(varname)
+        varReadMyString = ioRead.InquireVariable(varname)
         for step in range(NSteps):
             adEngine.BeginStep()
-            result = adEngine.Get(varReadMyStringVar)
+            result = adEngine.Get(varReadMyString)
             adEngine.EndStep()
             self.assertEqual(result, theString + str(step))
         adEngine.Close()
 
+    def test_write_read_string_highAPI(self):
+        comm = MPI.COMM_WORLD
+        theString = 'hello adios'
+        bpFilename = 'string_test_highAPI.bp'
+        varname = 'mystringvar'
+        NSteps = 3
+        adios = adios2.ADIOS(comm)
+        with adios2.open(bpFilename, "w") as fh:
+
+            for step in range(NSteps):
+                fh.write(varname, theString + str(step), end_step=True)
+
+        with adios2.open(bpFilename, "r") as fh:
+            for fstep in fh:
+                step = fstep.current_step()
+                result = fstep.read(varname)
+                self.assertEqual("".join([chr(s) for s in result]), theString + str(step))
 
 if __name__ == '__main__':
     unittest.main()
