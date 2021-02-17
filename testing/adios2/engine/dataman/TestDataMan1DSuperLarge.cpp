cd1a2b9485b325dedda099cc959e465934df133f
Jason Wang <jason.ruonan.wang@gmail.com>
2021-02-17 10:52:20 -0500

    reduced testing data size to fit CI capacity

--- testing/adios2/engine/dataman/TestDataMan1DSuperLarge.cpp
+++ testing/adios2/engine/dataman/TestDataMan1DSuperLarge.cpp
@@ -275,13 +275,13 @@ public:
 TEST_F(DataManEngineTest, 1DSuperLarge)
 {
     // set parameters
-    Dims shape = {4000000};
+    Dims shape = {2000000};
     Dims start = {0};
-    Dims count = {4000000};
+    Dims count = {2000000};
     size_t steps = 10;
     adios2::Params engineParams = {{"IPAddress", "127.0.0.1"},
                                    {"Port", "22300"},
-                                   {"MaxStepBufferSize", "400000000"},
+                                   {"MaxStepBufferSize", "200000000"},
                                    {"TransportMode", "reliable"}};
 
     // run workflow
