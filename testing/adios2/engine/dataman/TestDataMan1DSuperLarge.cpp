762e1c160c2f3071075e923a1a93175c58425a68
Jason Wang <jason.ruonan.wang@gmail.com>
2021-02-18 23:33:27 -0500

    reduced buffer size

--- testing/adios2/engine/dataman/TestDataMan1DSuperLarge.cpp
+++ testing/adios2/engine/dataman/TestDataMan1DSuperLarge.cpp
@@ -271,15 +271,15 @@ public:
 TEST_F(DataManEngineTest, 1DSuperLarge)
 {
     // set parameters
-    Dims shape = {1000000};
+    Dims shape = {500000};
     Dims start = {0};
-    Dims count = {1000000};
+    Dims count = {500000};
     size_t steps = 10;
 
     // run workflow
     adios2::Params readerEngineParams = {{"IPAddress", "127.0.0.1"},
                                          {"Port", "13480"},
-                                         {"MaxStepBufferSize", "100000000"},
+                                         {"MaxStepBufferSize", "50000000"},
                                          {"TransportMode", "reliable"}};
     auto r = std::thread(DataManReader, shape, start, count, steps,
                          readerEngineParams);
