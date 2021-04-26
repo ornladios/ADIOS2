5f114b32ec02323b8e0ff5c8ccef2a4813f658af
Caitlin Ross <caitlin.ross@kitware.com>
2021-04-26 11:21:29 -0400

    add perfstubs to thirdparty

---
+++ thirdparty/perfstubs/update.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+set -e
+set -x
+shopt -s dotglob
+
+readonly name="perfstubs"
+readonly ownership="PerfStubs Upstream <kwrobot@kitware.com>"
+readonly subtree="thirdparty/perfstubs/perfstubs"
+readonly repo="https://github.com/khuck/perfstubs.git"
+readonly tag="master"
+
+readonly shortlog="true"
+readonly paths="
+LICENSE
+README.md
+perfstubs_api/README.md
+perfstubs_api/timer.h
+perfstubs_api/timer.c
+perfstubs_api/tool.h
+"
+
+extract_source () {
+    git_archive
+}
+
+. "${BASH_SOURCE%/*}/../update-common.sh"
