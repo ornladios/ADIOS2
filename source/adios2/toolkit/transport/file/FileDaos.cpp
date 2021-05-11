8827712c1e3768c5f53f340a08dd016d4adbf456
Lipeng Wan <lwan86@jlselogin4.ftm.alcf.anl.gov>
2021-05-11 22:13:52 +0000

    added DAOS transport

--- source/adios2/toolkit/transport/file/FileDaos.cpp
+++ source/adios2/toolkit/transport/file/FileDaos.cpp
@@ -30,13 +30,14 @@ namespace transport
 
 FileDaos::FileDaos(helper::Comm const &comm) : Transport("File", "Daos", comm)
 {
+    
 }
 
 FileDaos::~FileDaos()
 {
     if (m_IsOpen)
     {
-        close(m_FileDescriptor);
+      //close(m_FileDescriptor);
     }
 }
 
@@ -46,7 +47,7 @@ void FileDaos::WaitForOpen()
     {
         if (m_OpenFuture.valid())
         {
-            m_FileDescriptor = m_OpenFuture.get();
+            m_DAOSOpenSucceed = m_OpenFuture.get();
         }
         m_IsOpening = false;
         CheckFile("couldn't open file " + m_Name + ", in call to Daos open");
@@ -57,21 +58,32 @@ void FileDaos::WaitForOpen()
 void FileDaos::Open(const std::string &name, const Mode openMode,
                     const bool async)
 {
-    auto lf_AsyncOpenWrite = [&](const std::string &name) -> int {
-        dfs_obj_t *obj;
+    auto lf_AsyncOpenWrite = [&](const std::string &name) -> bool {
+        
         ProfilerStart("open");
-        errno = 0;
-        int FD = dfs_open(/*DFS*/ NULL, /*PARENT*/ NULL, m_Name.c_str(),
-                          O_WRONLY | O_CREAT | O_TRUNC, 0666, /*CID*/ NULL,
+        //errno = 0;
+        int rc = dfs_open(/*DFS*/ dfs_mt, /*PARENT*/ NULL, m_Name.c_str(),
+			  S_IFREG | S_IWUSR, 
+                          O_WRONLY | O_CREAT | O_TRUNC, /*CID*/ 0,
                           /*chunksize*/ 0, NULL, &obj);
-        m_Errno = errno;
+        m_Errno = rc;
         ProfilerStop("open");
-        return FD;
+	bool DAOSOpenReturn;
+	if (rc == 0)
+	{
+	    DAOSOpenReturn = true;
+	}
+	else
+	{
+    	    DAOSOpenReturn = false;	    
+	}
+        return DAOSOpenReturn;
     };
 
     m_Name = name;
     CheckName();
     m_OpenMode = openMode;
+    int rc;
     switch (m_OpenMode)
     {
 
@@ -85,29 +97,41 @@ void FileDaos::Open(const std::string &name, const Mode openMode,
         else
         {
             ProfilerStart("open");
-            errno = 0;
-            m_FileDescriptor =
-                open(m_Name.c_str(), O_WRONLY | O_CREAT | O_TRUNC, 0666);
-            m_Errno = errno;
+            //errno = 0;
+            //m_FileDescriptor =
+            //    open(m_Name.c_str(), O_WRONLY | O_CREAT | O_TRUNC, 0666);
+	    rc = dfs_open(/*DFS*/ dfs_mt, /*PARENT*/ NULL, m_Name.c_str(),
+			  S_IFREG | S_IWUSR,
+                          O_WRONLY | O_CREAT | O_TRUNC, /*CID*/ 0,
+                          /*chunksize*/ 0, NULL, &obj);
+            m_Errno = rc;
             ProfilerStop("open");
+
         }
         break;
 
     case (Mode::Append):
         ProfilerStart("open");
-        errno = 0;
+        //errno = 0;
         // m_FileDescriptor = open(m_Name.c_str(), O_RDWR);
-        m_FileDescriptor = open(m_Name.c_str(), O_RDWR | O_CREAT, 0777);
-        lseek(m_FileDescriptor, 0, SEEK_END);
-        m_Errno = errno;
+        //m_FileDescriptor = open(m_Name.c_str(), O_RDWR | O_CREAT, 0777);
+        //lseek(m_FileDescriptor, 0, SEEK_END);
+	rc = dfs_open(/*DFS*/ dfs_mt, /*PARENT*/ NULL, m_Name.c_str(),
+			  S_IFREG | S_IWUSR | S_IRUSR,
+                          O_RDWR | O_CREAT, /*CID*/ 0,
+                          /*chunksize*/ 0, NULL, &obj);
+        m_Errno = rc;
         ProfilerStop("open");
         break;
 
     case (Mode::Read):
         ProfilerStart("open");
-        errno = 0;
-        m_FileDescriptor = open(m_Name.c_str(), O_RDONLY);
-        m_Errno = errno;
+        //errno = 0;
+        //m_FileDescriptor = open(m_Name.c_str(), O_RDONLY);
+	rc = dfs_open(/*DFS*/ dfs_mt, /*PARENT*/ NULL, m_Name.c_str(),
+			  S_IFREG | S_IRUSR,
+                          O_RDONLY, /*CID*/ 0, /*chunksize*/ 0, NULL, &obj);
+        m_Errno = rc;
         ProfilerStop("open");
         break;
 
@@ -115,6 +139,14 @@ void FileDaos::Open(const std::string &name, const Mode openMode,
         CheckFile("unknown open mode for file " + m_Name +
                   ", in call to Daos open");
     }
+    if (rc == 0)
+    {
+	m_DAOSOpenSucceed = true;
+    }
+    else
+    {
+	m_DAOSOpenSucceed = false;	    
+    }	        
 
     if (!m_IsOpening)
     {
@@ -125,16 +157,37 @@ void FileDaos::Open(const std::string &name, const Mode openMode,
 
 void FileDaos::Write(const char *buffer, size_t size, size_t start)
 {
-    auto lf_Write = [&](const char *buffer, size_t size) {
-        while (size > 0)
+  auto lf_Write = [&](const char *buffer, size_t size) {
+        daos_size_t io_size;
+	daos_size_t written_size = 0;
+        d_sg_list_t wsgl;
+	d_iov_t iov;
+	int rc = 0;
+	
+	d_iov_set(&iov, const_cast<char*>(buffer), size);
+	wsgl.sg_nr = 1;
+	wsgl.sg_nr_out = 1;
+	wsgl.sg_iovs = &iov;
+        while (written_size < size)
         {
+            io_size = size-written_size;
+	    wsgl.sg_iovs[0].iov_len = io_size;
             ProfilerStart("write");
-            errno = 0;
-            const auto writtenSize = write(m_FileDescriptor, buffer, size);
-            m_Errno = errno;
-            ProfilerStop("write");
+            //errno = 0;
 
-            if (writtenSize == -1)
+            //const auto writtenSize = write(m_FileDescriptor, buffer, size);
+
+	    rc = dfs_write(dfs_mt, obj, &wsgl, m_GlobalOffset, NULL);
+	    
+	    if (rc)
+	    {
+                throw std::ios_base::failure(
+                    "ERROR: couldn't write to file " + m_Name +
+                    ", in call to Daos Write" + SysErrMsg());	        
+	    }
+            m_Errno = rc;
+            ProfilerStop("write");	    
+            /*if (writtenSize == -1)
             {
                 if (errno == EINTR)
                 {
@@ -144,27 +197,18 @@ void FileDaos::Write(const char *buffer, size_t size, size_t start)
                 throw std::ios_base::failure(
                     "ERROR: couldn't write to file " + m_Name +
                     ", in call to Daos Write" + SysErrMsg());
-            }
+	    }*/
 
-            buffer += writtenSize;
-            size -= writtenSize;
+            buffer += written_size;
+            written_size += io_size;
+	    m_GlobalOffset += io_size;
         }
     };
 
     WaitForOpen();
     if (start != MaxSizeT)
     {
-        errno = 0;
-        const auto newPosition = lseek(m_FileDescriptor, start, SEEK_SET);
-        m_Errno = errno;
-
-        if (static_cast<size_t>(newPosition) != start)
-        {
-            throw std::ios_base::failure(
-                "ERROR: couldn't move to start position " +
-                std::to_string(start) + " in file " + m_Name +
-                ", in call to Daos lseek" + SysErrMsg());
-        }
+        m_GlobalOffset = start;
     }
 
     if (size > DefaultMaxFileBatchSize)
@@ -175,7 +219,7 @@ void FileDaos::Write(const char *buffer, size_t size, size_t start)
         size_t position = 0;
         for (size_t b = 0; b < batches; ++b)
         {
-            lf_Write(&buffer[position], DefaultMaxFileBatchSize);
+	    lf_Write(&buffer[position], DefaultMaxFileBatchSize);
             position += DefaultMaxFileBatchSize;
         }
         lf_Write(&buffer[position], remainder);
@@ -189,28 +233,37 @@ void FileDaos::Write(const char *buffer, size_t size, size_t start)
 void FileDaos::Read(char *buffer, size_t size, size_t start)
 {
     auto lf_Read = [&](char *buffer, size_t size) {
-        while (size > 0)
+        daos_size_t request_size;
+	daos_size_t got_size;
+	daos_size_t read_size = 0;
+        d_sg_list_t rsgl;
+	d_iov_t iov;
+	int rc = 0;
+	
+	d_iov_set(&iov, const_cast<char*>(buffer), size);
+	rsgl.sg_nr = 1;
+	rsgl.sg_nr_out = 1;
+	rsgl.sg_iovs = &iov;       
+        while (read_size < size)
         {
+	    request_size = size-read_size;
+	    rsgl.sg_iovs[0].iov_len = request_size;
             ProfilerStart("read");
-            errno = 0;
-            const auto readSize = read(m_FileDescriptor, buffer, size);
-            m_Errno = errno;
-            ProfilerStop("read");
 
-            if (readSize == -1)
-            {
-                if (errno == EINTR)
-                {
-                    continue;
-                }
+	    rc = dfs_read(dfs_mt, obj, &rsgl, m_GlobalOffset, &got_size, NULL);
 
+	    if (rc)
+	    {
                 throw std::ios_base::failure(
                     "ERROR: couldn't read from file " + m_Name +
-                    ", in call to Daos IO read" + SysErrMsg());
-            }
-
-            buffer += readSize;
-            size -= readSize;
+                    ", in call to Daos Read" + SysErrMsg());	        
+	    }
+            m_Errno = rc;
+            ProfilerStop("read");	    
+	    
+            buffer += read_size;
+            read_size += got_size;
+	    m_GlobalOffset += got_size;
         }
     };
 
@@ -218,17 +271,7 @@ void FileDaos::Read(char *buffer, size_t size, size_t start)
 
     if (start != MaxSizeT)
     {
-        errno = 0;
-        const auto newPosition = lseek(m_FileDescriptor, start, SEEK_SET);
-        m_Errno = errno;
-
-        if (static_cast<size_t>(newPosition) != start)
-        {
-            throw std::ios_base::failure(
-                "ERROR: couldn't move to start position " +
-                std::to_string(start) + " in file " + m_Name +
-                ", in call to Daos lseek" + SysErrMsg());
-        }
+        m_GlobalOffset = start;
     }
 
     if (size > DefaultMaxFileBatchSize)
@@ -239,30 +282,34 @@ void FileDaos::Read(char *buffer, size_t size, size_t start)
         size_t position = 0;
         for (size_t b = 0; b < batches; ++b)
         {
-            lf_Read(&buffer[position], DefaultMaxFileBatchSize);
+	    lf_Read(&buffer[position], DefaultMaxFileBatchSize);
             position += DefaultMaxFileBatchSize;
         }
         lf_Read(&buffer[position], remainder);
     }
     else
     {
-        lf_Read(buffer, size);
+      lf_Read(buffer, size);
     }
 }
 
 size_t FileDaos::GetSize()
 {
-    struct stat fileStat;
+    //struct stat fileStat;
     WaitForOpen();
-    errno = 0;
-    if (fstat(m_FileDescriptor, &fileStat) == -1)
+    daos_size_t file_size;
+    int rc = 0;
+    rc = dfs_get_size(dfs_mt, obj, &file_size);
+    //errno = 0;
+    /*if (fstat(m_FileDescriptor, &fileStat) == -1)
     {
         m_Errno = errno;
         throw std::ios_base::failure("ERROR: couldn't get size of file " +
                                      m_Name + SysErrMsg());
-    }
-    m_Errno = errno;
-    return static_cast<size_t>(fileStat.st_size);
+				     }*/
+    m_Errno = rc;
+    return static_cast<size_t>(file_size);
+    
 }
 
 void FileDaos::Flush() {}
@@ -271,12 +318,14 @@ void FileDaos::Close()
 {
     WaitForOpen();
     ProfilerStart("close");
-    errno = 0;
-    const int status = close(m_FileDescriptor);
-    m_Errno = errno;
+    //errno = 0;
+    int rc;
+    rc = dfs_release(obj);
+    //const int status = close(m_FileDescriptor);
+    m_Errno = rc;
     ProfilerStop("close");
 
-    if (status == -1)
+    if (rc)
     {
         throw std::ios_base::failure("ERROR: couldn't close file " + m_Name +
                                      ", in call to Daos IO close" +
@@ -293,12 +342,14 @@ void FileDaos::Delete()
     {
         Close();
     }
-    std::remove(m_Name.c_str());
+    //std::remove(m_Name.c_str());
+    int rc;
+    dfs_remove(dfs_mt, NULL, m_Name.c_str(), true, NULL);
 }
 
 void FileDaos::CheckFile(const std::string hint) const
 {
-    if (m_FileDescriptor == -1)
+    if (!m_DAOSOpenSucceed)
     {
         throw std::ios_base::failure("ERROR: " + hint + SysErrMsg());
     }
@@ -306,36 +357,18 @@ void FileDaos::CheckFile(const std::string hint) const
 
 std::string FileDaos::SysErrMsg() const
 {
-    return std::string(": errno = " + std::to_string(m_Errno) + ": " +
-                       strerror(m_Errno));
+    std::string desc(d_errdesc(m_Errno));
+    return std::string(": errno = " + std::to_string(m_Errno) + ": " + desc);
 }
 
 void FileDaos::SeekToEnd()
 {
-    WaitForOpen();
-    errno = 0;
-    const int status = lseek(m_FileDescriptor, 0, SEEK_END);
-    m_Errno = 0;
-    if (status == -1)
-    {
-        throw std::ios_base::failure(
-            "ERROR: couldn't seek to the end of file " + m_Name +
-            ", in call to Daos IO lseek" + SysErrMsg());
-    }
+    m_GlobalOffset = GetSize();
 }
 
 void FileDaos::SeekToBegin()
 {
-    WaitForOpen();
-    errno = 0;
-    const int status = lseek(m_FileDescriptor, 0, SEEK_SET);
-    m_Errno = errno;
-    if (status == -1)
-    {
-        throw std::ios_base::failure(
-            "ERROR: couldn't seek to the begin of file " + m_Name +
-            ", in call to Daos IO lseek" + SysErrMsg());
-    }
+    m_GlobalOffset = 0;
 }
 
 } // end namespace transport
