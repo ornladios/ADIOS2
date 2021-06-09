8f31fdb2f313a5d6b3a5b81087268b72acb2b23c
Junmin Gu <jgu@lbl.gov>
2021-06-08 18:28:39 -0700

    clang-format

--- source/adios2/toolkit/query/XmlWorker.cpp
+++ source/adios2/toolkit/query/XmlWorker.cpp
@@ -94,8 +94,8 @@ void XmlWorker::ParseIONode(const pugi::xml_node &ioNode)
         const pugi::xml_node &variable = qTagNode.child("var");
         QueryVar *q =
             ParseVarNode(variable, m_SourceReader->m_IO, *m_SourceReader);
-        if ( !q )
-           continue;
+        if (!q)
+            continue;
 
         if (ref.first.size() == 0)
         {
@@ -143,7 +143,7 @@ QueryVar *XmlWorker::ParseVarNode(const pugi::xml_node &node,
     if (varType == DataType::None)
     {
         std::cerr << "No such variable: " << variableName << std::endl;
-        throw std::ios_base::failure("No such variable: "+variableName);
+        throw std::ios_base::failure("No such variable: " + variableName);
     }
 #define declare_type(T)                                                        \
     if (varType == helper::GetDataType<T>())                                   \
