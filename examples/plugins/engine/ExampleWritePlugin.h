e9835a38f7ec0648abfec801ed43a9973aa9265a
Caitlin Ross <caitlin.ross@kitware.com>
2021-06-10 16:29:48 -0400

    plugin engine: read/write example

---
+++ examples/plugins/engine/ExampleWritePlugin.h
@@ -0,0 +1,84 @@
+/*
+ * Distributed under the OSI-approved Apache License, Version 2.0.  See
+ * accompanying file Copyright.txt for details.
+ *
+ * ExampleWritePlugin.h Simple file writing engine. Creates directory
+ * (called "ExamplePlugin" by default, but can be changed with engine parameter
+ * "DirName") and writes variable info to vars.txt and actual data to data.txt.
+ *
+ *  Created on: Jul 5, 2021
+ *      Author: Chuck Atkins <chuck.atkins@kitware.com>
+ *              Caitlin Ross <caitlin.ross@kitware.com>
+ */
+
+#ifndef EXAMPLEWRITEPLUGIN_H_
+#define EXAMPLEWRITEPLUGIN_H_
+
+#include <fstream>
+#include <memory>
+#include <string>
+
+#include "adios2/common/ADIOSMacros.h"
+#include "adios2/common/ADIOSTypes.h"
+#include "adios2/core/IO.h"
+#include "adios2/engine/plugin/PluginEngineInterface.h"
+#include "adios2/helper/adiosComm.h"
+#include "adios2/helper/adiosType.h"
+
+namespace adios2
+{
+namespace core
+{
+namespace engine
+{
+
+/** An engine interface to be used by the plugin infrastructure */
+class ExampleWritePlugin : public PluginEngineInterface
+{
+public:
+    ExampleWritePlugin(IO &io, const std::string &name, const Mode openMode,
+                       helper::Comm comm);
+    virtual ~ExampleWritePlugin();
+
+    /** Indicates beginning of a step **/
+    StepStatus BeginStep(StepMode mode,
+                         const float timeoutSeconds = -1.0) override;
+
+    /** Indicates end of a step **/
+    void EndStep() override;
+
+    /** Return the current step **/
+    size_t CurrentStep() const override;
+
+    /** Execute deferred mode Puts **/
+    void PerformPuts() override;
+
+protected:
+    void Init() override;
+
+#define declare(T)                                                             \
+    void DoPutSync(Variable<T> &variable, const T *values) override;           \
+    void DoPutDeferred(Variable<T> &variable, const T *values) override;
+    ADIOS2_FOREACH_STDTYPE_1ARG(declare)
+#undef declare
+
+    void DoClose(const int transportIndex = -1) override;
+
+private:
+    std::ofstream m_DataFile;
+    std::ofstream m_VarFile;
+    size_t m_CurrentStep = 0;
+
+    void WriteVarsFromIO();
+
+    template <typename T>
+    void WriteVariableInfo(core::Variable<T> &variable);
+
+    template <typename T>
+    void WriteArray(Variable<T> &variable, const T *values);
+};
+
+} // end namespace engine
+} // end namespace core
+} // end namespace adios2
+#endif /* EXAMPLEWRITEPLUGIN_H_ */
---
+++ examples/plugins/engine/examplePluginEngine_write.cpp
@@ -0,0 +1,105 @@
+/*
+ * Distributed under the OSI-approved Apache License, Version 2.0.  See
+ * accompanying file Copyright.txt for details.
+ *
+ * examplePluginEngine_write.cpp example showing how to use ExampleWritePlugin
+ * engine
+ *
+ *  Created on: July 5, 2021
+ *      Author: Caitlin Ross <caitlin.ross@kitware.com>
+ */
+
+#include <ios>       //std::ios_base::failure
+#include <iostream>  //std::cout
+#include <stdexcept> //std::invalid_argument std::exception
+#include <vector>
+
+#include "adios2.h"
+#include "adios2/engine/plugin/PluginEngine.h"
+
+#include "ExampleWritePlugin.h"
+
+void testStreaming(adios2::Engine &writer, std::vector<float> &myFloats,
+                   adios2::Variable<float> &var)
+{
+    for (int i = 0; i < 2; i++)
+    {
+        if (i == 1)
+        {
+            for (auto &num : myFloats)
+            {
+                num *= 2;
+            }
+        }
+        writer.BeginStep();
+        writer.Put<float>(var, myFloats.data());
+        writer.EndStep();
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    bool streaming = false;
+    if (argc > 1)
+    {
+        streaming = std::atoi(argv[1]) == 1;
+    }
+
+    /** Application variable */
+    std::vector<float> myFloats = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    const std::size_t Nx = myFloats.size();
+
+    /** Register Plugin with the PluginEngine **/
+    adios2::core::engine::PluginEngine::RegisterPlugin<
+        adios2::core::engine::ExampleWritePlugin>("MyPlugin");
+
+    try
+    {
+        /** ADIOS class factory of IO class objects */
+        adios2::ADIOS adios;
+
+        /*** IO class object: settings and factory of Settings: Variables,
+         * Parameters, Transports, and Execution: Engines */
+        adios2::IO io = adios.DeclareIO("PluginIO");
+
+        /** global array: name, { shape (total dimensions) }, { start (local) },
+         * { count (local) }, all are constant dimensions */
+        adios2::Variable<float> var = io.DefineVariable<float>(
+            "data", {}, {}, {Nx}, adios2::ConstantDims);
+
+        /** Engine derived class, spawned to start IO operations */
+        io.SetEngine("Plugin");
+        io.SetParameters({{"PluginName", "MyPlugin"}});
+        adios2::Engine writer = io.Open("TestPlugin", adios2::Mode::Write);
+
+        if (streaming)
+        {
+            testStreaming(writer, myFloats, var);
+        }
+        else
+        {
+            writer.Put<float>(var, myFloats.data());
+            writer.PerformPuts();
+        }
+
+        /** Engine becomes unreachable after this*/
+        writer.Close();
+    }
+    catch (std::invalid_argument &e)
+    {
+        std::cout << "Invalid argument exception, STOPPING PROGRAM\n";
+        std::cout << e.what() << "\n";
+    }
+    catch (std::ios_base::failure &e)
+    {
+        std::cout << "IO System base failure exception, STOPPING PROGRAM\n";
+        std::cout << e.what() << "\n";
+    }
+    catch (std::exception &e)
+    {
+        std::cout << "Exception, STOPPING PROGRAM from rank\n";
+        std::cout << e.what() << "\n";
+    }
+
+    return 0;
+}
