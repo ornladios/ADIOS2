99bf665dc7a6e978b9b3a94c8646909fad60ee44
Caitlin Ross <caitlin.ross@kitware.com>
2021-06-18 11:54:07 -0400

    tau contract test: check that output isn't empty

--- testing/contract/tau/test.sh
+++ testing/contract/tau/test.sh
@@ -12,5 +12,8 @@ TAU=$(spack location -i tau)/bin/tau_exec
 
 mpiexec -np 2 ${TAU} ${build_dir}/bin/adios2-variables-shapes-cpp
 
+[ ! -f profile.0.0.0 ] || [ ! -s profile.0.0.0 ] && { echo "Error: file profile.0.0.0 not found or empty"; exit 1; }
+[ ! -f profile.1.0.0 ] || [ ! -s profile.1.0.0 ] && { echo "Error: file profile.1.0.0 not found or empty"; exit 1; }
+
 cat profile.0.0.0
 cat profile.1.0.0
