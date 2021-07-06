93781e9b5e96ac0d52c6b4b678e87c2369801f8f
Greg Eisenhauer <eisen@cc.gatech.edu>
2021-07-06 11:45:21 -0400

    clang-format

--- source/adios2/toolkit/format/buffer/chunk/ChunkV.cpp
+++ source/adios2/toolkit/format/buffer/chunk/ChunkV.cpp
@@ -26,7 +26,6 @@ ChunkV::~ChunkV()
 {
     for (const auto &Chunk : m_Chunks)
     {
-        std::cout << "freeing chunk " << (void *)Chunk << std::endl;
         free((void *)Chunk);
     }
 }
@@ -34,8 +33,6 @@ ChunkV::~ChunkV()
 size_t ChunkV::AddToVec(const size_t size, const void *buf, int align,
                         bool CopyReqd)
 {
-    std::cout << "In add to vec.  size " << size << " buf " << (void *)buf
-              << " align " << align << " Copy " << CopyReqd << std::endl;
     int badAlign = CurOffset % align;
     if (badAlign)
     {
@@ -48,26 +45,22 @@ size_t ChunkV::AddToVec(const size_t size, const void *buf, int align,
     if (size == 0)
         return CurOffset;
 
-    std::cout << "In add to vec" << std::endl;
     if (!CopyReqd && !m_AlwaysCopy)
     {
         // just add buf to internal version of output vector
         VecEntry entry = {true, buf, 0, size};
         DataV.push_back(entry);
-        std::cout << "In add to vec2" << std::endl;
     }
     else
     {
         // we can possibly append this entry to the last if the last was
         // internal
-        std::cout << "In add to vec3" << std::endl;
         bool AppendPossible = DataV.size() && !DataV.back().External;
 
         if (AppendPossible && (m_TailChunkPos + size > m_ChunkSize))
         {
             // No room in current chunk, close it out
             // realloc down to used size (helpful?) and set size in array
-            std::cout << "In add to vec5" << std::endl;
             m_Chunks.back() = (char *)realloc(m_Chunks.back(), m_TailChunkPos);
 
             m_TailChunkPos = 0;
@@ -76,7 +69,6 @@ size_t ChunkV::AddToVec(const size_t size, const void *buf, int align,
         }
         if (AppendPossible)
         {
-            std::cout << "In add to vec4" << std::endl;
             // We can use current chunk, just append the data;
             memcpy(m_TailChunk + m_TailChunkPos, buf, size);
             DataV.back().Size += size;
@@ -85,18 +77,13 @@ size_t ChunkV::AddToVec(const size_t size, const void *buf, int align,
         else
         {
             // We need a new chunk, get the larger of size or m_ChunkSize
-            std::cout << "In add to vec6 m_ChunkSize" << m_ChunkSize << std::endl;
             size_t NewSize = m_ChunkSize;
-            std::cout << "In add to vec7" << std::endl;
             if (size > m_ChunkSize)
                 NewSize = size;
-            std::cout << "In add to vec8 new size" << NewSize << std::endl;
             m_TailChunk = (char *)malloc(NewSize);
-            std::cout << "In add to vec9 size" << size << std::endl;
             memcpy(m_TailChunk, buf, size);
             m_TailChunkPos = size;
             VecEntry entry = {false, m_TailChunk, 0, size};
-            std::cout << "In add to vec10 size" << m_TailChunkPos << std::endl;
             DataV.push_back(entry);
         }
     }
