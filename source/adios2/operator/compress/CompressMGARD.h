c4fc04b118554f7e0af0756b96e5eea87e919a93
Jason Wang <jason.ruonan.wang@gmail.com>
2021-09-23 21:46:03 -0400

    made mgard buffer self-contained and backward-compatible

--- source/adios2/operator/compress/CompressMGARD.h
+++ source/adios2/operator/compress/CompressMGARD.h
@@ -60,6 +60,19 @@ public:
                       Params &info) final;
 
     bool IsDataTypeValid(const DataType type) const final;
+
+private:
+    /**
+     * Decompress function for V1 buffer. Do NOT remove even if the buffer
+     * version is updated. Data might be still in lagacy formats. This function
+     * must be kept for backward compatibility
+     * @param bufferIn : compressed data buffer (V1 only)
+     * @param sizeIn : number of bytes in bufferIn
+     * @param dataOut : decompressed data buffer
+     * @return : number of bytes in dataOut
+     */
+    size_t DecompressV1(const char *bufferIn, const size_t sizeIn,
+                        char *dataOut);
 };
 
 } // end namespace compress
