2a95d2e5b3762305fdfe27fdb30b25a8de86ad68
lwan86 <lipengwan86@gmail.com>
2021-10-12 22:14:12 -0400

    fix clang-format issue

--- source/utils/adios_iotest/processConfig.cpp
+++ source/utils/adios_iotest/processConfig.cpp
@@ -312,21 +312,21 @@ void printConfig(const Config &cfg)
         }
         switch (cmd->op)
         {
-        case Operation::Sleep: 
+        case Operation::Sleep:
         {
             auto cmdS = dynamic_cast<const CommandSleep *>(cmd.get());
             std::cout << "        Sleep for " << cmdS->sleepTime_us
                       << " microseconds " << std::endl;
             break;
         }
-        case Operation::Busy: 
+        case Operation::Busy:
         {
             auto cmdS = dynamic_cast<const CommandBusy *>(cmd.get());
             std::cout << "          Be busy for " << cmdS->busyTime_us
                       << " microseconds " << std::endl;
             break;
         }
-        case Operation::Write: 
+        case Operation::Write:
         {
             auto cmdW = dynamic_cast<CommandWrite *>(cmd.get());
             std::cout << "        Write to output " << cmdW->streamName
