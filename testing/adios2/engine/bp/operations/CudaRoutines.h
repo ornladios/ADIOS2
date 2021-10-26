827d92c58c08650cd0499ec1b98ef1b0f1e30559
Vicente Adolfo Bolea Sanchez <vicente.bolea@kitware.com>
2021-10-26 17:31:52 -0400

    ZFP: add tests for ZFP+CUDA

---
+++ testing/adios2/engine/bp/operations/CudaRoutines.h
@@ -0,0 +1,9 @@
+#ifndef __TESTING_ADIOS2_CUDA_ROUTINES_H__
+#define __TESTING_ADIOS2_CUDA_ROUTINES_H__
+
+#include <cuda.h>
+#include <cuda_runtime.h>
+
+void cuda_increment(int M, int N, int offset, float *vec, float val);
+
+#endif
