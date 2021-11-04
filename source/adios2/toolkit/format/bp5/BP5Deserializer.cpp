e3ed41b401e6fa61a8754ad6c248a6d4f0f8e73b
Greg Eisenhauer <eisen@cc.gatech.edu>
2021-11-04 11:17:22 -0400

    Fix modifiability

--- source/adios2/toolkit/format/bp5/BP5Deserializer.cpp
+++ source/adios2/toolkit/format/bp5/BP5Deserializer.cpp
@@ -698,12 +698,13 @@ void BP5Deserializer::InstallAttributeData(void *AttributeBlock,
             else if (Type == helper::GetDataType<std::string>())
             {
                 m_Engine->m_IO.DefineAttribute<std::string>(
-                    FieldName, *(char **)field_data);
+                    FieldName, *(char **)field_data, "", "/", true);
             }
 #define declare_type(T)                                                        \
     else if (Type == helper::GetDataType<T>())                                 \
     {                                                                          \
-        m_Engine->m_IO.DefineAttribute<T>(FieldName, *(T *)field_data);        \
+        m_Engine->m_IO.DefineAttribute<T>(FieldName, *(T *)field_data, "",     \
+                                          "/", true);                          \
     }
 
             ADIOS2_FOREACH_PRIMITIVE_STDTYPE_1ARG(declare_type)
@@ -741,13 +742,14 @@ void BP5Deserializer::InstallAttributeData(void *AttributeBlock,
                     array[i].assign(str_array[i]);
                 }
                 m_Engine->m_IO.DefineAttribute<std::string>(
-                    FieldName, array.data(), array.size());
+                    FieldName, array.data(), array.size(), "", "/", true);
             }
 #define declare_type(T)                                                        \
     else if (Type == helper::GetDataType<T>())                                 \
     {                                                                          \
         T **array = *(T ***)field_data;                                        \
-        m_Engine->m_IO.DefineAttribute<T>(FieldName, (T *)array, ElemCount);   \
+        m_Engine->m_IO.DefineAttribute<T>(FieldName, (T *)array, ElemCount,    \
+                                          "", "/", true);                      \
     }
 
             ADIOS2_FOREACH_PRIMITIVE_STDTYPE_1ARG(declare_type)
