bb36df6b48b649d1bb497c58f8dc0f56748f4ba6
Jason Wang <jason.ruonan.wang@gmail.com>
2021-11-08 12:46:41 -0500

    changed all names with InsituMPI to SSC

--- testing/examples/heatTransfer/TestSSCMxM.cmake
+++ testing/examples/heatTransfer/TestSSCMxM.cmake
@@ -5,7 +5,7 @@
 
 include(ADIOSFunctions)
 
-add_test(NAME HeatTransfer.InsituMPI.MxM
+add_test(NAME HeatTransfer.SSC.MxM
   COMMAND ${MPIEXEC_EXECUTABLE} ${MPIEXEC_EXTRA_FLAGS}
     ${MPIEXEC_NUMPROC_FLAG} 4
       $<TARGET_FILE:heatTransfer_write_adios2>
@@ -17,9 +17,9 @@ add_test(NAME HeatTransfer.InsituMPI.MxM
         ${PROJECT_SOURCE_DIR}/examples/heatTransfer/heat_ssc.xml
         Write.bp Read.bp 2 2
 )
-set_tests_properties(HeatTransfer.InsituMPI.MxM PROPERTIES PROCESSORS 8)
+set_tests_properties(HeatTransfer.SSC.MxM PROPERTIES PROCESSORS 8)
 
-add_test(NAME HeatTransfer.InsituMPI.MxM.Dump
+add_test(NAME HeatTransfer.SSC.MxM.Dump
   COMMAND ${CMAKE_COMMAND}
     -DARG1=-d 
     -DINPUT_FILE=Read.bp
@@ -27,10 +27,10 @@ add_test(NAME HeatTransfer.InsituMPI.MxM.Dump
     -P "${PROJECT_BINARY_DIR}/$<CONFIG>/bpls.cmake"
 )
 
-add_test(NAME HeatTransfer.InsituMPI.MxM.Validate
+add_test(NAME HeatTransfer.SSC.MxM.Validate
   COMMAND ${DIFF_COMMAND} -u -w
     ${CMAKE_CURRENT_SOURCE_DIR}/HeatTransfer.Dump.txt
     Dump.txt
 )
 
-SetupTestPipeline(HeatTransfer.InsituMPI.MxM ";Dump;Validate" TRUE)
+SetupTestPipeline(HeatTransfer.SSC.MxM ";Dump;Validate" TRUE)
