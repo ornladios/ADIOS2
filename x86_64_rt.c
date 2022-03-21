f6735d459aaf376a1fb64bc5ffb0f24206c72020
dill Upstream <robot@adios2>
2022-03-21 07:43:11 -0400

    dill 2022-03-21 (4ea1b7ad)
    Code extracted from:
    
        https://github.com/GTkorvo/dill.git
    
    at commit 4ea1b7ad1d7e93dcfb360996822ebec157261a1d (master).
    
    Upstream Shortlog
    -----------------
    
    Chuck Atkins (2):
          00c7145c ci: Update CI infrastructure
          00a28127 ci: Drop Windows and nvhpc builds until issues can be addressed
    
    Greg Eisenhauer (9):
          b0bbddf9 Enable nvhpc compiler, code mods to pass
          22dee7f4 Enable Windows build
          6b0a8744 A shot at memory protection
          e200a974 diag
          5180bea2 diag
          a7f5d413 diag
          f790fd15 semi
          a84dd085 nvc warning fix
          4c26b29e Compile for universal binary
    
    eisen (6):
          6f6b3413 Find Perl
          e73f72b9 kill warnings
          9c3a069c Find Perl
          ca9a8ab1 different flush call
          cafebbae New flush
          a9540fe4 warnings

--- x86_64_rt.c
+++ x86_64_rt.c
@@ -1,9 +1,18 @@
 #include "config.h"
+#include <stdint.h>
+#include <stdio.h>
 #include "dill.h"
 #include "dill_internal.h"
+#ifdef HAVE_SYS_MMAN_H
 #include "sys/mman.h"
+#endif
 #ifdef HAVE_MEMORY_H
 #include "memory.h"
+#endif
+#ifdef USE_VIRTUAL_PROTECT
+#include <windows.h>
+#include <memoryapi.h>
+#include <intrin.h>
 #endif
 #include "x86.h"
 
@@ -23,7 +32,7 @@ x86_64_rt_call_link(char *code, call_t *t)
     int i;
 
     for(i=0; i< t->call_count; i++) {
-	unsigned long tmp = (unsigned long) t->call_locs[i].xfer_addr;
+	uintptr_t tmp = (uintptr_t) t->call_locs[i].xfer_addr;
 	long *call_addr = (long *) (code + t->call_locs[i].loc + 2);
 	memcpy(call_addr, &tmp, 8);
     }
@@ -38,14 +47,22 @@ x86_64_flush(void *base, void *limit)
 
 	/* flush every 8 bytes of preallocated insn stream. */
 	while((char*)ptr < (char*) limit) {
+#ifndef _MSC_VER
+#ifdef __x86_64__
 	    asm volatile ("clflush (%0)" : /* */ : "r" (ptr));
+#endif
+#else
+        _mm_clflush(ptr);
+#endif
 	    ptr = (char *)ptr + 8;
 	}
+#ifndef _MSC_VER
 	asm volatile("nop");
 	asm volatile("nop");
 	asm volatile("nop");
 	asm volatile("nop");
 	asm volatile("nop");
+#endif
     }
 #endif
 }    
@@ -65,6 +82,11 @@ x86_64_package_stitch(char *code, call_t *t, dill_pkg pkg)
 		      PROT_EXEC | PROT_READ | PROT_WRITE, 
 		      MAP_ANONYMOUS|MAP_PRIVATE, -1, 0);
     memcpy(tmp, code, pkg->code_size);
+#endif
+#ifdef USE_VIRTUAL_PROTECT
+    int result;
+    DWORD dummy;
+    result = VirtualProtect(tmp, pkg->code_size, PAGE_EXECUTE_READWRITE, &dummy);
 #endif
     return tmp + pkg->entry_offset;
 }
