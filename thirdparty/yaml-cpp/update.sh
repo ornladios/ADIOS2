547a8010942491e416db5a3105854b36ed2f840a
Chuck Atkins <chuck.atkins@kitware.com>
2022-03-29 01:30:49 -0400

    yaml-cpp: Bump release to 0.7.0

--- thirdparty/yaml-cpp/update.sh
+++ thirdparty/yaml-cpp/update.sh
@@ -8,9 +8,12 @@ readonly name="yaml-cpp"
 readonly ownership="yaml-cpp Upstream <robot@adios2>"
 readonly subtree="thirdparty/yaml-cpp/yaml-cpp"
 readonly repo="https://github.com/jbeder/yaml-cpp.git"
-readonly tag="yaml-cpp-0.6.3"
+readonly tag="yaml-cpp-0.7.0"
 readonly shortlog="true"
 readonly paths="
+  LICENSE
+  include/
+  src/
 "
 
 extract_source () {
