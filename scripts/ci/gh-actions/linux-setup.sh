16012eeea28105ff2b8c5ff1bad4dbc36ff8d8da
Vicente Adolfo Bolea Sanchez <vicente.bolea@kitware.com>
2022-09-15 12:39:51 -0400

    CMAKE: Use Ninja for gcc[9,10,11]
    - It also enable parallel builds for those builds.
    - It also enforce early-stop build on error.

--- scripts/ci/gh-actions/linux-setup.sh
+++ scripts/ci/gh-actions/linux-setup.sh
@@ -1,17 +1,31 @@
-#!/bin/bash
+#!/bin/bash --login
 
-set -e
+set -ex
 
 export CI_ROOT_DIR="${GITHUB_WORKSPACE}/.."
 export CI_SOURCE_DIR="${GITHUB_WORKSPACE}"
+
+declare -r local_scripts_dir="$(dirname -- $0)/config"
+
+echo "**********Install dependencies Begin**********"
+
+mkdir -p "${CI_ROOT_DIR}/.local/bin" || true
+
+# Append bin to the workflow PATH file
+echo "${CI_ROOT_DIR}/.local/bin" >> "$GITHUB_PATH"
+
+find "$local_scripts_dir" -type f -name '*.sh' -perm /a=x -exec ./{} \;
+find "$local_scripts_dir" -type f -name '*.cmake' -exec cmake --trace -VV -P {} \;
+
+echo "**********Install dependencies End**********"
 
 SETUP_SCRIPT=${CI_SOURCE_DIR}/scripts/ci/setup/ci-${GH_YML_JOBNAME}.sh
 
 if [ -x "${SETUP_SCRIPT}" ]
 then
   echo "**********Setup Begin**********"
-  echo ${SETUP_SCRIPT}
-  ${SETUP_SCRIPT}
+  echo "${SETUP_SCRIPT}"
+  "${SETUP_SCRIPT}"
   echo "**********Setup End**********"
 fi
 
