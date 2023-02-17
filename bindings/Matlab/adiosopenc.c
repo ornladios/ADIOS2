ee2752c141c464495483b6d505804d600e8aba53
Norbert Podhorszki <pnorbert@ornl.gov>
2023-02-17 05:13:51 -0800

    Fix Matlab build which depends on the C bindings not the C++ bindings.

--- bindings/Matlab/adiosopenc.c
+++ bindings/Matlab/adiosopenc.c
@@ -147,7 +147,7 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
 
     /********************************************************/
     /* Open ADIOS file now and get variables and attributes */
-    adiosobj = adios2_init();
+    adiosobj = adios2_init(false);
     group = adios2_declare_io(adiosobj, "matlabiogroup"); // name is arbitrary
     fp = adios2_open(group, fname, adios2_mode_read);
     if (fp == NULL)
