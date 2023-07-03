ce73176881354c77a0b7aca1aabddcad15b29470
Greg Eisenhauer <eisen@cc.gatech.edu>
2023-07-03 13:55:04 -0400

    Merge pull request #3684 from eisenhauer/FixLeak
    Fix memory leak when there are Joined Arrays in streaming mode
    
    (cherry picked from commit 68b282cbb023e037d3cbdaed57aa7de28b2c7650)

--- source/adios2/toolkit/format/bp5/BP5Deserializer.h
+++ source/adios2/toolkit/format/bp5/BP5Deserializer.h
@@ -250,6 +250,7 @@ private:
     /* We assume operators are not thread-safe, call Decompress() one at a time
      */
     std::mutex mutexDecompress;
+    std::vector<void *> *m_FreeableJDOA = nullptr;
 };
 
 } // end namespace format
