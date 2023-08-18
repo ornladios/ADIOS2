3a6cb62320c559809abb188ab4df4e751f21ff11
Norbert Podhorszki <pnorbert@ornl.gov>
2023-08-18 07:13:09 -0400

    Rename test. fix windows compile errors.

--- testing/adios2/unit/TestChunkV.cpp
+++ testing/adios2/unit/TestChunkV.cpp
@@ -21,17 +21,18 @@ namespace adios2
 namespace format
 {
 
-static void print_array(const std::string name, const uint8_t *ptr, const uint8_t size)
+static void print_array(const std::string name, const uint8_t *ptr,
+                        const size_t size)
 {
     std::cout << name << " ptr = " << (void *)ptr << "= [ ";
-    for (uint8_t n = 0; n < size; ++n)
+    for (size_t n = 0; n < size; ++n)
     {
         std::cout << (unsigned int)ptr[n] << " ";
     }
     std::cout << "]\n";
 }
 
-TEST(ADIOS2ChunkV, ADIOS2ChunkVAlignment)
+TEST(ChunkV, AlignmentToMemBlockSize)
 {
     /* Align chunks to a certain byte size, here 32 */
     size_t MemBlockSize = 32;
@@ -40,15 +41,18 @@ TEST(ADIOS2ChunkV, ADIOS2ChunkVAlignment)
 
     /* Align "allocation" to a certain byte size, like type size, here 8 */
     size_t AlignnmentSize = 8;
-    /* Make allocation not aligned to either AlignmentSize nor the MemBlockSize*/
+    /* Make allocation not aligned to either AlignmentSize nor the
+     * MemBlockSize*/
     size_t AllocSize = 51;
 
     {
-        adios2::format::BufferV::BufferPos pos = b.Allocate(AllocSize, AlignnmentSize);
+        adios2::format::BufferV::BufferPos pos =
+            b.Allocate(AllocSize, AlignnmentSize);
         ASSERT_EQ(pos.bufferIdx, 0);
         ASSERT_EQ(pos.globalPos, 0);
         ASSERT_EQ(pos.posInBuffer, 0);
-        uint8_t *ptr = reinterpret_cast<uint8_t *>(b.GetPtr(pos.bufferIdx, pos.posInBuffer));
+        uint8_t *ptr = reinterpret_cast<uint8_t *>(
+            b.GetPtr(pos.bufferIdx, pos.posInBuffer));
         for (uint8_t n = 0; n < AllocSize; ++n)
         {
             ptr[n] = n;
@@ -57,17 +61,20 @@ TEST(ADIOS2ChunkV, ADIOS2ChunkVAlignment)
 
         std::vector<core::iovec> vec = b.DataVec();
         ASSERT_EQ(vec.size(), 1);
-        /* First block is alone in first chunk, so only real length (51) should be here */
+        /* First block is alone in first chunk, so only real length (51) should
+         * be here */
         ASSERT_EQ(vec[0].iov_len, 51);
     }
     {
         /* second block should fit in existing chunk but aligned to 56 bytes
            (first AllocSize aligned to AlignmentSize is 56) */
-        adios2::format::BufferV::BufferPos pos = b.Allocate(AllocSize, AlignnmentSize);
+        adios2::format::BufferV::BufferPos pos =
+            b.Allocate(AllocSize, AlignnmentSize);
         ASSERT_EQ(pos.bufferIdx, 0);
         ASSERT_EQ(pos.globalPos, 56);
         ASSERT_EQ(pos.posInBuffer, 56);
-        uint8_t *ptr = reinterpret_cast<uint8_t *>(b.GetPtr(pos.bufferIdx, pos.posInBuffer));
+        uint8_t *ptr = reinterpret_cast<uint8_t *>(
+            b.GetPtr(pos.bufferIdx, pos.posInBuffer));
         for (uint8_t n = 0; n < AllocSize; ++n)
         {
             ptr[n] = n;
@@ -84,11 +91,13 @@ TEST(ADIOS2ChunkV, ADIOS2ChunkVAlignment)
            - first chunk should be 128 bytes long (aligned to 32)
            - globalPos should be 128 (0 in this second chunk)
         */
-        adios2::format::BufferV::BufferPos pos = b.Allocate(AllocSize, AlignnmentSize);
+        adios2::format::BufferV::BufferPos pos =
+            b.Allocate(AllocSize, AlignnmentSize);
         ASSERT_EQ(pos.bufferIdx, 1);
         ASSERT_EQ(pos.globalPos, 128);
         ASSERT_EQ(pos.posInBuffer, 0);
-        uint8_t *ptr = reinterpret_cast<uint8_t *>(b.GetPtr(pos.bufferIdx, pos.posInBuffer));
+        uint8_t *ptr = reinterpret_cast<uint8_t *>(
+            b.GetPtr(pos.bufferIdx, pos.posInBuffer));
         for (uint8_t n = 0; n < AllocSize; ++n)
         {
             ptr[n] = n;
@@ -97,13 +106,17 @@ TEST(ADIOS2ChunkV, ADIOS2ChunkVAlignment)
 
         std::vector<core::iovec> vec = b.DataVec();
         ASSERT_EQ(vec.size(), 2);
-        /* First chunk must be finalized and aligned to MemBlockSize, so should be 128 now */
+        /* First chunk must be finalized and aligned to MemBlockSize, so should
+         * be 128 now */
         ASSERT_EQ(vec[0].iov_len, 128);
-        /* Third block is alone in second chunk, so only real length (51) should be here */
+        /* Third block is alone in second chunk, so only real length (51) should
+         * be here */
         ASSERT_EQ(vec[1].iov_len, 51);
 
-        const uint8_t *chunk0 = reinterpret_cast<const uint8_t *>(vec[0].iov_base);
-        const uint8_t *chunk1 = reinterpret_cast<const uint8_t *>(vec[1].iov_base);
+        const uint8_t *chunk0 =
+            reinterpret_cast<const uint8_t *>(vec[0].iov_base);
+        const uint8_t *chunk1 =
+            reinterpret_cast<const uint8_t *>(vec[1].iov_base);
         /* first array in chunk0[0..AllocSize-1]*/
         ASSERT_EQ(chunk0[0], 0);
         ASSERT_EQ(chunk0[AllocSize - 1], AllocSize - 1);
