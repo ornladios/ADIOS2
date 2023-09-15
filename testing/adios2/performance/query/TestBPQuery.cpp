dc4ec1bba1210b7755de5fc7eac540d9cdeb3c2b
Junmin Gu <jgu@lbl.gov>
2023-09-15 13:19:41 -0700

    moved QueryWorker to the beginstep/endstep loop because bp5 metadata loading pattern
    deprecate bp3 engine test and replaced  with  bp5 engine test

--- testing/adios2/performance/query/TestBPQuery.cpp
+++ testing/adios2/performance/query/TestBPQuery.cpp
@@ -14,7 +14,6 @@
 
 #include <gtest/gtest.h>
 
-// #include "../engine/SmallTestData.h"
 
 // std::string engineName; // comes from command line
 
@@ -98,7 +97,6 @@ void BPQueryTest::QueryIntVar(const std::string &fname, adios2::ADIOS &adios,
     std::string queryFile = "./" + ioName + "test.xml"; //"./test.xml";
     std::cout << ioName << std::endl;
     WriteXmlQuery1D(queryFile, ioName, "intV");
-    adios2::QueryWorker w = adios2::QueryWorker(queryFile, bpReader);
 
     std::vector<size_t> rr;
     if (engineName.compare("BP4") == 0)
@@ -108,6 +106,7 @@ void BPQueryTest::QueryIntVar(const std::string &fname, adios2::ADIOS &adios,
 
     while (bpReader.BeginStep() == adios2::StepStatus::OK)
     {
+        adios2::QueryWorker w = adios2::QueryWorker(queryFile, bpReader);
         std::vector<adios2::Box<adios2::Dims>> touched_blocks;
         adios2::Box<adios2::Dims> empty;
         w.GetResultCoverage(empty, touched_blocks);
@@ -135,7 +134,6 @@ void BPQueryTest::QueryDoubleVar(const std::string &fname, adios2::ADIOS &adios,
     // std::string queryFile = "./.test.xml";
     std::string queryFile = "./" + ioName + "test.xml";
     WriteXmlQuery1D(queryFile, ioName, "doubleV");
-    adios2::QueryWorker w = adios2::QueryWorker(queryFile, bpReader);
 
     std::vector<size_t> rr; //= {0,9,9};
     if (engineName.compare("BP4") == 0)
@@ -144,6 +142,7 @@ void BPQueryTest::QueryDoubleVar(const std::string &fname, adios2::ADIOS &adios,
         rr = {0, 1, 1};
     while (bpReader.BeginStep() == adios2::StepStatus::OK)
     {
+        adios2::QueryWorker w = adios2::QueryWorker(queryFile, bpReader);
         std::vector<adios2::Box<adios2::Dims>> touched_blocks;
         adios2::Box<adios2::Dims> empty;
         w.GetResultCoverage(empty, touched_blocks);
@@ -234,9 +233,9 @@ void BPQueryTest::WriteFile(const std::string &fname, adios2::ADIOS &adios,
 // 1D  test data
 //******************************************************************************
 
-TEST_F(BPQueryTest, BP3)
+TEST_F(BPQueryTest, BP5)
 {
-    std::string engineName = "BP3";
+    std::string engineName = "BP5";
     // Each process would write a 1x8 array and all processes would
     // form a mpiSize * Nx 1D array
     const std::string fname(engineName + "Query1D.bp");
