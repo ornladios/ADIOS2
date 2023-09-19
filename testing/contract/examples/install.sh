b881acc22e2332c078adee5a12a2c9047fdc4f13
Spiros Tsalikis <spiros.tsalikis@kitware.com>
2023-09-19 12:38:19 -0400

    Remove testing based on ADIOS2-examples

--- testing/contract/examples/install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -x
-set -e
-
-source $(dirname $(readlink -f ${BASH_SOURCE}))/setup.sh
-
-cmake --install ${build_dir}
--- testing/contract/tau/depends.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set -x
-set -e
-
-sudo /opt/spack/bin/spack install -v tau ~fortran ~papi ~pdt ~otf2
