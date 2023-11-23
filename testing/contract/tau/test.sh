fab66d4bd4d33de87ce8f73ff0269dcc582339b2
Spiros Tsalikis <spiros.tsalikis@kitware.com>
2023-11-23 12:10:23 -0500

    Enable Shell-Check for tau contract scripts

--- testing/contract/tau/test.sh
+++ testing/contract/tau/test.sh
@@ -3,14 +3,19 @@
 set -x
 set -e
 
-source $(dirname $(readlink -f ${BASH_SOURCE}))/setup.sh
+# shellcheck disable=SC1091
+source "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")/setup.sh"
 
-mkdir -p ${test_dir}
-cd ${test_dir}
+# Fail if is not set
+install_dir="${install_dir:?}"
+test_dir="${test_dir:?}"
+
+mkdir -p "${test_dir}"
+cd "${test_dir}"
 
 TAU=$(spack location -i tau)/bin/tau_exec
 
-mpiexec -np 2 ${TAU} ${install_dir}/bin/adios2_basics_variablesShapes
+mpiexec -np 2 "${TAU}" "${install_dir}/bin/adios2_basics_variablesShapes"
 
 [ ! -f profile.0.0.0 ] || [ ! -s profile.0.0.0 ] && { echo "Error: file profile.0.0.0 not found or empty"; exit 1; }
 [ ! -f profile.1.0.0 ] || [ ! -s profile.1.0.0 ] && { echo "Error: file profile.1.0.0 not found or empty"; exit 1; }
