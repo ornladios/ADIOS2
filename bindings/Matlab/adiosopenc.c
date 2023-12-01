e9b7c0caa74664ffd9e1b5b4446f9b20f39b2b93
Tom Gade <tomgade09@gmail.com>
2023-12-01 09:28:56 -0600

    Update adiosopenc.c to use adios2_init_serial regardless of MPI use.

--- bindings/Matlab/adiosopenc.c
+++ bindings/Matlab/adiosopenc.c
@@ -140,11 +140,7 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
 
     /********************************************************/
     /* Open ADIOS file now and get variables and attributes */
-#if ADIOS2_USE_MPI
-    adiosobj = adios2_init(false);
-#else
-    adiosobj = adios2_init();
-#endif
+    adiosobj = adios2_init_serial();
     group = adios2_declare_io(adiosobj, "matlabiogroup"); // name is arbitrary
     fp = adios2_open(group, fname, adios2_mode_read);
     if (fp == NULL)
