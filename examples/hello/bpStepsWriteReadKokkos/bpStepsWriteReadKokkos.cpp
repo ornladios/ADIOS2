c6317ba17cbf63603532fa80f694c7461d5637b1
anagainaru <ana.gainaru@gmail.com>
2023-12-10 14:03:41 -0500

    Update the BP Kokkos example to be able to use any combination of memory spaces

--- examples/hello/bpStepsWriteReadKokkos/bpStepsWriteReadKokkos.cpp
+++ examples/hello/bpStepsWriteReadKokkos/bpStepsWriteReadKokkos.cpp
@@ -3,144 +3,161 @@
  * accompanying file Copyright.txt for details.
  *
  * bpStepsWriteReadKokkos.cpp  Simple example of writing and reading bpFloats through ADIOS2 BP
- * engine with multiple simulations steps for every IO step using Kokkos
+ * engine using Kokkos on any memory space
  */
 #include <ios>
 #include <iostream>
-#include <stdexcept> //std::invalid_argument std::exception
 #include <vector>
 
 #include <adios2.h>
 
 #include <Kokkos_Core.hpp>
 
-void writer(adios2::ADIOS &adios, const std::string &engine, const std::string &fname,
-            const size_t Nx, unsigned int nSteps)
+template <class MemSpace, class ExecSpace>
+int BPWrite(const std::string fname, const size_t Nx, const size_t Ny, const size_t nSteps,
+            const std::string engine)
 {
-    // Initialize the simulation bpFloats with the default memory space
-    using mem_space = Kokkos::DefaultExecutionSpace::memory_space;
-    Kokkos::View<float *, mem_space> gpuSimData("simBuffer", Nx);
-    Kokkos::parallel_for(
-        "initBuffer", Kokkos::RangePolicy<Kokkos::DefaultExecutionSpace>(0, Nx),
-        KOKKOS_LAMBDA(int i) { gpuSimData(i) = static_cast<float>(i); });
+    // Initialize the simulation data
+    Kokkos::View<float **, MemSpace> gpuSimData("simBuffer", Nx, Ny);
+    static_assert(Kokkos::SpaceAccessibility<ExecSpace, MemSpace>::accessible, "");
+    Kokkos::parallel_for("initBuffer", Kokkos::RangePolicy<ExecSpace>(0, Nx), KOKKOS_LAMBDA(int i) {
+        for (int j = 0; j < Ny; j++)
+            gpuSimData(i, j) = static_cast<float>(i);
+    });
     Kokkos::fence();
 
-    // Set up the ADIOS structures
-    adios2::IO bpIO = adios.DeclareIO("WriteIO");
-    bpIO.SetEngine(engine);
+    adios2::ADIOS adios;
+    adios2::IO io = adios.DeclareIO("WriteIO");
+    io.SetEngine(engine);
 
-    const adios2::Dims shape{static_cast<size_t>(Nx)};
-    const adios2::Dims start{static_cast<size_t>(0)};
-    const adios2::Dims count{Nx};
-    auto bpFloats = bpIO.DefineVariable<float>("bpFloats", shape, start, count);
-    auto bpStep = bpIO.DefineVariable<unsigned int>("bpStep");
+    const adios2::Dims shape{Nx, Ny};
+    const adios2::Dims start{0, 0};
+    const adios2::Dims count{Nx, Ny};
+    auto data = io.DefineVariable<float>("bpFloats", shape, start, count);
 
-    adios2::Engine bpWriter = bpIO.Open(fname, adios2::Mode::Write);
+    adios2::Engine bpWriter = io.Open(fname, adios2::Mode::Write);
 
     // Simulation steps
     for (unsigned int step = 0; step < nSteps; ++step)
     {
-        // Make a 1D selection to describe the local dimensions of the
-        // variable we write and its offsets in the global spaces
-        adios2::Box<adios2::Dims> sel({0}, {Nx});
-        bpFloats.SetSelection(sel);
+        adios2::Box<adios2::Dims> sel({0, 0}, {Nx, Ny});
+        data.SetSelection(sel);
 
-        // Start IO step every write step
         bpWriter.BeginStep();
-        bpWriter.Put(bpFloats, gpuSimData.data());
-        bpWriter.Put(bpStep, step);
+        bpWriter.Put(data, gpuSimData.data());
         bpWriter.EndStep();
 
-        // Update values in the simulation bpFloats using the default
-        // execution space
-        Kokkos::parallel_for(
-            "updateBuffer", Kokkos::RangePolicy<Kokkos::DefaultExecutionSpace>(0, Nx),
-            KOKKOS_LAMBDA(int i) { gpuSimData(i) += 10; });
+        // Update values in the simulation data
+        Kokkos::parallel_for("updateBuffer", Kokkos::RangePolicy<ExecSpace>(0, Nx),
+                             KOKKOS_LAMBDA(int i) {
+                                 for (int j = 0; j < Ny; j++)
+                                     gpuSimData(i, j) += 10;
+                             });
         Kokkos::fence();
     }
 
     bpWriter.Close();
-    Kokkos::DefaultExecutionSpace exe_space;
+    ExecSpace exe_space;
     std::cout << "Done writing on memory space: " << exe_space.name() << std::endl;
+    return 0;
 }
 
-void reader(adios2::ADIOS &adios, const std::string &engine, const std::string &fname,
-            const size_t Nx, unsigned int /*nSteps*/)
+template <class MemSpace>
+std::array<size_t, 2> GetDimenstions(adios2::Variable<float> data)
 {
-    // Create ADIOS structures
-    adios2::IO bpIO = adios.DeclareIO("ReadIO");
-    bpIO.SetEngine(engine);
+    return {data.Shape()[1], data.Shape()[0]};
+}
 
-    Kokkos::DefaultExecutionSpace exe_space;
+template <>
+std::array<size_t, 2> GetDimenstions<Kokkos::HostSpace>(adios2::Variable<float> data)
+{
+    return {data.Shape()[0], data.Shape()[1]};
+}
+
+template <class MemSpace, class ExecSpace>
+int BPRead(const std::string fname, const std::string engine)
+{
+    adios2::ADIOS adios;
+    adios2::IO io = adios.DeclareIO("ReadIO");
+    io.SetEngine(engine);
+
+    ExecSpace exe_space;
     std::cout << "Read on memory space: " << exe_space.name() << std::endl;
 
-    adios2::Engine bpReader = bpIO.Open(fname, adios2::Mode::Read);
+    adios2::Engine bpReader = io.Open(fname, adios2::Mode::Read);
 
-    using mem_space = Kokkos::DefaultExecutionSpace::memory_space;
-    Kokkos::View<float *, mem_space> gpuSimData("simBuffer", Nx);
-    unsigned int inStep = 0;
-    for (unsigned int step = 0; bpReader.BeginStep() == adios2::StepStatus::OK; ++step)
+    unsigned int step = 0;
+    for (; bpReader.BeginStep() == adios2::StepStatus::OK; ++step)
     {
-        auto bpFloats = bpIO.InquireVariable<float>("bpFloats");
-        if (bpFloats)
+        auto data = io.InquireVariable<float>("bpFloats");
+        if (data.Shape().size() != 2)
         {
-            const adios2::Dims start{0};
-            const adios2::Dims count{Nx};
-            const adios2::Box<adios2::Dims> sel(start, count);
-            bpFloats.SetSelection(sel);
+            std::cout << "Error, the bpFloats variable in the BP file "
+                         " on step "
+                      << step << "needs to have two dimensions" << std::endl;
+            break;
+        }
 
-            bpReader.Get(bpFloats, gpuSimData.data());
-        }
-        auto bpStep = bpIO.InquireVariable<unsigned int>("bpStep");
-        if (bpStep)
-        {
-            bpReader.Get(bpStep, &inStep);
-        }
+        const adios2::Dims start{0, 0};
+        const adios2::Box<adios2::Dims> sel(start, data.Shape());
+        data.SetSelection(sel);
+
+        auto dims = GetDimenstions<MemSpace>(data);
+        size_t Nx = dims[0], Ny = dims[1];
+        Kokkos::View<float **, MemSpace> gpuSimData("simBuffer", Nx, Ny);
+        bpReader.Get(data, gpuSimData.data());
         bpReader.EndStep();
-        if (inStep != step)
+
+        auto cpuData = Kokkos::create_mirror_view_and_copy(Kokkos::HostSpace{}, gpuSimData);
+        for (size_t i = 0; i < Nx; i++)
         {
-            std::cout << "ERROR: step mismatch\n";
-            return;
+            for (size_t j = 0; j < Ny; j++)
+                std::cout << cpuData(i, j) << " ";
+            std::cout << std::endl;
         }
     }
 
     bpReader.Close();
+    return 0;
 }
 
 int main(int argc, char **argv)
 {
-    Kokkos::initialize(argc, argv);
-
     const std::string engine = argv[1] ? argv[1] : "BPFile";
     std::cout << "Using engine " << engine << std::endl;
+    const size_t Nx = 6, Ny = 10, nSteps = 1;
+    const std::string fromMemSpace = "Default";
+    const std::string toMemSpace = "Default";
 
-    const std::string filename = engine + "StepsWriteReadCuda.bp";
-    const unsigned int nSteps = 10;
-    const unsigned int Nx = 6000;
-    try
+    const std::string filename = engine + "StepsWriteReadKokkos";
+    Kokkos::initialize(argc, argv);
     {
-        /** ADIOS class factory of IO class objects */
-        adios2::ADIOS adios;
+        std::cout << "Using engine " << engine << std::endl;
 
-        writer(adios, engine, filename, Nx, nSteps);
-        reader(adios, engine, filename, Nx, nSteps);
-    }
-    catch (std::invalid_argument &e)
-    {
-        std::cout << "Invalid argument exception, STOPPING PROGRAM\n";
-        std::cout << e.what() << "\n";
-    }
-    catch (std::ios_base::failure &e)
-    {
-        std::cout << "IO System base failure exception, STOPPING PROGRAM\n";
-        std::cout << e.what() << "\n";
-    }
-    catch (std::exception &e)
-    {
-        std::cout << "Exception, STOPPING PROGRAM\n";
-        std::cout << e.what() << "\n";
+        if (fromMemSpace == "Default")
+        {
+            using mem_space = Kokkos::DefaultExecutionSpace::memory_space;
+            std::cout << "Writing on memory space: DefaultMemorySpace" << std::endl;
+            BPWrite<mem_space, Kokkos::DefaultExecutionSpace>(filename + ".bp", Nx, Ny, nSteps,
+                                                              engine);
+        }
+        else
+        {
+            std::cout << "Writing on memory space: HostSpace" << std::endl;
+            BPWrite<Kokkos::HostSpace, Kokkos::Serial>(filename + ".bp", nSteps, Nx, Ny, engine);
+        }
+        if (toMemSpace == "Default")
+        {
+            using mem_space = Kokkos::DefaultExecutionSpace::memory_space;
+            std::cout << "Reading on memory space: DefaultMemorySpace" << std::endl;
+            BPRead<mem_space, Kokkos::DefaultExecutionSpace>(filename + ".bp", engine);
+        }
+        else
+        {
+            std::cout << "Reading on memory space: HostSpace" << std::endl;
+            BPRead<Kokkos::HostSpace, Kokkos::Serial>(filename + ".bp", engine);
+        }
     }
     Kokkos::finalize();
-
     return 0;
 }
