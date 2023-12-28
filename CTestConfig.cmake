5b60bd8ec8db10441592de1d41c62346c2e2c6e0
Greg Eisenhauer <eisen@cc.gatech.edu>
2023-12-28 13:04:04 +1100

    Run all ASAN tests

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -14,35 +14,4 @@ set(MEMORYCHECK_SUPPRESSIONS_FILE ${CMAKE_SOURCE_DIR}/scripts/dashboard/nightly/
 
 # Ignore tests that are currently failing, remove tests here as they are fixed
 list(APPEND CTEST_CUSTOM_MEMCHECK_IGNORE 
-Staging.TimeoutReader.1x1.CommMin.BP5.SST
-Staging.1x1Struct.BP5
-Staging.WriteMemorySelectionRead.1x1.CommMin.BP.SST
-Staging.1x1.Local2.CommMin.BP.SST
-Staging.OnDemandSingle.1x1.CommMin.BP5.SST
-Staging.AllToAllDistribution.1x1x3.CommMin.BP5.SST
-Staging.RoundRobinDistribution.1x1x3.CommMin.BP5.SST
-Staging.1x1.LocalMultiblock.CommMin.BP5.SST
-Staging.1x1.SstRUDP.CommMin.BP5.SST
-Staging.WriteMemorySelectionRead.1x1.CommMin.BP5.SST
-Staging.1x1LockGeometry.CommMin.BP5.SST
-Staging.1x1.ForcePreload.CommMin.BP5.SST
-Staging.1x1.NoPreload.CommMin.BP5.SST
-Staging.DiscardWriter.1x1.CommMin.BP5.SST
-Staging.LatestReaderHold.1x1.CommMin.BP5.SST
-Staging.LatestReader.1x1.CommMin.BP5.SST
-Engine.BP.BPWriteReadAsStreamTestADIOS2.ReaderWriterDefineVariable.BP5.Serial
-Staging.ZFPCompression.3x5.CommMin.BP5.SST
-Staging.ZFPCompression.1x1.CommMin.BP5.SST
-Staging.1x1Joined.CommMin.BP5.SST
-Staging.1x1Struct.CommMin.BP5.SST
-Staging.1x1.Local2.CommMin.BP5.SST
-Staging.1x1VarDestruction.CommMin.BP5.SST
-Staging.1x1.ModAttrs.CommMin.BP5.SST
-Staging.1x1.Attrs.CommMin.BP5.SST
-Staging.1x1.CommMin.BP5.SST
-Engine.SST.SstWriteFails.InvalidBeginStep.Serial
-remoteServerCleanup
-Remote.BPWriteMemorySelectionRead.FileRemote
-Remote.BPWriteMemorySelectionRead.GetRemote
-Remote.BPWriteReadADIOS2stdio.GetRemote
 )
