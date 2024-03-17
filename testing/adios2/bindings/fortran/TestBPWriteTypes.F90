6d0e2834313826144f9f27ce9c8ff299acbb2535
Greg Eisenhauer <eisen@cc.gatech.edu>
2024-03-17 10:15:46 -0400

    Always use derived_variable, even if not enabled

--- testing/adios2/bindings/fortran/TestBPWriteTypes.F90
+++ testing/adios2/bindings/fortran/TestBPWriteTypes.F90
@@ -164,10 +164,10 @@ program TestBPWriteTypes
       adios2_null_dims, &
       adios2_constant_dims, ierr)
 
-#if ADIOS2_USE_Derived_Variable
    ! derived variable
    call adios2_define_derived_variable(derived_variable, ioWrite, "derived/magnitude_of_var_R64", &
       "x:var_R64 y:var_R64 z:var_R64 magnitude(x,y,z)", adios2_derived_var_type_metadata_only, ierr)
+#if ADIOS2_USE_Derived_Variable
 #define TOTAL_VAR_COUNT 15
 #else
 #define TOTAL_VAR_COUNT 14
