7d7c7b7855424b0da08794554c85816178fcd1c9
Greg Eisenhauer <eisen@cc.gatech.edu>
2024-05-28 13:24:59 -0400

    remove unnecessary assert

--- source/adios2/toolkit/remote/remote_server.cpp
+++ source/adios2/toolkit/remote/remote_server.cpp
@@ -513,7 +513,6 @@ static bool server_timeout(void *CMvoid, int time_since_service)
 
 #define IMAX_BITS(m) ((m) / ((m) % 255 + 1) / 255 % 255 * 8 + 7 - 86 / ((m) % 255 + 12))
 #define RAND_MAX_WIDTH IMAX_BITS(RAND_MAX)
-_Static_assert((RAND_MAX & (RAND_MAX + 1u)) == 0, "RAND_MAX not a Mersenne number");
 
 uint64_t rand64(void)
 {
