1fc30ef872ed383dce8eb4f8b6d3b5f892fcb01a
Greg Eisenhauer <eisen@cc.gatech.edu>
2024-07-31 17:01:57 -0500

    EVpath upstream (#4277)
    * EVPath 2024-07-19 (de1ab63f)
    
    Code extracted from:
    
        https://github.com/GTkorvo/EVPath.git
    
    at commit de1ab63f3b0d6ee2a76cd37a17515a4fdcd01091 (master).
    
    Upstream Shortlog
    -----------------
    
    * EVPath 2024-07-31 (771d3a4e)
    
    Code extracted from:
    
        https://github.com/GTkorvo/EVPath.git
    
    at commit 771d3a4e8bc5b5f7f9d772e1acab89eee50dd7c6 (master).
    
    Upstream Shortlog
    -----------------
    
    ---------
    
    Co-authored-by: EVPath Upstream <robot@adios2>

--- thirdparty/EVPath/EVPath/ip_config.c
+++ thirdparty/EVPath/EVPath/ip_config.c
@@ -334,7 +334,27 @@ get_qual_hostname(char *buf, int len, attr_list attrs,
     trace_func(trace_data, "CM<IP_CONFIG> - Tentative Qualified hostname %s", buf);
     if (memchr(buf, '.', strlen(buf)) == NULL) {
 	/* useless hostname if it's not fully qualified */
-	buf[0] = 0;
+	struct addrinfo hints, *info, *p;
+	int gai_result;
+
+	char hostname[1024];
+	hostname[1023] = '\0';
+	gethostname(hostname, 1023);
+
+	memset(&hints, 0, sizeof hints);
+	hints.ai_family = AF_UNSPEC; /*either IPV4 or IPV6*/
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_flags = AI_CANONNAME;
+
+	if ((gai_result = getaddrinfo(hostname, NULL, &hints, &info)) != 0) {
+	    fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(gai_result));
+	}
+
+	for(p = info; p != NULL; p = p->ai_next) {
+	    strcpy(buf, p->ai_canonname);
+	}
+
+	freeaddrinfo(info);
     }
     if ((buf[0] != 0) && ((host = gethostbyname(buf)) == NULL)) {
 	/* useless hostname if we can't translate it */
