1fb431e13376c717a240e274ed7a114d64d0f820
Vicente Adolfo Bolea Sanchez <vicente.bolea@kitware.com>
2024-07-31 14:59:53 -0400

    ci: disable tests windows builds

--- scripts/ci/cmake/ci-win2022-vs2022-msmpi.cmake
+++ scripts/ci/cmake/ci-win2022-vs2022-msmpi.cmake
@@ -23,5 +23,9 @@ set(CTEST_CMAKE_GENERATOR "Visual Studio 17 2022")
 set(CTEST_CMAKE_GENERATOR_PLATFORM "x64")
 list(APPEND CTEST_UPDATE_NOTES_FILES "${CMAKE_CURRENT_LIST_FILE}")
 # https://github.com/ornladios/ADIOS2/issues/4276
-set(CTEST_TEST_ARGS EXCLUDE "Api.Python.FileReader|Api.Python.BPWriteTypesHighLevelAPI_HDF5")
+# https://github.com/ornladios/ADIOS2/issues/4279
+# https://github.com/ornladios/ADIOS2/issues/4278
+set(CTEST_TEST_ARGS
+    EXCLUDE "Api.Python.FileReader|Api.Python.BPWriteTypesHighLevelAPI_HDF5|Api.Python.BPWriteTypesHighLevelAPI.MPI"
+)
 include(${CMAKE_CURRENT_LIST_DIR}/ci-common.cmake)
--- scripts/ci/cmake/ci-win2022-vs2022-serial.cmake
+++ scripts/ci/cmake/ci-win2022-vs2022-serial.cmake
@@ -26,6 +26,7 @@ set(CTEST_CMAKE_GENERATOR "Visual Studio 17 2022")
 set(CTEST_CMAKE_GENERATOR_PLATFORM "x64")
 list(APPEND CTEST_UPDATE_NOTES_FILES "${CMAKE_CURRENT_LIST_FILE}")
 # https://github.com/ornladios/ADIOS2/issues/4276
+# https://github.com/ornladios/ADIOS2/issues/4279
 set(CTEST_TEST_ARGS EXCLUDE "Api.Python.FileReader|Api.Python.BPWriteTypesHighLevelAPI_HDF5")
 
 include(${CMAKE_CURRENT_LIST_DIR}/ci-common.cmake)
