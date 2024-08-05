35455db82da9e701c7cf20d7b2c955511bec9987
ffs Upstream <robot@adios2>
2024-08-05 14:38:22 -0500

    ffs 2024-08-05 (58aece47)
    Code extracted from:
    
        https://github.com/GTkorvo/ffs.git
    
    at commit 58aece47a424eeb5f78769264ee0ee0e50be0456 (master).
    
    Upstream Shortlog
    -----------------

--- version.c
+++ version.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-static char *FFS_version = "FFS Version 3.1.2  - Sun Aug  4 18:47:12 EDT 2024\n";
+static char *FFS_version = "FFS Version 3.1.3  - Mon Aug  5 14:59:11 EDT 2024\n";
 
 void
 FFSprint_version()
