178c136efa2df1a7225a35db978e0265473b923e
Greg Eisenhauer <eisen@cc.gatech.edu>
2024-08-19 16:36:27 -0400

    Fix so that has correct arguments for non-root

--- testing/adios2/engine/bp/start_xrootd.sh
+++ testing/adios2/engine/bp/start_xrootd.sh
@@ -4,6 +4,6 @@ set -x
 
 if [ "$(id -u)" -eq 0 ]; then
     # we run as root in CI in docker images, this is OK, but we have to tell XRootD that it's OK.
-    XROOTD_USER="daemon"
+    XROOTD_USER="-R daemon"
 fi
-"$1" -b -l /tmp/xroot.log -R "$XROOTD_USER" -w "$2" -c "$2"/xroot/etc/xrootd/xrootd-ssi.cfg
+"$1" -b -l /tmp/xroot.log "$XROOTD_USER" -w "$2" -c "$2"/xroot/etc/xrootd/xrootd-ssi.cfg
