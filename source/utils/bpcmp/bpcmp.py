ed2b3524d144fdbec7d73c024af8082f0f860d5f
anagainaru <ana.gainaru@gmail.com>
2024-08-30 22:26:13 -0400

    Changes to the python script
    Co-authored-by: Vicente Adolfo Bolea Sanchez <vicente.bolea@kitware.com>

--- source/utils/bpcmp/bpcmp.py
+++ source/utils/bpcmp/bpcmp.py
@@ -1,17 +1,25 @@
 import numpy as np
 import sys
+import errno
 import argparse
 from adios2 import Stream
 import itertools
 
 verbose = False
 
-# Function to detect the mismatch between two values
-# Input: val1, val2 - type T scalars
-#        tolerance - tolerance value for comparison
-#        tolperc - tolerance percentage between the two values
-# Output: true (if there is a mismatch), false otherwise
 def DetectMismatch(val1, val2, tolerance, tolperc):
+    """
+    Detects the mismatch between two scalar of any comparable type.
+
+    Args:
+        val1 : Any type scalar
+        val2 : Any type scalar
+        tolerance (float) : Tolerance used by the comparison
+        tolperc (float) : Accepted tolerance between the two values as percentage
+
+    Returns:
+        bool: true (if there is a mismatch), false otherwise
+    """
     mismatch = True
     if val1 == val2:
         return False
@@ -26,12 +34,19 @@ def DetectMismatch(val1, val2, tolerance, tolperc):
         mismatch = (val1 != val2)
     return mismatch
 
-# Function to detect the mismatch between two N dim arrays
-# Input: array1, array2 - N dimensional arrays
-#        tolerance - tolerance value for comparison
-#        tolperc - tolerance percentage between the two values
-# Output: a list of mismatch values in the array: (index, value1, value2)
 def CompareValues(array1, array2, tolerance, tolperc):
+    """
+    Detects the mismatch between two N dim arrays
+
+    Args:
+        array1 : N dimensional array
+        array2 : N dimensional array
+        tolerance (float) : Tolerance used by the comparison
+        tolperc (float) : Accepted tolerance between the two values as percentage
+
+    Returns:
+        list(tuple): a list of mismatch values in the array: (index, value1, value2)
+    """
     mismatch = []
     dimensions = array1.shape
     for idx in itertools.product(*[range(s) for s in dimensions]):
@@ -39,20 +54,27 @@ def CompareValues(array1, array2, tolerance, tolperc):
             mismatch.append((idx, array1[idx], array2[idx]))
     return mismatch
 
-# Function to detect the mismatch between two BP files
-# Input: filename1, filename2 - BP file name
-#        tolerance - tolerance value for comparison
-#        tolperc - tolerance percentage between the two values
-#        stats - boolean that constrols if stats or values are compared
-# Output: total number of mismatches between the two files
 def CompareFiles(filename1, filename2, tolerance, tolperc, stats=False):
+    """
+    Detects the mismatch between two BP files
+
+    Args:
+        filename1 : BP file name
+        filename2 : BP file name
+        tolerance (float) : Tolerance used by the comparison
+        tolperc (float) : Accepted tolerance between the two values as percentage
+        stats (bool) : boolean that constrols if stats are compared or not
+
+    Returns:
+        int: total number of mismatches between the two files
+    """
     err = 0
     with Stream(filename1, 'rra') as s1, Stream(filename2, 'rra') as s2:
         # compare total number of steps
         num_steps = s1.num_steps()
         if num_steps != s2.num_steps():
-            print("DIFFER : NUM_STEPS : " + str(num_steps) + " <> " + str(s2.num_steps()) +
-                  " : " + filename1 + " : " + filename2)
+            print(f"DIFFER : NUM_STEPS : {num_steps} <> {s2.num_steps()} : {filename1}"
+                  f" : {filename2}")
             num_steps = min(num_steps, s2.num_steps())
             err += 1
 
@@ -62,8 +84,7 @@ def CompareFiles(filename1, filename2, tolerance, tolperc, stats=False):
         notinS2 = np.setdiff1d(list(varInfoS1.keys()), list(varInfoS2.keys()))
         # compare total number of written variables
         if len(notinS1) > 0 or len(notinS1) > 0:
-            print("DIFFER : AVAIL_VARS : " + str(notinS1) + " <> " + str(notinS2) + " : " +
-                  filename1 + " : " + filename2)
+            print(f"DIFFER : AVAIL_VARS : {notinS1} <> {notinS2} : {filename1} : {filename2}")
             err += 1
 
         # compare each common variable
@@ -71,34 +92,30 @@ def CompareFiles(filename1, filename2, tolerance, tolperc, stats=False):
             # compare number of steps
             varSteps = int(varInfoS1[varName]["AvailableStepsCount"])
             if varSteps != int(varInfoS2[varName]["AvailableStepsCount"]):
-                print("DIFFER : VARIABLE : " + varName + " : NUM_STEPS : " +
-                      str(varInfoS1[varName]["AvailableStepsCount"]) + " <> " +
-                      str(varInfoS2[varName]["AvailableStepsCount"]) + " : " + filename1 + " : " +
-                      filename2)
+                print(f"DIFFER : VARIABLE : {varName} : NUM_STEPS: "
+                      f"{varInfoS1[varName]['AvailableStepsCount']} <> "
+                      f"{varInfoS2[varName]['AvailableStepsCount']} : {filename1} : {filename2}")
                 varSteps = min(varSteps, int(varInfoS2[varName]["AvailableStepsCount"]))
                 err += 1
 
             # compare types
             if varInfoS1[varName]["Type"] != varInfoS2[varName]["Type"]:
-                print("DIFFER : VARIABLE : " + varName + " : TYPE : " +
-                      str(varInfoS1[varName]["Type"]) + " <> " + str(varInfoS2[varName]["Type"]) +
-                      " : " + filename1 + " : " + filename2)
+                print(f"DIFFER : VARIABLE : {varName} : TYPE : {varInfoS1[varName]['Type']}"
+                      f" <> {varInfoS2[varName]['Type']} : {filename1} : {filename2}")
                 err += 1
 
             # compare dimensions (single value and shape)
             if varInfoS1[varName]["SingleValue"] != varInfoS2[varName]["SingleValue"]:
-                print("DIFFER : VARIABLE : " + varName + " : SINGLE_VALUE : " +
-                      str(varInfoS1[varName]["SingleValue"]) + " <> " +
-                      str(varInfoS2[varName]["SingleValue"]) + " : " + filename1 + " : " +
-                      filename2)
+                print(f"DIFFER : VARIABLE : {varName} : SINGLE_VALUE : "
+                      f"{varInfoS1[varName]['SingleValue']} <> "
+                      f"{varInfoS2[varName]['SingleValue']} : {filename1} : {filename2}")
                 err += 1
                 # no point in comparing values if there is a difference in shape
                 continue
 
             if varInfoS1[varName]["Shape"] != varInfoS2[varName]["Shape"]:
-                print("DIFFER : VARIABLE : " + varName + " : SHAPE : " +
-                      str(varInfoS1[varName]["Shape"]) + " <> " +
-                      str(varInfoS2[varName]["Shape"]) + " : " + filename1 + " : " + filename2)
+                print(f"DIFFER : VARIABLE : {varName} : SHAPE : {varInfoS1[varName]['Shape']}"
+                      f" <> {varInfoS2[varName]['Shape']} : {filename1} : {filename2}")
                 err += 1
                 # no point in comparing values if there is a difference in shape
                 continue
@@ -108,16 +125,14 @@ def CompareFiles(filename1, filename2, tolerance, tolperc, stats=False):
                 if DetectMismatch(float(varInfoS1[varName]["Min"]),
                                   float(varInfoS2[varName]["Min"]),
                                   tolerance, tolperc):
-                    print("DIFFER : VARIABLE : " + varName + " : MIN : " +
-                          str(varInfoS1[varName]["Min"]) + " <> " +
-                          str(varInfoS2[varName]["Min"]) + " : " + filename1 + " : " + filename2)
+                    print(f"DIFFER : VARIABLE : {varName} : MIN : {varInfoS1[varName]['Min']}"
+                          f" <> {varInfoS2[varName]['Min']} : {filename1} : {filename2}")
                     err += 1
                 if DetectMismatch(float(varInfoS1[varName]["Max"]),
                                   float(varInfoS2[varName]["Max"]),
                                   tolerance, tolperc):
-                    print("DIFFER : VARIABLE : " + varName + " : MAX : " +
-                          str(varInfoS1[varName]["Max"]) + " <> " +
-                          str(varInfoS2[varName]["Max"]) + " : " + filename1 + " : " + filename2)
+                    print(f"DIFFER : VARIABLE : {varName} : MAX : {varInfoS1[varName]['Max']}"
+                          f" <> {varInfoS2[varName]['Max']} : {filename1} : {filename2}")
                     err += 1
             # compare values
             for step in range(varSteps):
@@ -129,13 +144,11 @@ def CompareFiles(filename1, filename2, tolerance, tolperc, stats=False):
                 err += len(ret)
                 if verbose:
                     for (idx, value1, value2) in ret:
-                        print("DIFFER : VARIABLE : " + varName + " : STEP : " + str(step) +
-                              " : POSITION : " + str(idx) + " : VALUE : " + str(value1) +
-                              " <> " + str(value2) + " : " + filename1 + " : " + filename2)
+                        print(f"DIFFER : VARIABLE : {varName} : STEP : {step} : POSITION : "
+                              f"{idx} : VALUE : {value1} <> {value2} : {filename1} : {filename2}")
                 else:
-                    print("DIFFER : VARIABLE : " + varName + " : STEP : " + str(step) +
-                          " : VALUES_SUMMARY : " + str(len(ret)) + " : " + filename1 +
-                          " : " + filename2)
+                    print(f"DIFFER : VARIABLE : {varName} : STEP : {step} : VALUES_SUMMARY : "
+                          f"{len(ret)} : {filename1} : {filename2}")
 
     return err
 
@@ -159,18 +172,18 @@ if __name__ == '__main__':
     args = parser.parse_args()
     if len(args.bpfiles) < 2:
         print("WARNING : At least two files need to be provided for any comparison")
-        exit()
+        exit(errno.ENOEXEC)
 
     verbose = args.verbose
 
     diff_found = 0
     for i in range(1, len(args.bpfiles)):
-        print("COMPARE : FILE :", args.bpfiles[0], ": FILE :", args.bpfiles[i])
+        print(f"COMPARE : FILE : {args.bpfiles[0]} : FILE : {args.bpfiles[i]}")
         diff_file = CompareFiles(args.bpfiles[0], args.bpfiles[i], args.tolerance, args.tolperc,
                                  args.stats)
         if diff_file > 0:
-            print("TOTAL_DIFFERENCES : " + str(diff_file) + " : FILE :", args.bpfiles[0],
-                  ": FILE :", args.bpfiles[i])
+            print(f"TOTAL_DIFFERENCES : {diff_file} : FILE : {args.bpfiles[0]} : FILE : "
+                  f"{args.bpfiles[i]}")
             diff_found += 1
 
     if diff_found == 0:
