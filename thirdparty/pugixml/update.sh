5d94c21f8c9c858126c6fbed10beee4deb032e3b
Vicente Adolfo Bolea Sanchez <vicente.bolea@kitware.com>
2025-01-10 16:20:14 -0500

    pugixml: bump to 1.15

--- thirdparty/pugixml/update.sh
+++ thirdparty/pugixml/update.sh
@@ -8,7 +8,7 @@ readonly name="pugixml"
 readonly ownership="pugixml Upstream <robot@adios2>"
 readonly subtree="thirdparty/pugixml/pugixml"
 readonly repo="https://github.com/zeux/pugixml.git"
-readonly tag="v1.14"
+readonly tag="v1.15"
 readonly shortlog="true"
 readonly paths="
 src/pugiconfig.hpp
