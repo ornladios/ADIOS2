f1c8707e6e366aa867a00a80ecea13c9dac8b812
Norbert Podhorszki <pnorbert@ornl.gov>
2025-04-30 15:09:31 -0400

    New reading engine "TimeSeries" that can read .ats text file listing a series of BP/H5 files.
    - Both Read and ReadRandomAccess modes are supported.
    - Full paths and relative paths to other directories are allowed in each line.
    - Mix of BP4, BP5 and HDF5 files are allowed.
    - Stream mode only returns `end of stream` when a line starting with `--end` encountered. `--end` is not required for ReadRandomAccess mode.
    - Empty lines and comments starting with `#` are ignored.
    
    Format of the ATS file:
    ```
    gs100.bp
    gs300.bp
    gs500.bp
    --end--
    ```
    
    - Test case with changing variable shape for BP3/BP4/BP5 engines

---
+++ source/adios2/engine/timeseries/TimeSeriesReader.h
@@ -0,0 +1,174 @@
+/*
+ * Distributed under the OSI-approved Apache License, Version 2.0.  See
+ * accompanying file Copyright.txt for details.
+ *
+ * TimeSeries.h
+ * A meta engine to process a series of files using existing engines.
+ * File format:
+ *   filename1
+ *   filename2
+ *   filename3
+ *   --end--
+ *
+ * --end-- signals that there is no more files coming to the stream
+ *
+ *  Created on: Apr 30, 2025
+ *      Author: Norbert Podhorszki pnorbert@ornl.gov
+ */
+
+#ifndef ADIOS2_ENGINE_TIMESERIESREADER_H_
+#define ADIOS2_ENGINE_TIMESERIESREADER_H_
+
+#include "adios2/common/ADIOSConfig.h"
+#include "adios2/core/ADIOS.h"
+#include "adios2/core/Engine.h"
+#include "adios2/helper/adiosComm.h"
+#include "adios2/helper/adiosFunctions.h"
+
+#include <deque>
+#include <fstream>
+
+namespace adios2
+{
+namespace core
+{
+namespace engine
+{
+
+class TimeSeriesReader : public Engine
+{
+public:
+    /**
+     * Constructor for single BP capsule engine, writes in BP format into a
+     * single
+     * heap capsule
+     * @param name unique name given to the engine
+     * @param accessMode
+     * @param comm
+     * @param method
+     * @param hostLanguage
+     */
+    TimeSeriesReader(IO &adios, const std::string &name, const Mode mode, helper::Comm comm);
+
+    ~TimeSeriesReader();
+    StepStatus BeginStep(StepMode mode = StepMode::Read, const float timeoutSeconds = -1.0) final;
+    void PerformGets() final;
+    size_t CurrentStep() const final;
+    void EndStep() final;
+
+    MinVarInfo *MinBlocksInfo(const VariableBase &, const size_t Step) const;
+    bool VarShape(const VariableBase &Var, const size_t Step, Dims &Shape) const;
+    bool VariableMinMax(const VariableBase &, const size_t Step, MinMaxStruct &MinMax);
+    std::string VariableExprStr(const VariableBase &Var);
+
+private:
+    int m_Verbosity = 0;
+    int m_ReaderRank; // my rank in the readers' comm
+    std::ifstream m_ATSFile;
+    size_t m_ATSFileLastPos = 0;
+    std::string m_ATSFileDir; // directory of the ATS file, for relative paths
+    std::deque<std::string> m_Filenames;
+
+    // m_NoMoreFiles: becomes true when --end-- is read from the ATS file but we still have
+    // to finish processing the files in m_Filenames
+    bool m_NoMoreFiles = false;
+
+    int m_CurrentStep = 0;
+    size_t m_StepsCount = 0;
+    int m_StartStepInCurrentFile = 0; // separate record for streamin mode
+    bool m_FirstStep = true;
+    bool m_BetweenStepPairs = false;
+    bool m_NeedPerformGets = false;
+
+    std::vector<adios2::core::IO *> m_IOs;
+    std::vector<adios2::core::Engine *> m_Engines;
+
+    struct VarInternalInfoSingle
+    {
+        void *originalVar; // Variable<T> in the actual IO
+        size_t ioIdx;      // actual IO in m_IOs
+        size_t engineIdx;  // actual engine in m_Engines
+        size_t startStep;  // start step in the engine
+        size_t endStep;    // end step in the engine
+        size_t stepCount;  // number of steps in the engine
+        VarInternalInfoSingle(void *p, size_t i, size_t e) : originalVar(p), ioIdx(i), engineIdx(e)
+        {
+        }
+    };
+
+    struct VarInternalInfo
+    {
+        const std::string name;                  // variable name
+        size_t nEngines;                         // number of engines this variable
+        std::vector<VarInternalInfoSingle> info; // Per-engine variable information
+        VarInternalInfo(const std::string &name, size_t n) : name(name), nEngines(n)
+        {
+            info.reserve(n);
+        }
+    };
+    std::unordered_map<std::string, VarInternalInfo> m_VarInternalInfo;
+
+    /**
+     * Create a new variable with name `name` in `io`
+     * based on an existing variable.
+     */
+    template <class T>
+    Variable<T> DuplicateVariable(Variable<T> *variable, IO &io, size_t ioIdx, size_t engineIdx,
+                                  T min, T max);
+
+    /**
+     * Create a new attribute with name `name` in `io`
+     * based on an existing attribute.
+     */
+    template <class T>
+    Attribute<T> DuplicateAttribute(Attribute<T> *attribute, IO &io);
+
+    size_t FindStep(const VarInternalInfo &vii, const size_t step) const;
+
+    void Init() final; ///< called from constructor, gets the selected TimeSeries
+                       /// transport method from settings
+    void InitParameters() final;
+    void InitTransports() final;
+    void ProcessIO(adios2::core::IO &io, adios2::core::Engine &e);
+    void InitFile(std::string filename,
+                  bool process); // open one file and process its variables and attributes
+    bool CheckForFiles();        // read (new) entries in ATS file until --end--
+
+#define declare_type(T)                                                                            \
+    void DoGetSync(Variable<T> &, T *) final;                                                      \
+    void DoGetDeferred(Variable<T> &, T *) final;
+    ADIOS2_FOREACH_STDTYPE_1ARG(declare_type)
+#undef declare_type
+
+    void DoClose(const int transportIndex = -1);
+
+    size_t DoSteps() const final;
+
+#define declare_type(T)                                                                            \
+    std::map<size_t, std::vector<typename Variable<T>::BPInfo>> DoAllStepsBlocksInfo(              \
+        const Variable<T> &variable) const final;                                                  \
+                                                                                                   \
+    std::vector<std::vector<typename Variable<T>::BPInfo>> DoAllRelativeStepsBlocksInfo(           \
+        const Variable<T> &) const final;                                                          \
+                                                                                                   \
+    std::vector<typename Variable<T>::BPInfo> DoBlocksInfo(const Variable<T> &variable,            \
+                                                           const size_t step) const final;
+
+    ADIOS2_FOREACH_STDTYPE_1ARG(declare_type)
+#undef declare_type
+
+    /**
+     * Called if destructor is called on an open engine.  Should warn or take
+     * any non-complex measure that might help recover.
+     */
+    void DestructorClose(bool Verbose) noexcept final{};
+
+    template <class T>
+    void GetCommon(Variable<T> &variable, T *data, adios2::Mode mode);
+};
+
+} // end namespace engine
+} // end namespace core
+} // end namespace adios2
+
+#endif /* ADIOS2_ENGINE_TIMESERIESREADER_H_ */
