f3a92f24c43230cc7064120711f78e1ffb99e82f
Greg Eisenhauer <eisen@cc.gatech.edu>
2025-06-12 15:16:06 -0400

    WIP:  Operator Parameters

--- source/adios2/operator/OperatorFactory.h
+++ source/adios2/operator/OperatorFactory.h
@@ -9,7 +9,9 @@
  */
 
 #include "adios2/common/ADIOSTypes.h"
+#include "adios2/core/Engine.h"
 #include "adios2/core/Operator.h"
+#include "adios2/core/VariableBase.h"
 #include <memory>
 
 namespace adios2
@@ -22,7 +24,10 @@ std::string OperatorTypeToString(const Operator::OperatorType type);
 std::shared_ptr<Operator> MakeOperator(const std::string &type, const Params &parameters);
 
 size_t Decompress(const char *bufferIn, const size_t sizeIn, char *dataOut, MemorySpace memSpace,
-                  std::shared_ptr<Operator> op = nullptr);
+                  std::shared_ptr<Operator> op = nullptr, Engine *engine = nullptr,
+                  VariableBase *var = nullptr);
+
+Params CreateOperatorParams(const Engine *engine, const VariableBase *variable);
 
 } // end namespace core
 } // end namespace adios2
