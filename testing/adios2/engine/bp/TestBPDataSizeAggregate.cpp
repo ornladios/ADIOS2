e203a9c6b2c45a8b9d2e635512a4bbb918ef2c2c
Scott Wittenburg <scott.wittenburg@kitware.com>
2025-06-16 19:39:56 -0600

    Test improvements
    * Make sure we know the expected value at every location
    * Allow supplying aggregation type and number of subfiles on cli
    * Allow arbitrary ordering the rank data sizes
        The goal here is to add another test that will write two
        time steps, such that for the second step, only *some* of
        the ranks change which subfile they are writing, while the
        rest keep writing to the same subfile.

--- testing/adios2/engine/bp/TestBPDataSizeAggregate.cpp
+++ testing/adios2/engine/bp/TestBPDataSizeAggregate.cpp
@@ -15,15 +15,25 @@ int worldRank, worldSize;
 
 namespace
 {
-uint64_t sum0ToN(uint64_t n)
+uint64_t sumFirstN(const std::vector<uint64_t> &vec, uint64_t n)
 {
     uint64_t sum = 0;
-    for (size_t i = 1; i <= n; ++i)
+    for (int i = 0; i < n; ++i)
     {
-        sum += i;
+        sum += vec[i];
     }
     return sum;
 }
+
+void swap(std::vector<uint64_t>& vec, uint64_t idx1, uint64_t idx2)
+{
+    uint64_t tmp = vec[idx1];
+    vec[idx1] = vec[idx2];
+    vec[idx2] = tmp;
+}
+
+std::string aggregationType;  // comes from command line
+std::string numberOfSubFiles; // comes from command line
 }
 
 class DSATest : public ::testing::Test
@@ -36,20 +46,36 @@ TEST_F(DSATest, TestWriteUnbalancedData)
 {
     adios2::ADIOS adios(MPI_COMM_WORLD);
 
+    std::vector<uint64_t> rankDataSizes(worldSize);
+    std::iota(rankDataSizes.begin(), rankDataSizes.end(), 1);
+    // swap(rankDataSizes, 1, 2);
+
+    std::cout << "Rank data sizes (in number of columns)" << std::endl;
+    for (int i = 0; i < rankDataSizes.size(); ++i)
+    {
+        std::cout << rankDataSizes[i] << " ";
+    }
+    std::cout << std::endl;
+
     uint64_t globalNx = worldSize;
-    uint64_t globalNy = sum0ToN(globalNx);
+    uint64_t globalNy = sumFirstN(rankDataSizes, rankDataSizes.size());
 
     {
         // Define local data, size varies by rank
-        uint64_t localNy = worldRank + 1;
+        uint64_t localNy = rankDataSizes[worldRank];
+        uint64_t localOffset = sumFirstN(rankDataSizes, worldRank);
+        std::cout << "Rank " << worldRank << " local offset: " << localOffset << std::endl;
         std::vector<double> rankData;
         rankData.resize(globalNx * localNy);
         uint64_t localElementCount = rankData.size();
 
         adios2::IO bpIO = adios.DeclareIO("WriteIO");
         bpIO.SetEngine("BPFile");
-        bpIO.SetParameter("AggregationType", "DataSizeBased");
-        bpIO.SetParameter("NumAggregators", "2");
+
+        std::cout << "Setting AggregationType to " << aggregationType << std::endl;
+        bpIO.SetParameter("AggregationType", aggregationType);
+        std::cout << "Setting NumSubFiles to " << numberOfSubFiles << std::endl;
+        bpIO.SetParameter("NumSubFiles", numberOfSubFiles);
 
         adios2::Variable<double> varGlobalArray =
             bpIO.DefineVariable<double>("GlobalArray", {globalNx, globalNy});
@@ -58,15 +84,18 @@ TEST_F(DSATest, TestWriteUnbalancedData)
 
         bpWriter.BeginStep();
 
-        for (uint64_t i = 0; i < localElementCount; ++i)
+        uint64_t index = 0;
+        for (uint64_t x = 0; x < globalNx; ++x)
         {
-            rankData[i] =
-                globalNx * worldSize * 1.0 + worldRank * localNy * 1.0 + static_cast<double>(i);
-            ;
+            uint64_t value = (x * globalNy) + localOffset;
+            for (uint64_t y = 0; y < localNy; ++y)
+            {
+                rankData[index++] = value++;
+            }
         }
 
         varGlobalArray.SetSelection(
-            adios2::Box<adios2::Dims>({0, static_cast<size_t>(sum0ToN(worldRank))},
+            adios2::Box<adios2::Dims>({0, static_cast<size_t>(localOffset)},
                                       {globalNx, static_cast<size_t>(localNy)}));
         bpWriter.Put<double>(varGlobalArray, rankData.data());
         bpWriter.EndStep();
@@ -96,6 +125,11 @@ TEST_F(DSATest, TestWriteUnbalancedData)
         bpReader.Get(var_array, array, adios2::Mode::Sync);
         ASSERT_EQ(array.size(), var_array.Shape()[0] * var_array.Shape()[1]);
 
+        for (size_t i = 0; i < array.size(); ++i)
+        {
+            ASSERT_EQ(array[i], static_cast<double>(i));
+        }
+
         bpReader.Close();
     }
 }
@@ -106,6 +140,16 @@ int main(int argc, char **argv)
     MPI_Comm_rank(MPI_COMM_WORLD, &worldRank);
     MPI_Comm_size(MPI_COMM_WORLD, &worldSize);
     ::testing::InitGoogleTest(&argc, argv);
+
+    if (argc > 1)
+    {
+        aggregationType = std::string(argv[1]);
+    }
+    if (argc > 2)
+    {
+        numberOfSubFiles = std::string(argv[2]);
+    }
+
     int result = RUN_ALL_TESTS();
 
     MPI_Finalize();
