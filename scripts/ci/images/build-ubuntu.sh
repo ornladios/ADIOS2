baf84d6359942edc8f966f2821d4a11998f31ce6
Vicente Adolfo Bolea Sanchez <vicente.bolea@kitware.com>
2025-07-01 12:02:02 -0400

    ci: use podman for building ubuntu images

--- scripts/ci/images/build-ubuntu.sh
+++ scripts/ci/images/build-ubuntu.sh
@@ -3,7 +3,7 @@
 set -ex
 
 # Build the base image
-docker build --progress=plain \
+podman build --progress=plain \
   --build-arg EXTRA_VARIANTS="+blosc+ssc ^mgard@2023-01-10" \
   --build-arg PATCH_VARIANT_XROOTD=ON \
   --rm -f ./Dockerfile.ci-spack-ubuntu20.04-base \
@@ -11,16 +11,16 @@ docker build --progress=plain \
   .
 
 # Build the gcc8, gcc9, and gcc10 images
-docker build --rm --build-arg GCC_VERSION=8 -f ./Dockerfile.ci-spack-ubuntu20.04-gcc -t ghcr.io/ornladios/adios2:ci-spack-ubuntu20.04-gcc8 .
-docker build --rm --build-arg GCC_VERSION=10 -f ./Dockerfile.ci-spack-ubuntu20.04-gcc -t ghcr.io/ornladios/adios2:ci-spack-ubuntu20.04-gcc10 .
-docker build --rm --build-arg GCC_VERSION=9 -f ./Dockerfile.ci-spack-ubuntu20.04-gcc -t ghcr.io/ornladios/adios2:ci-spack-ubuntu20.04-gcc9 .
+podman build --rm --build-arg GCC_VERSION=8 -f ./Dockerfile.ci-spack-ubuntu20.04-gcc -t ghcr.io/ornladios/adios2:ci-spack-ubuntu20.04-gcc8 .
+podman build --rm --build-arg GCC_VERSION=10 -f ./Dockerfile.ci-spack-ubuntu20.04-gcc -t ghcr.io/ornladios/adios2:ci-spack-ubuntu20.04-gcc10 .
+podman build --rm --build-arg GCC_VERSION=9 -f ./Dockerfile.ci-spack-ubuntu20.04-gcc -t ghcr.io/ornladios/adios2:ci-spack-ubuntu20.04-gcc9 .
 
 # Build the clang6 and clang10 images
-docker build --rm --build-arg CLANG_VERSION=6.0 -f ./Dockerfile.ci-spack-ubuntu20.04-clang -t ghcr.io/ornladios/adios2:ci-spack-ubuntu20.04-clang6 .
-docker build --rm --build-arg CLANG_VERSION=10 -f ./Dockerfile.ci-spack-ubuntu20.04-clang -t ghcr.io/ornladios/adios2:ci-spack-ubuntu20.04-clang10 .
+podman build --rm --build-arg CLANG_VERSION=6.0 -f ./Dockerfile.ci-spack-ubuntu20.04-clang -t ghcr.io/ornladios/adios2:ci-spack-ubuntu20.04-clang6 .
+podman build --rm --build-arg CLANG_VERSION=10 -f ./Dockerfile.ci-spack-ubuntu20.04-clang -t ghcr.io/ornladios/adios2:ci-spack-ubuntu20.04-clang10 .
 
 # Build the ubuntu 22.04 img
-docker build \
+podman build \
   --progress=plain \
   --rm \
   --build-arg BASE_IMAGE="ecpe4s/ubuntu22.04-runner-amd64-gcc-11.4:2024.04.19" \
@@ -31,10 +31,10 @@ docker build \
   .
 
 # Push images to github container registry
-docker push ghcr.io/ornladios/adios2:ci-spack-ubuntu20.04-base
-docker push ghcr.io/ornladios/adios2:ci-spack-ubuntu20.04-gcc8
-docker push ghcr.io/ornladios/adios2:ci-spack-ubuntu20.04-gcc9
-docker push ghcr.io/ornladios/adios2:ci-spack-ubuntu20.04-gcc10
-docker push ghcr.io/ornladios/adios2:ci-spack-ubuntu20.04-clang6
-docker push ghcr.io/ornladios/adios2:ci-spack-ubuntu20.04-clang10
-docker push ghcr.io/ornladios/adios2:ci-spack-ubuntu22.04-gcc11
+podman push ghcr.io/ornladios/adios2:ci-spack-ubuntu20.04-base
+podman push ghcr.io/ornladios/adios2:ci-spack-ubuntu20.04-gcc8
+podman push ghcr.io/ornladios/adios2:ci-spack-ubuntu20.04-gcc9
+podman push ghcr.io/ornladios/adios2:ci-spack-ubuntu20.04-gcc10
+podman push ghcr.io/ornladios/adios2:ci-spack-ubuntu20.04-clang6
+podman push ghcr.io/ornladios/adios2:ci-spack-ubuntu20.04-clang10
+podman push ghcr.io/ornladios/adios2:ci-spack-ubuntu22.04-gcc11
