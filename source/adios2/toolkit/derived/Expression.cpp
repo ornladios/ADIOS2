6fbfc955520ef0c20bc826eeb6c5030bcbc9c006
Ana Gainaru <ana.gainaru@gmail.com>
2025-09-24 12:31:16 -0400

    Remove one level of abstraction from the Expression class

--- source/adios2/toolkit/derived/Expression.cpp
+++ source/adios2/toolkit/derived/Expression.cpp
@@ -87,53 +87,45 @@ ExpressionOperator convert_op(std::string opname)
     return op;
 };
 
-adios2::derived::ExpressionTree ASTNode_to_ExpressionTree(adios2::detail::ASTNode *node)
+void ASTNode_to_ExpressionTree(adios2::detail::ASTNode *node, adios2::derived::Expression &exp)
 {
-    adios2::derived::ExpressionTree exprTree_node(convert_op(node->get_opname()));
+    exp.SetOperationType(convert_op(node->get_opname()));
     for (adios2::detail::ASTNode *e : node->get_subexprs())
     {
         switch (convert_op(e->get_opname()))
         {
         case adios2::detail::ExpressionOperator::OP_ALIAS: // add variable given by alias
-            // add an index operation in the chain if the variable contains indeces
-            /*if (e->lookup_var_indices(e->alias) != "")
-            {
-                ExpressionTree index_expr(adios2::detail::ExpressionOperator::OP_INDEX);
-                index_expr.set_indeces(e->lookup_var_indices(e->alias));
-                index_expr.add_child(e->lookup_var_path(e->alias));
-                expTree_node->add_child(expr);
-            }*/
-            exprTree_node.add_child(e->get_varname());
+            exp.AddVarChild(e->get_varname());
             break;
         case adios2::detail::ExpressionOperator::OP_PATH: // add variable name
-            exprTree_node.add_child(e->get_varname());
+            exp.AddVarChild(e->get_varname());
             break;
-        case adios2::detail::ExpressionOperator::OP_NUM: // set the base value for the operation
-            exprTree_node.set_base(e->get_value());
+        case adios2::detail::ExpressionOperator::OP_NUM: // add number
+            exp.AddNumChild(e->get_number());
             break;
         default: // if the children nodes are other expressions, convert them to expressions
-            auto temp_node = ASTNode_to_ExpressionTree(e);
+            adios2::derived::Expression temp_node("");
+            ASTNode_to_ExpressionTree(e, temp_node);
             // move from a binary to a multinary tree if the child has the same operation
             if (convert_op(e->get_opname()) == convert_op(node->get_opname()) &&
-                adios2::detail::op_property.at(convert_op(e->get_opname())).is_associative)
+                adios2::detail::get_op_associativity(convert_op(e->get_opname())))
             {
+                auto subExpr = temp_node.GetChildren();
                 // concatenate exprTree with temp_node
-                for (std::tuple<adios2::derived::ExpressionTree, std::string, bool> childTree :
-                     temp_node.sub_exprs)
+                for (auto childTree : subExpr)
                 {
                     if (std::get<2>(childTree) == true)
-                        exprTree_node.add_child(std::get<0>(childTree));
+                        exp.AddExpChild(std::get<0>(childTree));
                     else
-                        exprTree_node.add_child(std::get<1>(childTree));
+                        exp.AddVarChild(std::get<1>(childTree));
                 }
             }
             else
             {
-                exprTree_node.add_child(temp_node);
+                exp.AddExpChild(temp_node);
             }
         }
     }
-    return exprTree_node;
 }
 }
 
@@ -165,13 +157,15 @@ std::map<adios2::detail::ExpressionOperator, OperatorFunctions> OpFunctions = {
     {adios2::detail::ExpressionOperator::OP_CURL, {Curl3DFunc, CurlDimsFunc, SameTypeFunc}}};
 
 Expression::Expression(std::string string_exp)
-: m_Shape({0}), m_Start({0}), m_Count({0}), ExprString(string_exp)
+: m_Operator(adios2::detail::ExpressionOperator::OP_NULL), m_Shape({0}), m_Start({0}), m_Count({0}),
+  m_ExprString(string_exp)
 {
-    adios2::detail::ASTDriver drv(string_exp);
-    m_Expr = adios2::detail::ASTNode_to_ExpressionTree(drv.getAST());
+    if (string_exp != "")
+    {
+        adios2::detail::ASTDriver drv(string_exp);
+        adios2::detail::ASTNode_to_ExpressionTree(drv.getAST(), *this);
+    }
 }
-
-std::vector<std::string> Expression::VariableNameList() { return m_Expr.VariableNameList(); }
 
 Dims Expression::GetShape() { return m_Shape; }
 
@@ -179,149 +173,10 @@ Dims Expression::GetStart() { return m_Start; }
 
 Dims Expression::GetCount() { return m_Count; }
 
-std::string Expression::toStringExpr() { return m_Expr.toStringExpr(); }
-
-std::tuple<Dims, Dims, Dims>
-Expression::GetDims(std::map<std::string, std::tuple<Dims, Dims, Dims>> NameToDims)
-{
-    return m_Expr.GetDims(NameToDims);
-}
-
-void Expression::SetDims(std::map<std::string, std::tuple<Dims, Dims, Dims>> NameToDims)
-{
-    auto outDims = m_Expr.GetDims(NameToDims);
-    m_Count = std::get<1>(outDims);
-    m_Start = std::get<0>(outDims);
-    m_Shape = std::get<2>(outDims);
-}
-
 DataType Expression::GetType(std::map<std::string, DataType> NameToType)
 {
-    return m_Expr.GetType(NameToType);
-}
-
-std::vector<DerivedData>
-Expression::ApplyExpression(const size_t numBlocks,
-                            std::map<std::string, std::vector<DerivedData>> nameToData)
-{
-    return m_Expr.ApplyExpression(numBlocks, nameToData);
-}
-
-void ExpressionTree::set_base(double c) { detail.constant = c; }
-
-void ExpressionTree::set_indeces(std::vector<std::tuple<size_t, size_t, size_t>> index_list)
-{
-    detail.indices = index_list;
-}
-
-void ExpressionTree::add_child(ExpressionTree exp) { sub_exprs.push_back({exp, "", true}); }
-
-void ExpressionTree::add_child(std::string var)
-{
-    sub_exprs.push_back({ExpressionTree(), var, false});
-}
-
-std::vector<std::string> ExpressionTree::VariableNameList()
-{
-    std::vector<std::string> var_list;
-    for (auto subexp : sub_exprs)
-    {
-        // if the sub_expression is a leaf
-        if (!std::get<2>(subexp))
-        {
-            var_list.push_back(std::get<1>(subexp));
-        }
-        else
-        {
-            auto subexpr_list = std::get<0>(subexp).VariableNameList();
-            var_list.insert(var_list.end(), subexpr_list.begin(), subexpr_list.end());
-        }
-    }
-    return var_list;
-}
-
-void ExpressionTree::print()
-{
-    std::cout << "Print Expression:" << std::endl;
-    std::cout << "\toperation: " << get_op_name(detail.operation) << std::endl;
-    std::cout << "\tconstant: " << detail.constant << std::endl;
-    std::cout << "\tchildren: " << sub_exprs.size() << std::endl;
-
-    for (std::tuple<ExpressionTree, std::string, bool> t : sub_exprs)
-    {
-        if (std::get<2>(t) == true)
-        {
-            std::get<0>(t).print();
-        }
-        else
-        {
-            std::cout << "string: " << std::get<1>(t) << std::endl;
-        }
-    }
-}
-
-std::string ExpressionTree::toStringExpr()
-{
-    std::string result = "";
-    result += get_op_name(detail.operation) + "(";
-    for (std::tuple<ExpressionTree, std::string, bool> t : sub_exprs)
-    {
-        if (std::get<2>(t) == true)
-        {
-            result += std::get<0>(t).toStringExpr();
-        }
-        else
-        {
-            result += "{" + std::get<1>(t) + "}";
-            if (!detail.indices.empty())
-            {
-                result += "[ ";
-                for (std::tuple<size_t, size_t, size_t> idx : detail.indices)
-                {
-                    result += (std::get<0>(idx) < 0 ? "" : std::to_string(std::get<0>(idx))) + ":";
-                    result += (std::get<1>(idx) < 0 ? "" : std::to_string(std::get<1>(idx))) + ":";
-                    result += (std::get<2>(idx) < 0 ? "" : std::to_string(std::get<2>(idx))) + ",";
-                }
-                // remove last comma
-                result.pop_back();
-                result += " ]";
-            }
-        }
-        result += ",";
-    }
-    // remove last comma
-    result.pop_back();
-    result += ")";
-
-    return result;
-}
-
-std::tuple<Dims, Dims, Dims>
-ExpressionTree::GetDims(std::map<std::string, std::tuple<Dims, Dims, Dims>> NameToDims)
-{
-    std::vector<std::tuple<Dims, Dims, Dims>> exprDims;
-    for (auto subexp : sub_exprs)
-    {
-        // if the sub_expression is a leaf, we get the shape from the input std::map
-        if (!std::get<2>(subexp))
-        {
-            exprDims.push_back(NameToDims[std::get<1>(subexp)]);
-        }
-        else
-        {
-            exprDims.push_back(std::get<0>(subexp).GetDims(NameToDims));
-        }
-    }
-    // get the output dimensions after applying the operator
-    auto op_fct = OpFunctions.at(detail.operation);
-    auto opDims = op_fct.DimsFct(exprDims);
-    return opDims;
-}
-
-DataType ExpressionTree::GetType(std::map<std::string, DataType> NameToType)
-{
     std::vector<DataType> exprType;
-    for (auto subexp : sub_exprs)
+    for (auto subexp : m_SubExprs)
     {
         // if the sub_expression is a leaf, we get the type of the current oeration
         if (!std::get<2>(subexp))
@@ -341,20 +196,59 @@ DataType ExpressionTree::GetType(std::map<std::string, DataType> NameToType)
             helper::Throw<std::invalid_argument>("Derived", "Expression", "GetType",
                                                  "Derived expression operators are not the same");
     // get the output type after applying the operator
-    auto op_fct = OpFunctions.at(detail.operation);
+    auto op_fct = OpFunctions.at(m_Operator);
     DataType opType = op_fct.TypeFct(exprType[0]);
     return opType;
 }
 
+std::tuple<Dims, Dims, Dims>
+Expression::GetDims(std::map<std::string, std::tuple<Dims, Dims, Dims>> NameToDims)
+{
+    std::vector<std::tuple<Dims, Dims, Dims>> exprDims;
+    for (auto subexp : m_SubExprs)
+    {
+        // if the sub_expression is a leaf, we get the shape from the input std::map
+        if (!std::get<2>(subexp))
+        {
+            exprDims.push_back(NameToDims[std::get<1>(subexp)]);
+        }
+        else
+        {
+            exprDims.push_back(std::get<0>(subexp).GetDims(NameToDims));
+        }
+    }
+    // get the output dimensions after applying the operator
+    auto op_fct = OpFunctions.at(m_Operator);
+    auto opDims = op_fct.DimsFct(exprDims, m_Consts.size() > 0);
+    return opDims;
+}
+
+std::vector<std::tuple<Expression, std::string, bool>> Expression::GetChildren()
+{
+    return m_SubExprs;
+}
+
+std::vector<std::string> Expression::GetConstants() { return m_Consts; }
+
+void Expression::SetDims(std::map<std::string, std::tuple<Dims, Dims, Dims>> NameToDims)
+{
+    auto outDims = GetDims(NameToDims);
+    m_Count = std::get<1>(outDims);
+    m_Start = std::get<0>(outDims);
+    m_Shape = std::get<2>(outDims);
+}
+
+void Expression::SetOperationType(adios2::detail::ExpressionOperator op) { m_Operator = op; }
+
 std::vector<DerivedData>
-ExpressionTree::ApplyExpression(const size_t numBlocks,
-                                std::map<std::string, std::vector<DerivedData>> nameToData)
+Expression::ApplyExpression(const size_t numBlocks,
+                            std::map<std::string, std::vector<DerivedData>> nameToData)
 {
     // create operands for the computation function
     // exprData[0] = list of void* data for block 0 for each variable
     std::vector<std::vector<DerivedData>> exprData(numBlocks);
     std::vector<bool> deallocate;
-    for (auto subexp : sub_exprs)
+    for (auto subexp : m_SubExprs)
     {
         // leafs
         if (!std::get<2>(subexp))
@@ -380,7 +274,7 @@ ExpressionTree::ApplyExpression(const size_t numBlocks,
     }
     // apply the computation operator on all blocks
     std::vector<DerivedData> outputData(numBlocks);
-    auto op_fct = OpFunctions.at(detail.operation);
+    auto op_fct = OpFunctions.at(m_Operator);
     // get the type of the output data
     std::vector<DataType> exprType;
     for (auto op : exprData[0])
@@ -416,6 +310,78 @@ ExpressionTree::ApplyExpression(const size_t numBlocks,
         }
     }
     return outputData;
+}
+
+void Expression::AddExpChild(Expression exp) { m_SubExprs.push_back({exp, "", true}); }
+
+void Expression::AddVarChild(std::string var) { m_SubExprs.push_back({Expression(), var, false}); }
+
+void Expression::AddNumChild(std::string c) { m_Consts.push_back(c); }
+
+std::string Expression::toStringExpr()
+{
+    std::string result = "";
+    result += get_op_name(m_Operator) + "(";
+    for (std::tuple<Expression, std::string, bool> t : m_SubExprs)
+    {
+        if (std::get<2>(t) == true)
+        {
+            result += std::get<0>(t).toStringExpr();
+        }
+        else
+        {
+            result += "{" + std::get<1>(t) + "}";
+        }
+        result += ",";
+    }
+    // remove last comma
+    result.pop_back();
+    result += ")";
+
+    return result;
+}
+
+std::vector<std::string> Expression::VariableNameList()
+{
+    std::vector<std::string> var_list;
+    for (auto subexp : m_SubExprs)
+    {
+        // if the sub_expression is a leaf
+        if (!std::get<2>(subexp))
+        {
+            var_list.push_back(std::get<1>(subexp));
+        }
+        else
+        {
+            auto subexpr_list = std::get<0>(subexp).VariableNameList();
+            var_list.insert(var_list.end(), subexpr_list.begin(), subexpr_list.end());
+        }
+    }
+    return var_list;
+}
+
+void Expression::Print()
+{
+    std::cout << "Print Expression:" << std::endl;
+    std::cout << "\toperation: " << get_op_name(m_Operator) << std::endl;
+    std::cout << "\tconstants: " << m_Consts.size() << std::endl;
+    std::cout << "\t\tvalues: ";
+    for (const auto &v : m_Consts)
+        std::cout << v << " ";
+    std::cout << std::endl;
+    std::cout << "\tchildren: " << m_SubExprs.size() << std::endl;
+
+    for (std::tuple<Expression, std::string, bool> t : m_SubExprs)
+    {
+        if (std::get<2>(t) == true)
+        {
+            std::get<0>(t).Print();
+        }
+        else
+        {
+            std::cout << "string: " << std::get<1>(t) << std::endl;
+        }
+    }
 }
 
 }
