9da6a7c358065904bf60238cbebc7cc8f04a6efb
Scott Wittenburg <scott.wittenburg@kitware.com>
2025-10-08 17:04:40 -0600

    WIP: add debug stmts

--- testing/adios2/engine/bp/TestBPRerouteMessage.cpp
+++ testing/adios2/engine/bp/TestBPRerouteMessage.cpp
@@ -5,10 +5,12 @@
 
 #include <adios2.h>
 #include <adios2/helper/adiosCommMPI.h>
+#include "adios2/helper/adiosMemory.h"
 #include <adios2/helper/adiosRerouting.h>
 #include <gtest/gtest.h>
 #include <mpi.h>
 #include <numeric>
+#include <sstream>
 #include <thread>
 
 using namespace adios2;
@@ -46,7 +48,18 @@ void SendAndReceiveMessage(helper::Comm &comm, int destRank, int srcRank)
     adios2::helper::RerouteMessage receivedMsg;
     receivedMsg.BlockingRecvFrom(comm, status.Source, recvBuffer);
 
-    // ASSERT_EQ(receivedMsg.m_MsgType, origMsg.m_MsgType);
+    std::stringstream ss;
+
+    ss << "m_MsgType, orig = " << static_cast<int>(origMsg.m_MsgType)
+              << ", rcvd = " << static_cast<int>(receivedMsg.m_MsgType) << "\n";
+    ss << "m_SrcRank, orig = " << srcRank << ", rcvd = " << receivedMsg.m_SrcRank << "\n";
+    ss << "m_DestRank, orig = " << worldRank << ", rcvd = " << receivedMsg.m_DestRank << "\n";
+    ss << "m_Offset, orig = " << origMsg.m_Offset << ", rcvd = " << receivedMsg.m_Offset << "\n";
+    ss << "m_Size, orig = " << origMsg.m_Size << ", rcvd = " << receivedMsg.m_Size << "\n";
+
+    std::cout << ss.str();
+
+    ASSERT_EQ(receivedMsg.m_MsgType, origMsg.m_MsgType);
     ASSERT_EQ(receivedMsg.m_SrcRank, srcRank);
     ASSERT_EQ(receivedMsg.m_DestRank, worldRank);
     ASSERT_EQ(receivedMsg.m_Offset, origMsg.m_Offset);
@@ -78,6 +91,32 @@ TEST_F(RerouteTest, TestSendReceiveSelf)
     int expectedSender = worldRank;
 
     SendAndReceiveMessage(comm, destRank, expectedSender);
+}
+
+TEST_F(RerouteTest, TestSendReceiveBare)
+{
+    helper::Comm comm = adios2::helper::CommDupMPI(MPI_COMM_WORLD);
+
+    int sendToRank = worldRank >= worldSize - 1 ? 0 : worldRank + 1;
+    int recvFromRank = worldRank <= 0 ? worldSize - 1 : worldRank - 1;
+
+    std::vector<char> sendBuffer;
+
+    sendBuffer.push_back(1);
+    sendBuffer.push_back(2);
+    sendBuffer.push_back(3);
+
+    comm.Isend(sendBuffer.data(), sendBuffer.size(), sendToRank, 0);
+
+    std::vector<char> recvBuffer;
+    recvBuffer.resize(sendBuffer.size());
+
+    comm.Recv(recvBuffer.data(), recvBuffer.size(), recvFromRank, 0);
+
+    ASSERT_EQ(recvBuffer.size(), 3);
+    ASSERT_EQ(static_cast<int>(recvBuffer[0]), 1);
+    ASSERT_EQ(static_cast<int>(recvBuffer[1]), 2);
+    ASSERT_EQ(static_cast<int>(recvBuffer[2]), 3);
 }
 
 int main(int argc, char **argv)
