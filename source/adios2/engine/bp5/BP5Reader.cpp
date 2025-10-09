f6bcef19583b101fb60db38120936293339e820e
Greg Eisenhauer <eisen@cc.gatech.edu>
2025-10-09 13:03:56 -0400

    Use RemoteName

--- source/adios2/engine/bp5/BP5Reader.cpp
+++ source/adios2/engine/bp5/BP5Reader.cpp
@@ -526,7 +526,7 @@ void BP5Reader::PerformGets()
                 }
             }
             m_Remote = std::unique_ptr<XrootdRemote>(new XrootdRemote(m_HostOptions));
-            m_Remote->Open(XRootDHost, XRootDPort, m_Name, m_OpenMode, RowMajorOrdering);
+            m_Remote->Open(XRootDHost, XRootDPort, RemoteName, m_OpenMode, RowMajorOrdering);
         }
         else
 #endif
