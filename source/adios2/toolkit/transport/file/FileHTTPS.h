65a830535958fa7b292a807b05da05bae02f4a54
Norbert Podhorszki <pnorbert@ornl.gov>
2025-10-23 11:43:41 -0400

    include functions

--- source/adios2/toolkit/transport/file/FileHTTPS.h
+++ source/adios2/toolkit/transport/file/FileHTTPS.h
@@ -4,6 +4,7 @@
 #include "../Transport.h"
 #include "./FileFStream.h"
 #include "adios2/common/ADIOSConfig.h"
+#include "adios2/helper/adiosFunctions.h"
 #include "adios2/helper/adiosNetwork.h" // NetworkSocket
 
 #include <openssl/err.h>
