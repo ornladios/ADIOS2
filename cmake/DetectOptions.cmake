70b09272d0be099617afb5edb8837eadc5d3f137
pnorbert <pnorbert@ornl.gov>
2025-10-28 14:09:20 -0400

    Aca 0.6 https again (#4693)
    * - HTTPS transport to read BP files from a HTTPS server
       E.g. bpls -l -E bp5 -T "Library=https" https://users.nccs.gov/~pnorbert/heatmap.bp -d temperature -n 10
    - Add OpenSSL as an adios_option for cmake, compile HTTPS transport conditionally.
    - Process ACA 0.6 format, archives under directories. Handle https default_protocol for a host
    - Campaign manager handles adios datasets on https. HTTPS transport processes remote file parametets from a campaign, and uses local cache for metadata.
    
    * cmake: setup OpenSSL dep in cmake config file
    
    * fix S3 url
    
    ---------
    
    Co-authored-by: Vicente Adolfo Bolea Sanchez <vicente.bolea@kitware.com>

--- cmake/DetectOptions.cmake
+++ cmake/DetectOptions.cmake
@@ -598,6 +598,16 @@ elseif(ADIOS2_USE_AWSSDK)
 endif()
 if(AWSSDK_FOUND)
     set(ADIOS2_HAVE_AWSSDK TRUE)
+endif(AWSSDK_FOUND)
+
+# OpenSSL
+if(ADIOS2_USE_OpenSSL STREQUAL AUTO)
+  find_package(OpenSSL COMPONENTS Crypto SSL)
+elseif(ADIOS2_USE_OpenSSL)
+  find_package(OpenSSL REQUIRED COMPONENTS Crypto SSL)
+endif()
+if(OpenSSL_FOUND)
+  set(ADIOS2_HAVE_OpenSSL TRUE)
 endif()
 
 # XRootD
