cc3775df924dc6c7bd1d35cca049bd1beee7e507
Vicente Adolfo Bolea Sanchez <vicente.bolea@kitware.com>
2025-11-24 21:50:19 -0500

    cmake: remove unneeded double find_package(python)

--- testing/adios2/engine/staging-common/TestSupp.cmake
+++ testing/adios2/engine/staging-common/TestSupp.cmake
@@ -56,8 +56,6 @@
 # resulting _CMD strings until add_common_test() which removes it.
 #
 
-find_package(Python3 REQUIRED)
-
 # Change the STAGING_COMMON_TEST_SUPP_VERBOSE value to ON for debugging output
 #
 set (STAGING_COMMON_TEST_SUPP_VERBOSE OFF)
