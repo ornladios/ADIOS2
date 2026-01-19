5b97826b48180f90d0ebf785e3f0a64987656233
atl Upstream <robot@adios2>
2026-01-19 07:53:59 -0500

    atl 2026-01-19 (e83910df)
    Code extracted from:
    
        https://github.com/GTkorvo/atl.git
    
    at commit e83910dfd1c778f0715358b63cc64a1bb36f60d8 (master).
    
    Upstream Shortlog
    -----------------

--- scripts/ci/cmake/windows2022-vs2022-msvc.cmake
+++ scripts/ci/cmake/windows2022-vs2022-msvc.cmake
@@ -1,11 +1,7 @@
 # Client maintainer: eisen@cc.gatech.edu
 
-set(dashboard_cache "
-")
-
-set(CTEST_CMAKE_GENERATOR "Visual Studio 16")
+set(CTEST_CMAKE_GENERATOR "Visual Studio 17 2022")
 set(CTEST_CMAKE_GENERATOR_PLATFORM x64)
-set(CTEST_CMAKE_GENERATOR_TOOLSET ClangCL)
 
 list(APPEND CTEST_UPDATE_NOTES_FILES "${CMAKE_CURRENT_LIST_FILE}")
 include(${CMAKE_CURRENT_LIST_DIR}/windows-common.cmake)
