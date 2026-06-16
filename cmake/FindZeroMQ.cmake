f8e15b198224f28e5e4bc33bd8a7a9f6fd13a5dc
Greg Eisenhauer <eisen@cc.gatech.edu>
2026-06-16 12:36:30 -0400

    cmake: rework FindZeroMQ.cmake to a direct search with pkg-config fallback
    The old module scraped the version out of zmq.h (tripping CMP0159) and
    its find_library missed vcpkg's decorated names. It now searches for
    libzmq directly first (CMAKE_PREFIX_PATH-aware, no pkg-config tool),
    falling back to pkg-config for decorated names, and provides
    ZeroMQ::ZeroMQ.
    
    Closes #5088

--- cmake/FindZeroMQ.cmake
+++ cmake/FindZeroMQ.cmake
@@ -2,49 +2,37 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
+# Locate libzmq and provide the ZeroMQ::ZeroMQ imported target.  A direct
+# library search is tried first (CMAKE_PREFIX_PATH-aware, no pkg-config tool
+# needed); pkg-config is the fallback for decorated library names like
+# vcpkg's libzmq-mt-4_3_5 that a plain search misses.
 
-if(NOT ZeroMQ_FOUND)
-  if((NOT ZeroMQ_ROOT) AND (NOT (ENV{ZeroMQ_ROOT} STREQUAL "")))
-    set(ZeroMQ_ROOT "$ENV{ZeroMQ_ROOT}")
-  endif()
-  if(ZeroMQ_ROOT)
-    set(ZeroMQ_INCLUDE_OPTS HINTS ${ZeroMQ_ROOT}/include NO_DEFAULT_PATHS)
-    set(ZeroMQ_LIBRARY_OPTS
-      HINTS ${ZeroMQ_ROOT}/lib ${ZeroMQ_ROOT}/lib64
-      NO_DEFAULT_PATHS
-    )
-  endif()
-
-  find_path(ZeroMQ_INCLUDE_DIR zmq.h ${ZeroMQ_INCLUDE_OPTS})
-  find_library(ZeroMQ_LIBRARY zmq ${ZeroMQ_LIBRARY_OPTS})
-  if(ZeroMQ_INCLUDE_DIR)
-    file(STRINGS ${ZeroMQ_INCLUDE_DIR}/zmq.h _ver_strings
-      REGEX "ZMQ_VERSION_[^ ]* [0-9]+"
-    )
-    foreach(v IN LISTS _ver_strings)
-      string(REGEX MATCH "ZMQ_VERSION_([^ ]+) ([0-9]+)" v "${v}")
-      set(ZeroMQ_VERSION_${CMAKE_MATCH_1} ${CMAKE_MATCH_2})
-    endforeach()
-    set(ZeroMQ_VERSION
-      ${ZeroMQ_VERSION_MAJOR}.${ZeroMQ_VERSION_MINOR}.${ZeroMQ_VERSION_PATCH}
-    )
-  endif()
-
-  include(FindPackageHandleStandardArgs)
-  find_package_handle_standard_args(ZeroMQ
-    FOUND_VAR ZeroMQ_FOUND
-    VERSION_VAR ZeroMQ_VERSION
-    REQUIRED_VARS ZeroMQ_LIBRARY ZeroMQ_INCLUDE_DIR
-  )
-  if(ZeroMQ_FOUND)
-    set(ZeroMQ_INCLUDE_DIRS ${ZeroMQ_INCLUDE_DIR})
-    set(ZeroMQ_LIBRARIES ${ZeroMQ_LIBRARY})
-    if(ZeroMQ_FOUND AND NOT TARGET ZeroMQ::ZMQ)
-      add_library(ZeroMQ::ZMQ UNKNOWN IMPORTED)
-      set_target_properties(ZeroMQ::ZMQ PROPERTIES
-        IMPORTED_LOCATION             "${ZeroMQ_LIBRARY}"
-        INTERFACE_INCLUDE_DIRECTORIES "${ZeroMQ_INCLUDE_DIR}"
-      )
+if(NOT TARGET ZeroMQ::ZeroMQ)
+  find_path(ZeroMQ_INCLUDE_DIR NAMES zmq.h)
+  find_library(ZeroMQ_LIBRARY NAMES zmq)
+  if(ZeroMQ_INCLUDE_DIR AND ZeroMQ_LIBRARY)
+    add_library(ZeroMQ::ZeroMQ UNKNOWN IMPORTED)
+    set_target_properties(ZeroMQ::ZeroMQ PROPERTIES
+      IMPORTED_LOCATION "${ZeroMQ_LIBRARY}"
+      INTERFACE_INCLUDE_DIRECTORIES "${ZeroMQ_INCLUDE_DIR}")
+  else()
+    find_package(PkgConfig QUIET)
+    if(PKG_CONFIG_FOUND)
+      pkg_check_modules(PC_ZeroMQ QUIET IMPORTED_TARGET libzmq)
+      if(TARGET PkgConfig::PC_ZeroMQ)
+        add_library(ZeroMQ::ZeroMQ INTERFACE IMPORTED)
+        set_property(TARGET ZeroMQ::ZeroMQ
+          PROPERTY INTERFACE_LINK_LIBRARIES PkgConfig::PC_ZeroMQ)
+      endif()
     endif()
   endif()
 endif()
+
+if(TARGET ZeroMQ::ZeroMQ)
+  set(ZeroMQ_TARGET ZeroMQ::ZeroMQ)
+endif()
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(ZeroMQ REQUIRED_VARS ZeroMQ_TARGET)
+
+mark_as_advanced(ZeroMQ_INCLUDE_DIR ZeroMQ_LIBRARY)
