a6521748b0c6afa3419d538ec8aa46ddaee69990
Vicente Adolfo Bolea Sanchez <vicente.bolea@kitware.com>
2026-09-02 13:44:03 -0400

    ci: fix macos jobs

--- scripts/ci/gh-actions/macos-setup.sh
+++ scripts/ci/gh-actions/macos-setup.sh
@@ -49,6 +49,7 @@ conda init --all
 conda config --remove channels defaults
 conda config --add channels conda-forge
 conda config --set channel_priority strict
+conda update -n base -c conda-forge conda -y
 conda update --all -y
 
 conda env create --verbose -f "gha/scripts/ci/gh-actions/conda-env-macos.yml"
